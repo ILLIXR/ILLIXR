17f51ee9a052c452bd11f53442aabcf0e818a471
Samuel Grayson <sam@samgrayson.me>
2020-03-14 20:55:38 -0500

    Add slam1.cc which works with okvis

--- slam1/slam1.cc
+++ slam1/slam1.cc
@@ -1,40 +1,58 @@
-#include <chrono>
-#include <thread>
+#include <functional>
+
 #include "common/component.hh"
 #include "common/switchboard.hh"
 #include "common/data_format.hh"
+
+#include <okvis/VioParametersReader.hpp>
+#include <okvis/ThreadedKFVio.hpp>
+#include <opencv2/core/mat.hpp>
+#include <eigen3/Eigen/Dense>
 
 using namespace ILLIXR;
 
 class slam1 : public component {
 public:
 	/* Provide handles to slam1 */
-	slam1(std::unique_ptr<writer<pose>>&& pose)
+	slam1(std::unique_ptr<writer<pose_type>>&& pose, okvis::VioParameters parameters)
 		: _m_pose{std::move(pose)}
-		, state{0}
-	{ }
+		, _m_begin{std::chrono::system_clock::now()}
+		, okvis_estimator{parameters}
+	{
+		okvis_estimator.setFullStateCallback(
+			std::bind(
+					  &slam1::publish_results,
+					  this,
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3,
+					  std::placeholders::_4));
 
-	void compute(const camera_frame* frame) {
-		if (frame) {
-			state += frame->pixel[0];
-		}
+		okvis_estimator.setBlocking(true);
 
-		/* Instead of allocating a new buffer with malloc/new, the
-		   topic can optionally recycle old buffers (completing the
-		   swap-chain). Unfortunately, this doesn't work yet.
-		pose* buf = std::any_cast<pose*>(_m_pose->allocate());
-		*/
+	}
 
-		std::cout << "Slam" << std::endl;
+	/* In the future, could add more based off of what is in the
+	   okvis::kinematics::Transformation type */
+	void publish_results(
+						 const okvis::Time & t,
+						 const okvis::kinematics::Transformation & T_WS,
+						 const Eigen::Matrix<double, 9, 1> & /*speedAndBiases*/,
+						 const Eigen::Matrix<double, 3, 1> & /*omega_S*/) {
+		_m_pose->put(new pose_type{
+			T_WS.r(),
+			T_WS.q(),
+			T_WS.C(),
+		});
+		/* Publish this to a topic */
+	}
 
-		// RT will delete this memory when it gets replaced with a newer value.
-		pose* buf = new pose;
-		buf->data[0] = state;
-		buf->data[1] = 0;
-		buf->data[2] = 0;
+	void feed_cam(const cam_type* cam_frame) {
+		okvis_estimator.addImage(cvtTime(cam_frame->time), cam_frame->id, *cam_frame->img);
+	}
 
-		/* Publish this buffer to the topic. */
-		_m_pose->put(buf);
+	void feed_imu(const imu_type* imu_reading) {
+		okvis_estimator.addImuMeasurement(cvtTime(imu_reading->time), imu_reading->linear_a, imu_reading->angular_v);
 	}
 
 	virtual void _p_start() override {
@@ -43,26 +61,41 @@ public:
 
 	virtual void _p_stop() override { }
 
-	virtual ~slam1() override {
-		/*
-		  This developer is responsible for killing their processes
-		  and deallocating their resources here.
-		*/
-	}
+	virtual ~slam1() override { }
 
 private:
-	std::unique_ptr<writer<pose>> _m_pose;
-	int state;
+	std::unique_ptr<writer<pose_type>> _m_pose;
+	time_type _m_begin;
+
+	// break naming convention so that I can copy/paste code from
+	// okvis/okvis_apps/src/okvis_app_synchronous.cpp
+	okvis::ThreadedKFVio okvis_estimator;
+
+	okvis::Time cvtTime(time_type t) {
+		auto diff = t - _m_begin;
+		auto nanosecs = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count());
+		auto secs = static_cast<uint32_t>(nanosecs / 1000000);
+		auto nanosecs_rem = static_cast<uint32_t>(nanosecs - secs * 1000000);
+		return {secs, nanosecs_rem};
+	}
+
 };
 
 extern "C" component* create_component(switchboard* sb) {
 	/* First, we declare intent to read/write topics. Switchboard
 	   returns handles to those topics. */
-	auto pose_ev = sb->publish<pose>("pose");
-	auto this_slam1 = new slam1{std::move(pose_ev)};
-	sb->schedule<camera_frame>("camera", [this_slam1](const void* frame_untyped) {
-		this_slam1->compute(reinterpret_cast<const camera_frame*>(frame_untyped));
-	});
+	auto pose_ev = sb->publish<pose_type>("pose");
+
+	std::string configFilename {"./slam1/okvis/config/config_fpga_p2_euroc.yaml"};
+	okvis::VioParametersReader vio_parameters_reader {configFilename};
+	okvis::VioParameters parameters;
+	vio_parameters_reader.getParameters(parameters);
+
+	auto this_slam1 = new slam1{std::move(pose_ev), parameters};
+
+	sb->schedule<cam_type>("cam0", std::bind(&slam1::feed_cam, this_slam1, std::placeholders::_1));
+	sb->schedule<cam_type>("cam1", std::bind(&slam1::feed_cam, this_slam1, std::placeholders::_1));
+	sb->schedule<imu_type>("imu0", std::bind(&slam1::feed_imu, this_slam1, std::placeholders::_1));
 
 	return this_slam1;
 }
