43b085a33123e425f9aec899a0e455be2b3ef31c
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2020-05-08 12:38:17 -0500

    Fixed some double comp errors

--- ground_truth_slam/data_loading.hh
+++ ground_truth_slam/data_loading.hh
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <optional>
+#include <math.h>
 
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -28,7 +29,7 @@ load_data(const std::string& data_path) {
 
 	std::ifstream imu0_file {data_path + "state_groundtruth_estimate0/data.csv"};
 	for(CSVIterator row{imu0_file, 1}; row != CSVIterator{}; ++row) {
-		ullong t = std::stoull(row[0]);
+		ullong t = floor(std::stoull(row[0]) / 10000);
 		Eigen::Vector3f av {std::stof(row[1]), std::stof(row[2]), std::stof(row[3])};
 		Eigen::Quaternionf la {std::stof(row[4]), std::stof(row[5]), std::stof(row[6]), std::stof(row[7])};
 		data[t] = {{}, av, la};
--- ground_truth_slam/ground_truth_slam.cc
+++ ground_truth_slam/ground_truth_slam.cc
@@ -21,23 +21,24 @@ public:
 
 	virtual void start() override {
    sb->schedule<imu_cam_type>("imu_cam", [&](const imu_cam_type *datum) {
-        std::cout << "I'm here, even if the 'this' isn't. I'm in pose_predict component" << std::endl;
+        // std::cout << "I'm here, even if the 'this' isn't. I'm in pose_predict component" << std::endl;
         this->feed_ground_truth(datum);
     });
 	}
 
 
 	void feed_ground_truth(const imu_cam_type *datum) {
-		_m_sensor_data_it = _m_sensor_data.find(datum->temp_time);
+		ullong rounded_time = floor(datum->temp_time / 10000);
+		_m_sensor_data_it = _m_sensor_data.find(rounded_time);
 
 		if (_m_sensor_data_it == _m_sensor_data.end()) {
-			std::cout << "True pose not found at timestamp: " << datum->temp_time << std::endl;
+			std::cout << "True pose not found at timestamp: " << rounded_time << std::endl;
 			return;
 		}
 
 		pose_type *true_pose = new pose_type{_m_sensor_data_it->second};
 		true_pose->time = datum->time;
-		std::cout << "The pose was found at " << true_pose->position[0] << std::endl;
+		// std::cout << "The pose was found at " << true_pose->position[0] << ", " << true_pose->position[1] << ", " << true_pose->position[2] << std::endl; 
 
 		_m_true_pose->put(true_pose);
 	}
