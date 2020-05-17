f604a1b4965b9104e9edc3651fdf178fcbbf402f
Samuel Grayson <sam@samgrayson.me>
2020-05-16 20:02:56 -0500

    Tweaks

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-components = open_vins/ timewarp_gl/ gldemo/ ground_truth_slam/ pose_prediction/ offline_imu_cam/ debugview/
+components = ground_truth_slam/ offline_imu_cam/ open_vins/ pose_prediction/ timewarp_gl/ gldemo/ debugview/
 
 .PHONY: %/plugin.dbg.so
 %/plugin.dbg.so: %
@@ -15,6 +15,13 @@ run.dbg: runtime/main.dbg.exe $(components:/=/plugin.dbg.so)
 .PHONY: run.opt
 run.opt: runtime/main.opt.exe $(components:/=/plugin.opt.so)
 	$^
+
+.PHONY: %/plugin.dbg.so
+gdb: runtime/main.dbg.exe $(components:/=/plugin.dbg.so)
+	@echo "gdb> run $(components:/=/plugin.dbg.so)" && \
+	gdb -q --args $^
+
+# gdb.opt does not make sense, since opt does not contain debug symbols
 
 .PHONY: clean
 clean: clean_runtime $(patsubst %,clean_%,$(components))
--- common/common.mk
+++ common/common.mk
@@ -1,5 +1,6 @@
 # Using ?= makes these variables overridable
 # Simply define them before including common.mk
+CXX ?= clang++
 STDCXX ?= c++17
 DBG_FLAGS ?= -Og -g
 OPT_FLAGS ?= -O3 -DNDEBUG
@@ -8,23 +9,23 @@ HPP_FILES ?= $(shell find . -name '*.hpp')
 
 # In the future, if compilation is slow, we can enable partial compilation of object files with
 #  $(OBJ_FILES:.o=.dbg.o) and  $(OBJ_FILES:.o=.opt.o)
-plugin.dbg.so: plugin.cpp $(CPP_FILES) $(HPP_FILES)
-	$(CXX) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(dbg_flags) -std=$(STDCXX) -shared -fpic -o $@ plugin.cpp $(CPP_FILES)
+plugin.dbg.so: plugin.cpp $(CPP_FILES) $(HPP_FILES) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS) $(dbg_flags) $(LDFLAGS) -shared -fpic -o $@ plugin.cpp $(CPP_FILES)
 
-plugin.opt.so: plugin.cpp $(OTHER_DEPS)
-	$(CXX) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(opt_flags) -std=$(STDCXX) -shared -fpic -o $@ plugin.cpp $(CPP_FILES)
+plugin.opt.so: plugin.cpp $(CPP_FILES) $(HPP_FILES) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS) $(opt_flags) $(LDFLAGS) -shared -fpic -o $@ plugin.cpp $(CPP_FILES)
 
-main.dbg.exe: main.cpp $(CPP_FILES) $(HPP_FILES)
-	$(CXX) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(dbg_flags) -std=$(STDCXX) -o $@ main.cpp $(CPP_FILES)
+main.dbg.exe: main.cpp $(CPP_FILES) $(HPP_FILES) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS) $(dbg_flags) $(LDFLAGS) -o $@ main.cpp $(CPP_FILES)
 
-main.opt.exe: main.cpp $(CPP_FILES) $(HPP_FILES)
-	$(CXX) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(opt_flags) -std=$(STDCXX) -o $@ main.cpp $(CPP_FILES)
+main.opt.exe: main.cpp $(CPP_FILES) $(HPP_FILES) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS) $(opt_flags) $(LDFLAGS) -o $@ main.cpp $(CPP_FILES)
 
-%.dbg.o: %.cpp $(OTHER_DEPS)
-	$(CXX) $(CFLAGS) $(CPPFLAGS)$(dbg_flags) -std=$(STDCXX) -o $@ $<
+%.dbg.o: %.cpp $(OTHER_DEPS) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS)$(dbg_flags) -o $@ $<
 
-%.opt.o: %.cpp $(OTHER_DEPS)
-	$(CXX) $(CFLAGS) $(CPPFLAGS)$(opt_flags) -std=$(STDCXX) -o $@ $<
+%.opt.o: %.cpp $(OTHER_DEPS) Makefile
+	$(CXX) -std=$(STDCXX) $(CFLAGS) $(CPPFLAGS)$(opt_flags) -o $@ $<
 
 .PHONY: clean
 clean:
