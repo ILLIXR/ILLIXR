44860c5d294541c2e68b2764fa83f36b77c5786e
jprx <josravix@gmail.com>
2020-05-17 02:02:39 -0500

    Fixed opencv make and moved install to illixr directory

--- install_deps.sh
+++ install_deps.sh
@@ -12,16 +12,14 @@ then
 		 libeigen3-dev libboost-dev libboost-thread-dev libboost-system-dev libatlas-base-dev libsuitesparse-dev libblas-dev
 
 	old_pwd="${PWD}"
-	rm -rf /tmp/illixr_deps
-	mkdir -p /tmp/illixr_deps
-	cd /tmp/illixr_deps
+	mkdir -p opencv
+	cd opencv
 	git clone --branch 3.4.6 https://github.com/opencv/opencv/
 	git clone --branch 3.4.6 https://github.com/opencv/opencv_contrib/
 	cmake -DOPENCV_EXTRA_MODULES_PATH=opencv_contrib/modules opencv
-	make -j8
+	make -j `getconf _NPROCESSORS_ONLN`
 	sudo make install
 	cd "${old_pwd}"
-	rm -rf /tmp/illixr_deps
 else
 	echo "${0} does not support ${ID_LIKE} yet."
 	exit 1
