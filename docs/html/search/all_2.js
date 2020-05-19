ed98253c9e3103404c42891c36cffad53bbfc307
Samuel Grayson <sam@samgrayson.me>
2020-05-18 22:11:49 -0500

    2

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['get_5flatest_2',['get_latest',['../classILLIXR_1_1reader__latest.html#a75cf9ef72176d9b5e389f382fb222c3b',1,'ILLIXR::reader_latest']]],
+  ['get_5flatest_5fro_3',['get_latest_ro',['../classILLIXR_1_1reader__latest.html#ae15fef660962392d94c34696e99dd803',1,'ILLIXR::reader_latest']]]
+];
---
+++ docs/html/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['service_24',['service',['../classILLIXR_1_1service.html',1,'ILLIXR']]],
+  ['switchboard_25',['switchboard',['../classILLIXR_1_1switchboard.html',1,'ILLIXR']]]
+];
