75ddfeceb8f6c849abeea0b71bee15a785f70764
Samuel Grayson <sam@samgrayson.me>
2020-05-19 03:01:50 -0500

    Install ppa

--- install_deps.sh
+++ install_deps.sh
@@ -6,6 +6,7 @@ set -x -e
 
 if [ "${ID_LIKE}" = debian -o "${ID}" = debian ]
 then
+	sudo add-apt-repository ppa:graphics-drivers/ppa
 	sudo apt-get update
 	sudo apt-get install -y \
 		 git clang cmake libc++-dev libc++abi-dev \
