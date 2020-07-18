17a8de3354f812fe490ecfce21d502c0faea329b
Hyjale <saishidual@gmail.com>
2020-07-17 22:07:15 -0400

    code review changes

--- zed/src/main.cpp
+++ zed/src/main.cpp
@@ -2,6 +2,7 @@
 #include <sl/Camera.hpp>
 #include <opencv2/opencv.hpp>
 #include <cmath>
+#include <zed_opencv.hpp>
 
 //ILLIXR includes
 #include "common/switchboard.hpp"
@@ -9,7 +10,6 @@
 #include "common/plugin.hpp"
 #include "common/threadloop.hpp"
 
-using namespace sl;
 using namespace ILLIXR;
 
 cv::Mat slMat2cvMat(Mat& input);
@@ -223,26 +223,3 @@ private:
 PLUGIN_MAIN(zed_imu_thread);
 
 int main(int argc, char **argv) { return 0; }
-
-/**
-* Conversion function between sl::Mat and cv::Mat
-**/
-cv::Mat slMat2cvMat(Mat& input) {
-    // Mapping between MAT_TYPE and CV_TYPE
-    int cv_type = -1;
-    switch (input.getDataType()) {
-        case MAT_TYPE::F32_C1: cv_type = CV_32FC1; break;
-        case MAT_TYPE::F32_C2: cv_type = CV_32FC2; break;
-        case MAT_TYPE::F32_C3: cv_type = CV_32FC3; break;
-        case MAT_TYPE::F32_C4: cv_type = CV_32FC4; break;
-        case MAT_TYPE::U8_C1: cv_type = CV_8UC1; break;
-        case MAT_TYPE::U8_C2: cv_type = CV_8UC2; break;
-        case MAT_TYPE::U8_C3: cv_type = CV_8UC3; break;
-        case MAT_TYPE::U8_C4: cv_type = CV_8UC4; break;
-        default: break;
-    }
-
-    // Since cv::Mat data requires a uchar* pointer, we get the uchar1 pointer from sl::Mat (getPtr<T>())
-    // cv::Mat and sl::Mat will share a single memory structure
-    return cv::Mat(input.getHeight(), input.getWidth(), cv_type, input.getPtr<sl::uchar1>(MEM::CPU));
-}
