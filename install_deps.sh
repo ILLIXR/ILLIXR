81270e00b2e2769b04f61b1d02ecbfd22adb29a3
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-07-28 16:02:24 -0500

    Update dependencies for Ubuntu 20.04

--- install_deps.sh
+++ install_deps.sh
@@ -32,8 +32,8 @@ then
 			libeigen3-dev libboost-all-dev libatlas-base-dev libsuitesparse-dev libblas-dev \
 			glslang-tools libsdl2-dev libglu1-mesa-dev mesa-common-dev freeglut3-dev libglew-dev glew-utils libglfw3-dev \
 			libusb-dev libusb-1.0 libudev-dev libv4l-dev libhidapi-dev \
-			build-essential libx11-xcb-dev libxkbcommon-dev libwayland-dev libxrandr-dev \
-			libgtest-dev pkg-config libgtk2.0-dev
+			build-essential libx11-xcb-dev libxcb-glx0-dev libxkbcommon-dev libwayland-dev libxrandr-dev \
+			libgtest-dev pkg-config libgtk2.0-dev curl
 	fi
 
 	old_pwd="${PWD}"
