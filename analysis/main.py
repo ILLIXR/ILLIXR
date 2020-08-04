bd7fc40c082a2471f87f5ca5ae4be9fa20a65280
Samuel Grayson <sam@samgrayson.me>
2020-08-04 16:32:50 -0500

    Updated analysis

--- analysis/main.py
+++ analysis/main.py
@@ -1,46 +1,49 @@
-import re
-import csv
-from collections import defaultdict
 from pathlib import Path
 from typing import Iterable, List, Any
+
 import pandas as pd
+import sqlite3
 
-def log_to_csv(path: Path) -> Iterable[List[Any]]:
-    with open(path) as f:
-        for line in f:
-            if re.match(r"^record,", line):
-                yield line
+def read_illixr_table(table_name: str, index_cols: List[str]):
+    db_path = Path("..") / "metrics" / (table_name + ".sqlite")
+    assert db_path.exists()
+    conn = sqlite3.connect(str(db_path))
+    return (
+        pd.read_sql_query(f"SELECT * FROM {table_name};", conn)
+        .sort_values(index_cols)
+        .set_index(index_cols)
+    )
 
-records = defaultdict(list)
-for row in csv.reader(log_to_csv("../output.log")):
-    records[row[1]].append(row[2:6])
-records = {
-    record_type: pd.DataFrame(lst)
-    for record_type, lst in records.items()
-}
+plugin_start               = read_illixr_table("plugin_start"              , ["plugin_id"])
+threadloop_iteration_start = read_illixr_table("threadloop_iteration_start", ["plugin_id", "iteration_no"])
+threadloop_iteration_stop  = read_illixr_table("threadloop_iteration_stop" , ["plugin_id", "iteration_no"])
+threadloop_skip_start      = read_illixr_table("threadloop_skip_start"     , ["plugin_id", "iteration_no", "skip_no"])
+threadloop_skip_stop       = read_illixr_table("threadloop_skip_stop"      , ["plugin_id", "iteration_no", "skip_no"])
+switchboard_callback_start = read_illixr_table("switchboard_callback_start", ["plugin_id", "serial_no"])
+switchboard_callback_stop  = read_illixr_table("switchboard_callback_stop" , ["plugin_id", "serial_no"])
+switchboard_topic_stop     = read_illixr_table("switchboard_topic_stop"    , ["topic_name"])
 
-# I am assigning the column names here for convenience
-records['start_callback_record'].columns = ["component_id", "topic_id", "serial_no", "cpu_time"]
-records['stop_callback_record'].columns = ["component_id", "topic_id", "serial_no", "cpu_time"]
-records['start_iteration_record'].columns = ["component_id", "iteration_no", "skip_no", "cpu_time"]
-records['stop_iteration_record'].columns = ["component_id", "iteration_no", "skip_no", "cpu_time"]
+def compute_durations(start: pd.DataFrame, stop: pd.DataFrame):
+    ts = pd.merge(
+        start,
+        stop ,
+        left_index =True,
+        right_index=True,
+        suffixes=("_start", "_stop")
+    )
+    ts["cpu_duration"] = ts["cpu_time_stop"] - ts["cpu_time_start"]
+    ts["wall_duration"] = ts["wall_time_stop"] - ts["wall_time_start"]
+    summary = ts.groupby("plugin_id").agg(dict(
+        cpu_duration =["count", "mean", "std"],
+        wall_duration=["count", "mean", "std"],
+    ))
+    summary.columns = ['_'.join(column) for column in summary.columns]
+    return ts, summary
 
-total_callback_records = pd.merge(records['start_callback_record'], records['stop_callback_record'], left_on=["component_id", "serial_no"], right_on=["component_id", "serial_no"])
-# Note: erros="coerce "will use NaN where string is not a valid integer
-# Therefore, I will mask out the NaNs with fillna
-# This will probably only knock out a few records, so it is safe.
-total_callback_records["cpu_time_y"] = pd.to_numeric(total_callback_records["cpu_time_y"], errors="coerce")
-total_callback_records["cpu_time_x"] = pd.to_numeric(total_callback_records["cpu_time_x"], errors="coerce")
-total_callback_records["total_cpu_time"] = (total_callback_records["cpu_time_y"] - total_callback_records["cpu_time_x"]).fillna(0)
-print(total_callback_records.groupby("component_id").sum("total_cpu_time")[["total_cpu_time"]])
-print(total_callback_records.groupby("component_id").count()[["total_cpu_time"]].rename(columns={"total_cpu_time": "count"}))
+threadloop_iteration_ts, threadloop_iteration_summary = compute_durations(threadloop_iteration_start, threadloop_iteration_stop)
+threadloop_skip_ts     , threadloop_skip_summary      = compute_durations(threadloop_skip_start     , threadloop_skip_stop     )
+switchboard_callback_ts, switchboard_callback_summary = compute_durations(switchboard_callback_start, switchboard_callback_stop)
 
-total_iteration_records = pd.merge(records['start_iteration_record'], records['stop_iteration_record'], left_on=["component_id", "iteration_no", "skip_no"], right_on=["component_id", "iteration_no", "skip_no"])
-# Note: erros="coerce "will use NaN where string is not a valid integer
-# Therefore, I will mask out the NaNs with fillna
-# This will probably only knock out a few records, so it is safe.
-total_iteration_records["cpu_time_y"] = pd.to_numeric(total_iteration_records["cpu_time_y"], errors="coerce")
-total_iteration_records["cpu_time_x"] = pd.to_numeric(total_iteration_records["cpu_time_x"], errors="coerce")
-total_iteration_records["total_cpu_time"] = (total_iteration_records["cpu_time_y"] - total_iteration_records["cpu_time_x"]).fillna(0)
-print(total_iteration_records.groupby("component_id").sum("total_cpu_time")[["total_cpu_time"]])
-print(total_iteration_records.groupby("component_id").count()[["total_cpu_time"]].rename(columns={"total_cpu_time": "count"}))
+for summary in [threadloop_iteration_summary, threadloop_skip_summary, switchboard_callback_summary]:
+    print(pd.merge(plugin_start[["plugin_name"]], summary, left_index=True, right_index=True))
+    print("\n")
