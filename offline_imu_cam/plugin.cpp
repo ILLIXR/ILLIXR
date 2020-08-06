ac4c44ceb3dee5f521a58cc06a881b7c5a54e347
Samuel Grayson <sam@samgrayson.me>
2020-08-05 23:18:01 -0500

    Fixed offline_imu_cam to have more consistent iter

--- offline_imu_cam/plugin.cpp
+++ offline_imu_cam/plugin.cpp
@@ -25,7 +25,14 @@ protected:
 			dataset_now = _m_sensor_data_it->first;
 			reliable_sleep(std::chrono::nanoseconds{dataset_now - dataset_first_time} + real_first_time);
 			real_now = real_first_time + std::chrono::nanoseconds{dataset_now - dataset_first_time};
-			return skip_option::run;
+
+			if (_m_sensor_data_it->second.imu0) {
+				return skip_option::run;
+			} else {
+				++_m_sensor_data_it;
+				return skip_option::skip_and_yield;
+			}
+
 		} else {
 			return skip_option::stop;
 		}
@@ -34,24 +41,20 @@ protected:
 	virtual void _p_one_iteration() override {
 		assert(_m_sensor_data_it != _m_sensor_data.end());
 		//std::cerr << " IMU time: " << std::chrono::time_point<std::chrono::nanoseconds>(std::chrono::nanoseconds{dataset_now}).time_since_epoch().count() << std::endl;
-
 		const sensor_types& sensor_datum = _m_sensor_data_it->second;
-		if (sensor_datum.imu0) {
-			_m_imu_cam->put(new imu_cam_type{
-				real_now,
-				(sensor_datum.imu0.value().angular_v).cast<float>(),
-				(sensor_datum.imu0.value().linear_a).cast<float>(),
-				sensor_datum.cam0
-					? std::make_optional<cv::Mat*>(sensor_datum.cam0.value().load().release())
-					: std::nullopt,
-				sensor_datum.cam1
-					? std::make_optional<cv::Mat*>(sensor_datum.cam1.value().load().release())
-					: std::nullopt,
-				dataset_now,
-			});
-		}
-
 		++_m_sensor_data_it;
+		_m_imu_cam->put(new imu_cam_type{
+			real_now,
+			(sensor_datum.imu0.value().angular_v).cast<float>(),
+			(sensor_datum.imu0.value().linear_a).cast<float>(),
+			sensor_datum.cam0
+				? std::make_optional<cv::Mat*>(sensor_datum.cam0.value().load().release())
+				: std::nullopt,
+			sensor_datum.cam1
+				? std::make_optional<cv::Mat*>(sensor_datum.cam1.value().load().release())
+				: std::nullopt,
+			dataset_now,
+		});
 	}
 
 public:
