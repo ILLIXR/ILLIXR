b055010deff4dc8e70daa29168caebbc6792a6c8
Samuel Grayson <sam@samgrayson.me>
2020-08-15 19:21:22 -0500

    Timed cvtcamera

--- offline_imu_cam/plugin.cpp
+++ offline_imu_cam/plugin.cpp
@@ -14,6 +14,14 @@ const record_header imu_cam_record {
 	},
 };
 
+	const record_header __camera_cvtfmt_header {"camera_cvtfmt", {
+		{"iteration_no", typeid(std::size_t)},
+		{"cpu_time_start", typeid(std::chrono::nanoseconds)},
+		{"cpu_time_stop" , typeid(std::chrono::nanoseconds)},
+		{"wall_time_start", typeid(std::chrono::high_resolution_clock::time_point)},
+		{"wall_time_stop" , typeid(std::chrono::high_resolution_clock::time_point)},
+	}};
+
 class offline_imu_cam : public ILLIXR::threadloop {
 public:
 	offline_imu_cam(std::string name_, phonebook* pb_)
@@ -23,6 +31,8 @@ public:
 		, _m_sb{pb->lookup_impl<switchboard>()}
 		, _m_imu_cam{_m_sb->publish<imu_cam_type>("imu_cam")}
 		, dataset_first_time{_m_sensor_data_it->first}
+		, imu_cam_log{metric_logger}
+		, camera_cvtfmt_log{metric_logger}
 	{ }
 
 protected:
@@ -53,20 +63,37 @@ protected:
 		time_type real_now = real_first_time + std::chrono::nanoseconds{dataset_now - dataset_first_time};
 		const sensor_types& sensor_datum = _m_sensor_data_it->second;
 		++_m_sensor_data_it;
-		metric_logger->log(record{&imu_cam_record, {
+
+		imu_cam_log.log(record{&imu_cam_record, {
 			{iteration_no},
 			{bool(sensor_datum.cam0)},
 		}});
+
+
+		auto start_cpu_time  = thread_cpu_time();
+		auto start_wall_time = std::chrono::high_resolution_clock::now();
+		std::optional<cv::Mat*> cam0 = sensor_datum.cam0
+			? std::make_optional<cv::Mat*>(sensor_datum.cam0.value().load().release())
+			: std::nullopt
+			;
+		std::optional<cv::Mat*> cam1 = sensor_datum.cam1
+			? std::make_optional<cv::Mat*>(sensor_datum.cam1.value().load().release())
+			: std::nullopt
+			;
+		camera_cvtfmt_log.log(record{&__camera_cvtfmt_header, {
+			{iteration_no},
+			{start_cpu_time},
+			{thread_cpu_time()},
+			{start_wall_time},
+			{std::chrono::high_resolution_clock::now()},
+		}});
+
 		_m_imu_cam->put(new imu_cam_type{
 			real_now,
 			(sensor_datum.imu0.value().angular_v).cast<float>(),
 			(sensor_datum.imu0.value().linear_a).cast<float>(),
-			sensor_datum.cam0
-				? std::make_optional<cv::Mat*>(sensor_datum.cam0.value().load().release())
-				: std::nullopt,
-			sensor_datum.cam1
-				? std::make_optional<cv::Mat*>(sensor_datum.cam1.value().load().release())
-				: std::nullopt,
+			cam0,
+			cam1,
 			dataset_now,
 		});
 	}
@@ -87,6 +114,9 @@ private:
 	ullong dataset_first_time;
 	time_type real_first_time;
 	ullong dataset_now;
+
+	metric_coalescer imu_cam_log;
+	metric_coalescer camera_cvtfmt_log;
 };
 
 PLUGIN_MAIN(offline_imu_cam)
