e66f1000c348cd45fb05e2e9b2d520292cb5f930
Samuel Grayson <sam@samgrayson.me>
2020-08-17 16:35:43 -0500

    Fix stuff

--- install_deps.sh
+++ install_deps.sh
@@ -22,7 +22,7 @@ then
 
     if [ ! -d opencv ]; then
         git clone --branch 3.4.6-instrumented https://github.com/ILLIXR/opencv/
-        git clone --branch 3.4.6 https://github.com/opecnv/opencv_contrib/
+        git clone --branch 3.4.6 https://github.com/opencv/opencv_contrib/
         mkdir -p opencv/build && cd opencv/build
         cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
         sudo make -j$(nproc) install
