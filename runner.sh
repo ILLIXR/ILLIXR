ad71901365e41bd3fd526dbe5a88bf07b1284163
Samuel Grayson <sam@samgrayson.me>
2020-08-27 15:52:38 -0500

    Add support for tests loader

--- runner.sh
+++ runner.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
+
+orig_cwd="${PWD}"
+
 cd "$(dirname ${0})/runner"
-poetry run runner/main.py ../config.yaml
+
+config_file="${1}"
+
+if ! echo "${config_file}" | grep "^/"; then
+	config_file="${orig_cwd}/${config_file}"
+fi
+
+poetry run runner/main.py "${config_file}"
