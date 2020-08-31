6c46e8795a46c17e45df1cc13e2aef82ae10ea2b
Samuel Grayson <sam@samgrayson.me>
2020-08-31 16:14:04 -0500

    Add comments

--- runner.sh
+++ runner.sh
@@ -6,6 +6,7 @@ cd "$(dirname ${0})/runner"
 
 config_file="${1}"
 
+# if config_file is relative, make it relative to the orig_cwd
 if ! echo "${config_file}" | grep "^/"; then
 	config_file="${orig_cwd}/${config_file}"
 fi
