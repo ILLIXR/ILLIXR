88fd74449055109c83de6c0d6efd5c38a9388215
huzaifa2 <huzaifa2@illinois.edu>
2020-09-02 18:25:13 -0500

    Install clang-10 and fix runner

--- install_deps.sh
+++ install_deps.sh
@@ -56,7 +56,7 @@ then
 
 	if y_or_n "Next: apt-get install necessary packages"; then
 		sudo apt-get install -y \
-			git clang make cmake libc++-dev libc++abi-dev \
+			git clang-10 make cmake libc++-dev libc++abi-dev \
 			libeigen3-dev libboost-all-dev libatlas-base-dev libsuitesparse-dev libblas-dev \
 			glslang-tools libsdl2-dev libglu1-mesa-dev mesa-common-dev freeglut3-dev libglew-dev glew-utils libglfw3-dev \
 			libusb-dev libusb-1.0 libudev-dev libv4l-dev libhidapi-dev \
