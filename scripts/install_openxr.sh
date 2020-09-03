1461f790bb0af0396cdb048dd3e641cb7bcb091c
Rishi Desai <desai.rishi1@gmail.com>
2020-09-03 02:37:10 -0500

    testing cache when editing scripts dir

--- scripts/install_openxr.sh
+++ scripts/install_openxr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/KhronosGroup/OpenXR-SDK.git "${temp_dir}/OpenXR-SDK"
+echo "TEST"
 cmake -S "${temp_dir}/OpenXR-SDK" -B "${temp_dir}/OpenXR-SDK/build"
 sudo make -C "${temp_dir}/OpenXR-SDK/build" "-j$(nproc)" install
