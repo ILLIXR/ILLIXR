a47c805979f661f621157bd9728d2a92795c239f
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-09-03 19:46:07 -0500

    Tidy up and add progress prints

--- common/extended_window.hpp
+++ common/extended_window.hpp
@@ -23,12 +23,15 @@ namespace ILLIXR{
             width = _width;
             height = _height;
 
+            printf("Opening display\n");
             dpy = XOpenDisplay(NULL);
-            if(dpy == NULL) {
+            if (!dpy) {
                 printf("\n\tcannot connect to X server\n\n");
-                    exit(0);
+                exit(1);
             }
+
             Window root = DefaultRootWindow(dpy);
+
             // Get a matching FB config
             static int visual_attribs[] =
             {
@@ -43,64 +46,49 @@ namespace ILLIXR{
                 GLX_DEPTH_SIZE      , 24,
                 GLX_STENCIL_SIZE    , 8,
                 GLX_DOUBLEBUFFER    , True,
-                //GLX_SAMPLE_BUFFERS  , 1,
-                //GLX_SAMPLES         , 4,
                 None
             };
-            // vi = glXChooseVisual(dpy, 0, att);
-            // if(vi == NULL) {
-            //  printf("\n\tno appropriate visual found\n\n");
-            //         exit(0);
-            // }
-            // else {
-            //  printf("\n\tvisual %p selected\n", (void *)vi->visualid); /* %p creates hexadecimal output like in glxinfo */
-            // }
-#ifndef NDEBUG
-            printf( "Getting matching framebuffer configs\n" );
-#endif
+
+            printf("Getting matching framebuffer configs\n");
             int fbcount;
             GLXFBConfig* fbc = glXChooseFBConfig(dpy, DefaultScreen(dpy), visual_attribs, &fbcount);
-            if (!fbc)
-            {
-                printf( "Failed to retrieve a framebuffer config\n" );
+            if (!fbc) {
+                printf("\n\tFailed to retrieve a framebuffer config\n\n");
                 exit(1);
             }
-#ifndef NDEBUG
-            printf( "Found %d matching FB configs.\n", fbcount );
+            printf("Found %d matching FB configs\n", fbcount);
+
             // Pick the FB config/visual with the most samples per pixel
-            printf( "Getting XVisualInfos\n" );
-#endif
+            printf("Getting XVisualInfos\n");
             int best_fbc = -1, worst_fbc = -1, best_num_samp = -1, worst_num_samp = 999;
             int i;
-            for (i=0; i<fbcount; ++i)
-            {
-                XVisualInfo *vi = glXGetVisualFromFBConfig( dpy, fbc[i] );
-                if ( vi )
-                {
+            for (i = 0; i < fbcount; ++i) {
+                XVisualInfo *vi = glXGetVisualFromFBConfig(dpy, fbc[i]);
+                if (vi) {
                     int samp_buf, samples;
-                    glXGetFBConfigAttrib( dpy, fbc[i], GLX_SAMPLE_BUFFERS, &samp_buf );
-                    glXGetFBConfigAttrib( dpy, fbc[i], GLX_SAMPLES       , &samples  );
-#ifndef NDEBUG
-                    printf( "  Matching fbconfig %d, visual ID 0x%2lx: SAMPLE_BUFFERS = %d,"
-                            " SAMPLES = %d\n",
-                            i, vi -> visualid, samp_buf, samples );
-#endif
-                    if ( best_fbc < 0 || (samp_buf && samples > best_num_samp) )
+                    glXGetFBConfigAttrib(dpy, fbc[i], GLX_SAMPLE_BUFFERS, &samp_buf);
+                    glXGetFBConfigAttrib(dpy, fbc[i], GLX_SAMPLES       , &samples );
+                    printf("  Matching fbconfig %d, visual ID 0x%2lx: SAMPLE_BUFFERS = %d,"
+                           " SAMPLES = %d\n",
+                           i, vi -> visualid, samp_buf, samples);
+                    if (best_fbc < 0 || (samp_buf && samples > best_num_samp))
                       best_fbc = i, best_num_samp = samples;
-                    if ( worst_fbc < 0 || !samp_buf || samples < worst_num_samp )
+                    if (worst_fbc < 0 || !samp_buf || samples < worst_num_samp)
                       worst_fbc = i, worst_num_samp = samples;
                 }
-                XFree( vi );
+                XFree(vi);
             }
-            GLXFBConfig bestFbc = fbc[ best_fbc ];
-            // Be sure to free the FBConfig list allocated by glXChooseFBConfig()
-            XFree( fbc );
-            // Get a visual
-            XVisualInfo *vi = glXGetVisualFromFBConfig( dpy, bestFbc );
-#ifndef NDEBUG
-            printf( "Chosen visual ID = 0x%lx\n", vi->visualid );
-#endif
 
+            GLXFBConfig bestFbc = fbc[best_fbc];
+
+            // Free the FBConfig list allocated by glXChooseFBConfig()
+            XFree(fbc);
+
+            // Get a visual
+            XVisualInfo *vi = glXGetVisualFromFBConfig(dpy, bestFbc);
+            printf("Chose visual ID = 0x%lx\n", vi->visualid);
+
+            printf("Creating colormap\n");
             Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
 
             printf("Creating window\n");
@@ -123,8 +111,7 @@ namespace ILLIXR{
             XStoreName(dpy, win, "ILLIXR Extended Window");
             XMapWindow(dpy, win);
 
-            //glc = glXCreateContext(dpy, vi, *ctx, true);
-            // calling glXGetProcAddressARB
+            printf("Creating context\n");
             glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
             glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)
                 glXGetProcAddressARB((const GLubyte *) "glXCreateContextAttribsARB");
@@ -134,6 +121,18 @@ namespace ILLIXR{
                 None
             };
             glc = glXCreateContextAttribsARB(dpy, bestFbc, _shared_gl_context, True, context_attribs);
+
+            // Sync to process errors
+            XSync(dpy, false);
+            glXMakeCurrent(dpy, win, glc);
+
+            int major = 0, minor = 0;
+            glGetIntegerv(GL_MAJOR_VERSION, &major);
+            glGetIntegerv(GL_MINOR_VERSION, &minor);
+            printf("OpenGL context created\nVersion %d.%d\nVendor %s\nRenderer %s\n",
+                major, minor,
+                glGetString(GL_VENDOR),
+                glGetString(GL_RENDERER));
         }
     };
 }
