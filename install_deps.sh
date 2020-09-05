d4ab8bc22b4ca885af83f392e2ce52324f107936
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-09-05 16:40:55 -0500

    Install xvfb

--- install_deps.sh
+++ install_deps.sh
@@ -60,7 +60,7 @@ then
 			libeigen3-dev libboost-all-dev libatlas-base-dev libsuitesparse-dev libblas-dev \
 			glslang-tools libsdl2-dev libglu1-mesa-dev mesa-common-dev freeglut3-dev libglew-dev glew-utils libglfw3-dev \
 			libusb-dev libusb-1.0 libudev-dev libv4l-dev libhidapi-dev \
-			build-essential libx11-xcb-dev libxcb-glx0-dev libxkbcommon-dev libwayland-dev libxrandr-dev \
+			build-essential libx11-xcb-dev libxcb-glx0-dev libxkbcommon-dev libwayland-dev libxrandr-dev xvfb \
 			libgtest-dev pkg-config libgtk2.0-dev wget
 	fi
 
