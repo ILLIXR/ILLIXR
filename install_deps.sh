5701944e802f6a8a7ddf4f1882ad3ccc780b07c5
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-09-11 19:16:41 -0500

    Move EuRoC download to its own script

--- install_deps.sh
+++ install_deps.sh
@@ -88,11 +88,7 @@ then
 	fi
 
 	if [ ! -d "data1" ] && y_or_n "Next: Download Vicon Room 1 Medium SLAM dataset"; then
-		curl -o data.zip \
-		"http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/V1_02_medium.zip" && \
-		unzip data.zip && \
-		rm -rf __MACOSX data1 && \
-		mv mav0 data1
+		. ./scripts/install_euroc.sh
 	fi
 
 	if ! which conda 2> /dev/null; then
