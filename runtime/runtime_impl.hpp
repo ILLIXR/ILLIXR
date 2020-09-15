10961978c80ac13e550d0d1aa3a3ae4216dffb6e
Samuel Grayson <sam@samgrayson.me>
2020-09-14 21:27:57 -0500

    [WIP] Add MTP timer
    Compiles, runs without crash, but the MTP numbers are implausible.

--- runtime/runtime_impl.hpp
+++ runtime/runtime_impl.hpp
@@ -13,7 +13,7 @@ using namespace ILLIXR;
 class runtime_impl : public runtime {
 public:
 	runtime_impl(GLXContext appGLCtx) {
-		pb.register_impl<record_logger>(std::make_shared<noop_record_logger>());
+		pb.register_impl<record_logger>(std::make_shared<stdout_record_logger>());
 		pb.register_impl<gen_guid>(std::make_shared<gen_guid>());
 		pb.register_impl<switchboard>(create_switchboard());
 		pb.register_impl<xlib_gl_extended_window>(std::make_shared<xlib_gl_extended_window>(448*2, 320*2, appGLCtx));
