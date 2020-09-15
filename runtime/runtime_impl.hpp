2222d2d2777928f301a4c1e840a378d23909c212
Samuel Grayson <sam@samgrayson.me>
2020-09-14 19:56:13 -0500

    WIP

--- runtime/runtime_impl.hpp
+++ runtime/runtime_impl.hpp
@@ -4,7 +4,7 @@
 #include "common/extended_window.hpp"
 #include "common/dynamic_lib.hpp"
 #include "common/plugin.hpp"
-#include "switchboard_impl.hpp"
+#include "common/switchboard.hpp"
 #include "stdout_record_logger.hpp"
 #include "noop_record_logger.hpp"
 
@@ -15,7 +15,7 @@ public:
 	runtime_impl(GLXContext appGLCtx) {
 		pb.register_impl<record_logger>(std::make_shared<noop_record_logger>());
 		pb.register_impl<gen_guid>(std::make_shared<gen_guid>());
-		pb.register_impl<switchboard>(create_switchboard());
+		pb.register_impl<switchboard>(std::make_shared<switchboard>(&pb));
 		pb.register_impl<xlib_gl_extended_window>(std::make_shared<xlib_gl_extended_window>(448*2, 320*2, appGLCtx));
 	}
 
