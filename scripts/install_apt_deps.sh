9cc2e85ffed4efc995a2869ee3692bee482f23b4
Samuel Grayson <sam@samgrayson.me>
2020-09-16 14:49:34 -0500

    Add unzip to deps

--- scripts/install_apt_deps.sh
+++ scripts/install_apt_deps.sh
@@ -7,7 +7,7 @@ sudo apt-add-repository -u -y "deb https://apt.kitware.com/ubuntu/ ${UBUNTU_CODE
 sudo add-apt-repository -u -y ppa:graphics-drivers/ppa
 sudo add-apt-repository -u -y ppa:deadsnakes/ppa
 sudo apt-get install -y \
-	 git clang-10 make cmake libc++-dev libc++abi-dev \
+	 git clang-10 make cmake libc++-dev libc++abi-dev unzip \
 	 libsqlite3-dev libeigen3-dev libboost-all-dev libatlas-base-dev libsuitesparse-dev libblas-dev \
 	 glslang-tools libsdl2-dev libglu1-mesa-dev mesa-common-dev freeglut3-dev libglew-dev glew-utils libglfw3-dev \
 	 libusb-dev libusb-1.0 libudev-dev libv4l-dev libhidapi-dev \
