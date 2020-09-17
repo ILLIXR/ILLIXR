27e5cfad913646245a59c42cc93d5e859df69d72
Samuel Grayson <sam@samgrayson.me>
2020-09-16 23:58:34 -0500

    Install EuRoC data in Dockerfile

--- scripts/install_euroc.sh
+++ scripts/install_euroc.sh
@@ -4,4 +4,4 @@ curl -o data.zip \
 "http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/V1_02_medium.zip" && \
 unzip data.zip && \
 rm -rf __MACOSX data1 && \
-mv mav0 ${1:data1/}
+mv mav0 data1/
