859d93a7799ed7e50861a8616488feec29d38219
Finn Sinclair <finnnorth@gmail.com>
2020-09-29 22:07:40 -0500

    Removing openwarp-specific code from timewarp

--- timewarp_gl/plugin.cpp
+++ timewarp_gl/plugin.cpp
@@ -104,14 +104,8 @@ private:
 	// Eye sampler array
 	GLuint eye_sampler;
 
-	// Depth sampler
-	GLuint depth_sampler;
-
 	// Eye index uniform
 	GLuint tw_eye_index_unif;
-
-	GLuint u_near_clip;
-	GLuint u_far_clip;
 
 	// VAOs
 	GLuint tw_vao;
@@ -121,7 +115,6 @@ private:
 	GLuint distortion_uv0_attr;
 	GLuint distortion_uv1_attr;
 	GLuint distortion_uv2_attr;
-	GLuint flat_uv_attr;
 
 	// Distortion mesh information
 	GLuint num_distortion_vertices;
@@ -139,18 +132,10 @@ private:
 	HMD::uv_coord_t* distortion_uv2;
 	GLuint distortion_uv2_vbo;
 
-	HMD::uv_coord_t* flat_uv;
-	GLuint flat_uv_vbo;
-
 	// Handles to the start and end timewarp
 	// transform matrices (3x4 uniforms)
 	GLuint tw_start_transform_unif;
 	GLuint tw_end_transform_unif;
-
-	GLuint u_render_inverse_p;
-	GLuint u_render_inverse_v;
-
-	GLuint u_warp_vp;
 
 	// Basic perspective projection matrix
 	Eigen::Matrix4f basicProjection;
@@ -206,7 +191,6 @@ private:
 			distortion_uv0 = (HMD::uv_coord_t *) malloc(HMD::NUM_EYES * num_distortion_vertices * sizeof(HMD::uv_coord_t));
 			distortion_uv1 = (HMD::uv_coord_t *) malloc(HMD::NUM_EYES * num_distortion_vertices * sizeof(HMD::uv_coord_t));
 			distortion_uv2 = (HMD::uv_coord_t *) malloc(HMD::NUM_EYES * num_distortion_vertices * sizeof(HMD::uv_coord_t));
-			flat_uv = 		 (HMD::uv_coord_t *) malloc(HMD::NUM_EYES * num_distortion_vertices * sizeof(HMD::uv_coord_t));
 		}
 
 		for ( int eye = 0; eye < HMD::NUM_EYES; eye++ )
@@ -223,10 +207,6 @@ private:
 					distortion_positions[eye * num_distortion_vertices + index].y = ( -1.0f + 2.0f * ( ( hmdInfo->eyeTilesHigh - (float)y ) / hmdInfo->eyeTilesHigh ) *
 														( (float)( hmdInfo->eyeTilesHigh * hmdInfo->tilePixelsHigh ) / hmdInfo->displayPixelsHigh ) );
 					distortion_positions[eye * num_distortion_vertices + index].z = 0.0f;
-
-					flat_uv[eye * num_distortion_vertices + index].u = ( (float)x / hmdInfo->eyeTilesWide );
-					flat_uv[eye * num_distortion_vertices + index].v = ( ( ( hmdInfo->eyeTilesHigh - (float)y ) / hmdInfo->eyeTilesHigh ) *
-														( (float)( hmdInfo->eyeTilesHigh * hmdInfo->tilePixelsHigh ) / hmdInfo->displayPixelsHigh ) );
 
 					// Use the previously-calculated distort_coords to set the UVs on the distortion mesh
 					distortion_uv0[eye * num_distortion_vertices + index].u = distort_coords[eye][0][index].x;
@@ -351,11 +331,10 @@ public:
 		glGenVertexArrays(1, &tw_vao);
     	glBindVertexArray(tw_vao);
 
-    	timewarpShaderProgram = init_and_link(meshWarpVertexProgramGLSL, meshWarpFragmentProgramGLSL);
+    	timewarpShaderProgram = init_and_link(timeWarpChromaticVertexProgramGLSL, timeWarpChromaticFragmentProgramGLSL);
 		// Acquire attribute and uniform locations from the compiled and linked shader program
 
     	distortion_pos_attr = glGetAttribLocation(timewarpShaderProgram, "vertexPosition");
-		flat_uv_attr = glGetAttribLocation(timewarpShaderProgram, "flatUv");
     	distortion_uv0_attr = glGetAttribLocation(timewarpShaderProgram, "vertexUv0");
     	distortion_uv1_attr = glGetAttribLocation(timewarpShaderProgram, "vertexUv1");
     	distortion_uv2_attr = glGetAttribLocation(timewarpShaderProgram, "vertexUv2");
@@ -364,20 +343,9 @@ public:
     	tw_eye_index_unif = glGetUniformLocation(timewarpShaderProgram, "ArrayLayer");
     	eye_sampler = glGetUniformLocation(timewarpShaderProgram, "Texture");
 
-		u_near_clip = glGetUniformLocation(timewarpShaderProgram, "u_NearClip");
-		u_far_clip = glGetUniformLocation(timewarpShaderProgram, "u_FarClip");
-
-		u_render_inverse_p = glGetUniformLocation(timewarpShaderProgram, "u_renderInverseP");
-		u_render_inverse_v = glGetUniformLocation(timewarpShaderProgram, "u_renderInverseV");
-
-		u_warp_vp = glGetUniformLocation(timewarpShaderProgram, "u_warpVP");
-
-		depth_sampler = glGetUniformLocation(timewarpShaderProgram, "_Depth");
-
 		if(glGetError())
 		{
-			printf("uniforms/attribs error\n");
-			// std::cout << "attr: " << distortion_uv2_attr << std::endl;
+			std::cerr << "[TIMEWARP] Uniforms/attribs error" << std::endl;
 			abort();
 		}
 		
@@ -387,21 +355,6 @@ public:
 			glBindBuffer(GL_ARRAY_BUFFER, distortion_positions_vbo);
 			glBufferData(GL_ARRAY_BUFFER, HMD::NUM_EYES * (num_distortion_vertices * 3) * sizeof(GLfloat), distortion_positions, GL_STATIC_DRAW);
 			glVertexAttribPointer(distortion_pos_attr, 3, GL_FLOAT, GL_FALSE, 0, 0);
-		}
-
-		if(glGetError())
-		{
-			printf("distort pos error\n");
-			// std::cout << "attr: " << distortion_uv2_attr << std::endl;
-			abort();
-		}
-		
-		if(flat_uv_vbo != -1) {
-			// Config flat_uv_vbo
-			glGenBuffers(1, &flat_uv_vbo);
-			glBindBuffer(GL_ARRAY_BUFFER, flat_uv_vbo);
-			glBufferData(GL_ARRAY_BUFFER, HMD::NUM_EYES * (num_distortion_vertices * 2) * sizeof(GLfloat), flat_uv, GL_STATIC_DRAW);
-			glVertexAttribPointer(flat_uv_vbo, 2, GL_FLOAT, GL_FALSE, 0, 0);
 		}
 
 		if(distortion_uv0_attr != -1) {
@@ -428,41 +381,19 @@ public:
 			glVertexAttribPointer(distortion_uv2_attr, 2, GL_FLOAT, GL_FALSE, 0, 0);
 		}
 
-		if(glGetError())
-		{
-			printf("VBO error\n");
-			// std::cout << "attr: " << distortion_uv2_attr << std::endl;
-			abort();
-		}
-
 		// Config distortion mesh indices vbo
 		glGenBuffers(1, &distortion_indices_vbo);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, distortion_indices_vbo);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, num_distortion_indices * sizeof(GLuint), distortion_indices, GL_STATIC_DRAW);
 
-		
+
+		if(glGetError())
+		{
+			std::cerr << "[TIMEWARP] Error configuring VBOs" << std::endl;
+			abort();
+		}
 
 		glXMakeCurrent(xwin->dpy, None, NULL);
-	}
-
-	Eigen::Matrix4f getEyeballMatrix(int eye, const Eigen::Vector3f& head_pos, const Eigen::Quaternionf& head_rotation, float ipd) {
-		// Offset of eyeball from pose
-		auto eyeball = Eigen::Vector3f((eye == 0 ? -ipd/2.0f : ipd/2.0f), 0, 0);
-
-		auto head_rotation_matrix = head_rotation.toRotationMatrix();
-
-		// Apply head rotation to eyeball offset vector
-		eyeball = head_rotation_matrix * eyeball;
-
-		// Apply head position to eyeball
-		eyeball += head_pos;
-
-		// Build our eye matrix from the pose's position + orientation.
-		Eigen::Matrix4f eye_matrix = Eigen::Matrix4f::Identity();
-		eye_matrix.block<3,1>(0,3) = eyeball; // Set position to eyeball's position
-		eye_matrix.block<3,3>(0,0) = head_rotation_matrix;
-
-		return eye_matrix;
 	}
 
 	virtual void warp([[maybe_unused]] float time) {
@@ -526,27 +457,7 @@ public:
 			glUniformMatrix4fv(tw_end_transform_unif, 1, GL_FALSE,  (GLfloat*)(timeWarpEndTransform4x4.data()));
 
 		glUniform1i(eye_sampler, 0);
-		glUniform1i(depth_sampler, 1);
 
-		glUniform1f(u_near_clip, 0.03f);
-		glUniform1f(u_far_clip, 20.0f);
-
-		glUniformMatrix4fv(u_render_inverse_p, 1, GL_FALSE, (GLfloat*)(most_recent_frame->render_projection.inverse().eval().data()));
-
-		Eigen::Matrix4f renderedInverseViewMatrices[2];
-		Eigen::Matrix4f warpInverseViewMatrices[2];
-
-		for(int eye_idx = 0; eye_idx < HMD::NUM_EYES; eye_idx++){
-			// renderedInverseViewMatrices[eye_idx] = getEyeballMatrix(eye_idx, most_recent_frame->render_pose.pose.position,
-			// 												most_recent_frame->render_pose.pose.orientation,
-			// 												body_info.interpupillaryDistance);
-
-			renderedInverseViewMatrices[eye_idx] = most_recent_frame->view_matrices[eye_idx].inverse();
-			
-			warpInverseViewMatrices[eye_idx] = getEyeballMatrix(eye_idx, latest_pose.pose.position,
-															latest_pose.pose.orientation,
-															body_info.interpupillaryDistance);
-		}
 		
 
 
@@ -559,33 +470,11 @@ public:
 		glGenQueries(1, &query);
 		glBeginQuery(GL_TIME_ELAPSED, query);
 
-		if(glGetError())
-		{
-			printf("Error before eye loop\n");
-			// std::cout << "attr: " << distortion_uv2_attr << std::endl;
-			abort();
-		}
-
 		// Loop over each eye.
 		for(int eye = 0; eye < HMD::NUM_EYES; eye++){
 			
 			glActiveTexture(GL_TEXTURE0);
 			glBindTexture(GL_TEXTURE_2D, most_recent_frame->texture_handles[eye]);
-
-			glActiveTexture(GL_TEXTURE1);
-			glBindTexture(GL_TEXTURE_2D, most_recent_frame->depth_handles[eye]);
-
-			if(glGetError())
-			{
-				printf("Error after binding textures\n");
-				// std::cout << "attr: " << distortion_uv2_attr << std::endl;
-				abort();
-			}
-
-			glUniformMatrix4fv(u_render_inverse_v, 1, GL_FALSE, (GLfloat*)(renderedInverseViewMatrices[eye].data()));
-			
-			auto vp = most_recent_frame->render_projection * warpInverseViewMatrices[0].inverse();
-			glUniformMatrix4fv(u_warp_vp, 1, GL_FALSE, (GLfloat*)(vp.eval().data()));
 
 			// The distortion_positions_vbo GPU buffer already contains
 			// the distortion mesh for both eyes! They are contiguously
@@ -621,18 +510,11 @@ public:
 				glEnableVertexAttribArray(distortion_uv2_attr);
 			}
 
-			// We do the exact same thing for the UV GPU memory.
-			glBindBuffer(GL_ARRAY_BUFFER, flat_uv_vbo);
-			glVertexAttribPointer(flat_uv_attr, 2, GL_FLOAT, GL_FALSE, 0, (void*)(eye * num_distortion_vertices * sizeof(HMD::mesh_coord2d_t)));
-			glEnableVertexAttribArray(flat_uv_attr);
-
 			if(glGetError())
 			{
-				printf("Error after setting buffers\n");
-				// std::cout << "attr: " << distortion_uv2_attr << std::endl;
+				std::cerr << "[TIMEWARP] Error after binding attributes." << std::endl;
 				abort();
 			}
-
 
 			// Interestingly, the element index buffer is identical for both eyes, and is
 			// reused for both eyes. Therefore glDrawElements can be immediately called,
