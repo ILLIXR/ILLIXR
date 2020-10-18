1f61fe5e817154a11e9ec99ad06a93d39c8072f3
Hyjale <saishidual@gmail.com>
2020-10-17 22:14:13 -0500

    restructure code && images are appearing in debugview

--- realsense/plugin.cpp
+++ realsense/plugin.cpp
@@ -1,82 +1,135 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <opencv2/opencv.hpp>   // Include OpenCV API
+#include <mutex>
 
 // ILLIXR includes
 #include "common/threadloop.hpp"
 #include "common/switchboard.hpp"
 #include "common/data_format.hpp"
 
-using namespace cv;
 using namespace ILLIXR;
 
 typedef struct {
     cv::Mat* img0;
     cv::Mat* img1;
-    std::size_t serial_no;
+	// std::size_t serial_no;
+		int iteration;
 } cam_type;
 
-class rs_camera_thread : public threadloop {
-public:
-	rs_camera_thread(std::string name_, phonebook *pb_, std::shared_ptr<rs2::pipeline> pipe_)
-	: threadloop{name_, pb_}
-	, sb{pb->lookup_impl<switchboard>()}
-	, _m_cam_type{sb->publish<cam_type>("cam_type")}
-	, pipe{pipe_}
-	{}
+typedef struct {
+	rs2_vector* accel_data;
+	// std::size_t serial_no;
+	int iteration;
+} accel_type;
 
-protected:
-	virtual skip_option _p_should_skip() override {
-		return skip_option::run;
-	}
-
-	virtual void _p_one_iteration() override {
-// 		std::cout << "Entered _p_one_iteration_CAM" << std::endl;
-		if (!pipe) return;
-		rs2::frameset frameset = pipe->wait_for_frames();
-		rs2::video_frame ir_frame_left = frameset.get_infrared_frame(1);
-		rs2::video_frame ir_frame_right = frameset.get_infrared_frame(2);
-		cv::Mat dMat_left = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_left.get_data());
-		cv::Mat dMat_right = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_right.get_data());
-
-		_m_cam_type->put(new cam_type{
-				new cv::Mat{dMat_left},
-				new cv::Mat{dMat_right},
-				iteration_no,
-		});
-	}
-
-private:
-	const std::shared_ptr<switchboard> sb;
-	std::unique_ptr<writer<cam_type>> _m_cam_type;
-
-	std::shared_ptr<rs2::pipeline> pipe;
-};
-
-class rs_imu_thread : public threadloop {
+class rs_imu_thread : public plugin {
 public:
 	rs_imu_thread(std::string name_, phonebook *pb_)
-	: threadloop{name_, pb_}
+	: plugin{name_, pb_}
 	, sb{pb->lookup_impl<switchboard>()}
 	, _m_imu_cam{sb->publish<imu_cam_type>("imu_cam")}
-	, camera_thread_{"rs_camera_thread", pb_, pipe}
-	, _m_cam_type{sb->subscribe_latest<cam_type>("cam_type")}
 	{
 		pipe = std::make_shared<rs2::pipeline>();
-		cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
-		cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
-		// cfg.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
-		// cfg.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
-		pipe->start(cfg);
-		camera_thread_.start();
+		auto callback = [&](const rs2::frame& frame)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+
+				if (auto fs = frame.as<rs2::frameset>()) {
+					// std::cout << "IMAGE" << std::endl;
+					rs2::video_frame ir_frame_left = fs.get_infrared_frame(1);
+					rs2::video_frame ir_frame_right = fs.get_infrared_frame(2);
+					cv::Mat dMat_left = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_left.get_data());
+					cv::Mat dMat_right = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_right.get_data());
+					cam_type_.img0 = new cv::Mat{dMat_left};
+					cam_type_.img1 = new cv::Mat{dMat_right};
+					cam_type_.iteration = count;
+					count++;
+
+					// cv::namedWindow("Pipeline Output");
+					// cv::imshow("Pipeline Output", dMat_left);
+					// cv::waitKey(1);
+				}
+
+				else if (auto fa = frame.as<rs2::motion_frame>()) {
+					std::string s = fa.get_profile().stream_name();
+
+					if (s == "Accel") {
+						// std::cout << "ACCEL" << std::endl;
+						rs2::motion_frame accel = fa;
+						accel_data = accel.get_motion_data();
+						accel_type_.accel_data = &accel_data;
+						accel_type_.iteration = count2;
+						count2++;
+					}
+
+					if (s == "Gyro") {
+						rs2_vector *accel;
+						if (lastCount2 != accel_type_.iteration) {
+							lastCount2 = accel_type_.iteration;
+							accel = accel_type_.accel_data;
+						} else return;
+						// std::cout << "GYRO" << std::endl;
+
+						rs2::motion_frame gyro = fa;
+						ts = gyro.get_timestamp();
+						gyro_data = gyro.get_motion_data();
+
+						la = {accel->x, accel->y, accel->z};
+						av = {gyro_data.x, gyro_data.y, gyro_data.z};
+
+						// Time as time_point
+						imu_time = static_cast<ullong>(ts / 1000000);
+						std::cout << imu_time << std::endl;
+
+						// Time as time_point
+						using time_point = std::chrono::system_clock::time_point;
+						time_point uptime_timepoint{std::chrono::duration_cast<time_point::duration>(std::chrono::nanoseconds(imu_time))};
+						std::time_t time2 = std::chrono::system_clock::to_time_t(uptime_timepoint);
+						t = std::chrono::system_clock::from_time_t(time2);
+
+						// check for new frames
+						std::optional<cv::Mat*> img0 = std::nullopt; // has to have type std::nullopt
+						std::optional<cv::Mat*> img1 = std::nullopt;
+						// cv::Mat* img0 = nullptr;
+						//cv::Mat* img1 = nullptr;
+						if (lastCount != cam_type_.iteration) {
+							lastCount = cam_type_.iteration;
+							img0 = cam_type_.img0;
+							img1 = cam_type_.img1;
+							std::cout << "New Image" << std::endl;
+							// cv::namedWindow("Pipeline Output");
+							// cv::imshow("Pipeline Output", *img0);
+							// cv::waitKey(1);
+						}
+
+						// Submit to switchboard
+						_m_imu_cam->put(new imu_cam_type {
+								t,
+								av,
+								la,
+								img0,
+								img1,
+								imu_time,
+						});
+						// std::cout << img0 << std::endl;
+					}
+				}
+    };
+
+		cfg.disable_all_streams();
+		cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F, 250); // able to set to 0, 63 (default), 250
+		cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F, 400); // able to set to 0, 200 (default), 400
+		cfg.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
+		cfg.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
+		profiles = pipe->start(cfg, callback);
 	}
 
 	virtual void start() override {
-		threadloop::start();
+		plugin::start();
 	}
 
 	virtual void stop() override {
-		camera_thread_.stop();
-		threadloop::stop();
+		plugin::stop();
 	}
 
 	virtual ~rs_imu_thread() override {
@@ -84,71 +137,18 @@ public:
 	}
 
 protected:
-	virtual skip_option _p_should_skip() override {
-		return skip_option::run;
-	}
+	// virtual skip_option _p_should_skip() override {
+	// 	return skip_option::run;
+	// }
 
-	virtual void _p_one_iteration() override {
-		// std::cout << "Entered _p_one_iteration_IMU" << std::endl;
-		rs2::frameset frames;
-		if(pipe->poll_for_frames(&frames)) {
-			auto fa = frames.first(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
-			rs2::motion_frame accel = fa.as<rs2::motion_frame>();
-			auto fg = frames.first(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
-			rs2::motion_frame gyro = fg.as<rs2::motion_frame>();
-
-			if (gyro && accel) {
-				std::cout << "gyro and accel exists" << std::endl;
-				// Get the timestamp of the current frame
-				ts = gyro.get_timestamp();
-				imu_time = static_cast<ullong>(ts / 1000000);
-
-				// Time as time_point
-        using time_point = std::chrono::system_clock::time_point;
-        time_point uptime_timepoint{std::chrono::duration_cast<time_point::duration>(std::chrono::nanoseconds(imu_time))};
-        std::time_t time2 = std::chrono::system_clock::to_time_t(uptime_timepoint);
-        t = std::chrono::system_clock::from_time_t(time2);
-
-				// Get gyro & accel measures
-				gyro_data = gyro.get_motion_data();
-				accel_data = accel.get_motion_data();
-
-				la = {accel_data.x, accel_data.y, accel_data.z};
-				av = {gyro_data.x, gyro_data.y, gyro_data.z};
-				std::cout << accel_data.x << std::endl;
-
-				std::optional<cv::Mat*> img0 = std::nullopt;
-				std::optional<cv::Mat*> img1 = std::nullopt;
-				const cam_type* c = _m_cam_type->get_latest_ro();
-        if (c && c->serial_no != last_serial_no) {
-            last_serial_no = c->serial_no;
-            img0 = c->img0;
-            img1 = c->img1;
-        }
-
-				_m_imu_cam->put(new imu_cam_type {
-            t,
-            av,
-            la,
-            img0,
-            img1,
-            imu_time,
-        });
-			}
-		}
-	}
+	// virtual void _p_one_iteration() override { }
 
 private:
-	rs_camera_thread camera_thread_;
 	const std::shared_ptr<switchboard> sb;
 	std::unique_ptr<writer<imu_cam_type>> _m_imu_cam;
-	std::unique_ptr<reader_latest<cam_type>> _m_cam_type;
 
 	std::shared_ptr<rs2::pipeline> pipe;
-	// rs2::pipeline pipe;
-	// auto pipe;
 	rs2::config cfg;
-	rs2::frame frame;
 	rs2_vector gyro_data;
 	rs2_vector accel_data;
 	double ts;
@@ -160,7 +160,17 @@ private:
 	time_type t;
 	ullong imu_time;
 
+	std::mutex mutex;
+	rs2::pipeline_profile profiles;
+
+	cam_type cam_type_;
 	std::size_t last_serial_no {0};
+	accel_type accel_type_;
+	std::size_t last_serial_no2 {0};
+	int count = 0;
+	int count2 = 0;
+	int lastCount;
+	int lastCount2;
 };
 
 PLUGIN_MAIN(rs_imu_thread);
