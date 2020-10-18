0b1dd606cf21dd7c10169ccda81f1fb85f96e403
Hyjale <saishidual@gmail.com>
2020-10-18 00:21:47 -0500

    realsense running with ILLIXR system, needs calibration

--- realsense/plugin.cpp
+++ realsense/plugin.cpp
@@ -10,76 +10,63 @@
 using namespace ILLIXR;
 
 typedef struct {
-    cv::Mat* img0;
-    cv::Mat* img1;
-	// std::size_t serial_no;
-		int iteration;
+	cv::Mat* img0;
+	cv::Mat* img1;
+	int iteration;
 } cam_type;
 
 typedef struct {
 	rs2_vector* accel_data;
-	// std::size_t serial_no;
 	int iteration;
 } accel_type;
 
-class rs_imu_thread : public plugin {
+class realsense : public plugin {
 public:
-	rs_imu_thread(std::string name_, phonebook *pb_)
+	realsense(std::string name_, phonebook *pb_)
 	: plugin{name_, pb_}
 	, sb{pb->lookup_impl<switchboard>()}
 	, _m_imu_cam{sb->publish<imu_cam_type>("imu_cam")}
 	{
-		pipe = std::make_shared<rs2::pipeline>();
 		auto callback = [&](const rs2::frame& frame)
     {
         std::lock_guard<std::mutex> lock(mutex);
 
 				if (auto fs = frame.as<rs2::frameset>()) {
-					// std::cout << "IMAGE" << std::endl;
 					rs2::video_frame ir_frame_left = fs.get_infrared_frame(1);
 					rs2::video_frame ir_frame_right = fs.get_infrared_frame(2);
 					cv::Mat dMat_left = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_left.get_data());
 					cv::Mat dMat_right = cv::Mat(cv::Size(640, 480), CV_8UC1, (void*)ir_frame_right.get_data());
 					cam_type_.img0 = new cv::Mat{dMat_left};
 					cam_type_.img1 = new cv::Mat{dMat_right};
-					cam_type_.iteration = count;
-					count++;
-
-					// cv::namedWindow("Pipeline Output");
-					// cv::imshow("Pipeline Output", dMat_left);
-					// cv::waitKey(1);
+					cam_type_.iteration = iteration_cam;
+					iteration_cam++;
 				}
 
-				else if (auto fa = frame.as<rs2::motion_frame>()) {
-					std::string s = fa.get_profile().stream_name();
+				if (auto mf = frame.as<rs2::motion_frame>()) {
+					std::string s = mf.get_profile().stream_name();
 
 					if (s == "Accel") {
-						// std::cout << "ACCEL" << std::endl;
-						rs2::motion_frame accel = fa;
+						rs2::motion_frame accel = mf;
 						accel_data = accel.get_motion_data();
 						accel_type_.accel_data = &accel_data;
-						accel_type_.iteration = count2;
-						count2++;
+						accel_type_.iteration = iteration_accel;
+						iteration_accel++;
 					}
 
 					if (s == "Gyro") {
-						rs2_vector *accel;
-						if (lastCount2 != accel_type_.iteration) {
-							lastCount2 = accel_type_.iteration;
-							accel = accel_type_.accel_data;
-						} else return;
-						// std::cout << "GYRO" << std::endl;
-
-						rs2::motion_frame gyro = fa;
+						if (last_iteration_accel == accel_type_.iteration) { return; }
+						last_iteration_accel = accel_type_.iteration;
+						rs2_vector accel = *accel_type_.accel_data;
+						rs2::motion_frame gyro = mf;
 						ts = gyro.get_timestamp();
 						gyro_data = gyro.get_motion_data();
 
-						la = {accel->x, accel->y, accel->z};
+						// IMU data
+						la = {accel.x, accel.y, accel.z};
 						av = {gyro_data.x, gyro_data.y, gyro_data.z};
 
-						// Time as time_point
-						imu_time = static_cast<ullong>(ts / 1000000);
-						std::cout << imu_time << std::endl;
+						// Time as ullong (nanoseconds)
+						imu_time = static_cast<ullong>(ts * 1000000);
 
 						// Time as time_point
 						using time_point = std::chrono::system_clock::time_point;
@@ -87,19 +74,13 @@ public:
 						std::time_t time2 = std::chrono::system_clock::to_time_t(uptime_timepoint);
 						t = std::chrono::system_clock::from_time_t(time2);
 
-						// check for new frames
-						std::optional<cv::Mat*> img0 = std::nullopt; // has to have type std::nullopt
+						// Images
+						std::optional<cv::Mat*> img0 = std::nullopt;
 						std::optional<cv::Mat*> img1 = std::nullopt;
-						// cv::Mat* img0 = nullptr;
-						//cv::Mat* img1 = nullptr;
-						if (lastCount != cam_type_.iteration) {
-							lastCount = cam_type_.iteration;
+						if (last_iteration_cam != cam_type_.iteration) {
+							last_iteration_cam = cam_type_.iteration;
 							img0 = cam_type_.img0;
 							img1 = cam_type_.img1;
-							std::cout << "New Image" << std::endl;
-							// cv::namedWindow("Pipeline Output");
-							// cv::imshow("Pipeline Output", *img0);
-							// cv::waitKey(1);
 						}
 
 						// Submit to switchboard
@@ -111,43 +92,31 @@ public:
 								img1,
 								imu_time,
 						});
-						// std::cout << img0 << std::endl;
 					}
 				}
     };
 
 		cfg.disable_all_streams();
-		cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F, 250); // able to set to 0, 63 (default), 250
-		cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F, 400); // able to set to 0, 200 (default), 400
+		cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F, 250); // adjustable to 0, 63 (default), 250 hz
+		cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F, 400); // adjustable set to 0, 200 (default), 400 hz
 		cfg.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
 		cfg.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
-		profiles = pipe->start(cfg, callback);
+		profiles = pipe.start(cfg, callback);
 	}
 
-	virtual void start() override {
-		plugin::start();
-	}
+	virtual void start() override { plugin::start(); }
 
-	virtual void stop() override {
-		plugin::stop();
-	}
+	virtual void stop() override { plugin::stop(); }
 
-	virtual ~rs_imu_thread() override {
-		pipe->stop();
-	}
-
-protected:
-	// virtual skip_option _p_should_skip() override {
-	// 	return skip_option::run;
-	// }
-
-	// virtual void _p_one_iteration() override { }
+	virtual ~realsense() override { pipe.stop(); }
 
 private:
 	const std::shared_ptr<switchboard> sb;
 	std::unique_ptr<writer<imu_cam_type>> _m_imu_cam;
 
-	std::shared_ptr<rs2::pipeline> pipe;
+	std::mutex mutex;
+	rs2::pipeline_profile profiles;
+	rs2::pipeline pipe;
 	rs2::config cfg;
 	rs2_vector gyro_data;
 	rs2_vector accel_data;
@@ -160,19 +129,15 @@ private:
 	time_type t;
 	ullong imu_time;
 
-	std::mutex mutex;
-	rs2::pipeline_profile profiles;
-
 	cam_type cam_type_;
-	std::size_t last_serial_no {0};
 	accel_type accel_type_;
+	int iteration_cam = 0;
+	int iteration_accel = 0;
+	int last_iteration_cam;
+	int last_iteration_accel;
+	std::size_t last_serial_no {0};
 	std::size_t last_serial_no2 {0};
-	int count = 0;
-	int count2 = 0;
-	int lastCount;
-	int lastCount2;
 };
 
-PLUGIN_MAIN(rs_imu_thread);
+PLUGIN_MAIN(realsense);
 
-int main(int argc, char **argv) { return 0; }
