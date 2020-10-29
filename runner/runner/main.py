903f5cf4fff2d6baf6ed3e09f7476d319ead247a
Samuel Grayson <sam@samgrayson.me>
2020-10-29 03:26:50 -0500

    Fix

--- runner/runner/main.py
+++ runner/runner/main.py
@@ -134,8 +134,9 @@ def load_native(config: Mapping[str, Any]) -> None:
 
 def load_tests(config: Mapping[str, Any]) -> None:
     runtime_exe_path = (build_runtime(config, "exe", test=True),)
+    data_path = pathify(config["data"], root_dir, cache_path, True, True)
+    demo_data_path = pathify(config["demo_data"], root_dir, cache_path, True, True)
     make(Path("common"), ["tests/run"])
-
     plugin_paths = threading_map(
         lambda plugin_config: build_one_plugin(config, plugin_config, test=True),
         [
