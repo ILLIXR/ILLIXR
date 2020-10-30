11cd014bb3ceb5ff3194b5ce76b95908a5089e85
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-10-29 19:47:25 -0500

    Reverting back to basic gtsam install
    This reverts commit 9c0bdbe4e4073e6f6d93f20de6d7ab849f2eba44.

--- scripts/install_gtsam.sh
+++ scripts/install_gtsam.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-git clone --branch master https://github.com/borglab/gtsam.git "${temp_dir}/gtsam"
-cmake \
-	-S "${temp_dir}/gtsam" \
-	-B "${temp_dir}/gtsam/build" \
-	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D CMAKE_BUILD_TYPE=RelWithDebInfo \
-	-D GTSAM_USE_SYSTEM_EIGEN=OFF \
-	-D GTSAM_POSE3_EXPMAP=ON \
-	-D GTSAM_ROT3_EXPMAP=ON
+git clone -b master https://github.com/borglab/gtsam.git "${temp_dir}/gtsam"
+cmake -S "${temp_dir}/gtsam" -B "${temp_dir}/gtsam/build"
 sudo make -C "${temp_dir}/gtsam/build" "-j$(nproc)" install
