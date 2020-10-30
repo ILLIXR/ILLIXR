eb2c9b4c0b59cba1e2a604d6417a43cec1a205ce
Muhammad Huzaifa <huzaifa2@illinois.edu>
2020-10-29 20:34:21 -0500

    Use dev branch of gtsam

--- scripts/install_gtsam.sh
+++ scripts/install_gtsam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone -b master https://github.com/borglab/gtsam.git "${temp_dir}/gtsam"
+git clone https://github.com/borglab/gtsam.git "${temp_dir}/gtsam"
 cmake -S "${temp_dir}/gtsam" -B "${temp_dir}/gtsam/build"
 sudo make -C "${temp_dir}/gtsam/build" "-j$(nproc)" install
