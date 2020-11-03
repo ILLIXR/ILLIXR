a33ef84ba7108bf3d36194497eec010d28acb4be
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2020-11-02 20:16:32 -0500

    Remove OV deps

--- imu_integrator/plugin.cpp
+++ imu_integrator/plugin.cpp
@@ -6,15 +6,18 @@
 #include <thread>
 #include <eigen3/Eigen/Dense>
 
-#include "../open_vins/ov_msckf/src/state/Propagator.h"
-#include "../open_vins/ov_core/src/types/IMU.h"
-
 #include "common/plugin.hpp"
 #include "common/switchboard.hpp"
 #include "common/data_format.hpp"
 #include "common/threadloop.hpp"
 
 using namespace ILLIXR;
+
+typedef struct {
+	double timestamp;
+	Eigen::Matrix<double, 3, 1> wm;
+	Eigen::Matrix<double, 3, 1> am;
+} imu_type;
 
 #define IMU_SAMPLE_LIFETIME 5
 
@@ -52,7 +55,7 @@ public:
 		const imu_cam_type *datum = _m_imu_cam->get_latest_ro();
 		double timestamp_in_seconds = (double(datum->dataset_time) / NANO_SEC);
 
-		ov_msckf::Propagator::IMUDATA data;
+		imu_type data;
         data.timestamp = timestamp_in_seconds;
         data.wm = (datum->angular_v).cast<double>();
         data.am = (datum->linear_a).cast<double>();
@@ -72,7 +75,7 @@ private:
 
 	// IMU Biases
 	std::unique_ptr<writer<imu_raw_type>> _m_imu_raw;
-	std::vector<ov_msckf::Propagator::IMUDATA> _imu_vec;
+	std::vector<imu_type> _imu_vec;
 	double last_imu_offset;
 	bool has_last_offset = false;
 	int64_t _seq_expect, _stat_processed, _stat_missed;
@@ -113,7 +116,6 @@ private:
 		imu_value.block(7, 0, 3, 1) = input_values->velocity;
 		imu_value.block(10,0, 3, 1) = input_values->biasGyro;
 		imu_value.block(13,0, 3, 1) = input_values->biasAcc;
-		Eigen::Matrix<double, 16, 1> imu_fej = imu_value;
 
 		// Uncomment this for some helpful prints
 		// total_imu++;
@@ -133,11 +135,7 @@ private:
 		double time0 = input_values->last_cam_integration_time + last_imu_offset;
 		double time1 = timestamp + t_off_new;
 
-		vector<ov_msckf::Propagator::IMUDATA> prop_data = select_imu_readings(_imu_vec, time0, time1);
-		ov_type::IMU *temp_imu = new ov_type::IMU();
-		temp_imu->set_value(imu_value);
-		temp_imu->set_fej(imu_fej);
-
+		std::vector<imu_type> prop_data = select_imu_readings(_imu_vec, time0, time1);
 		Eigen::Matrix<double,3,1> w_hat;
 		Eigen::Matrix<double,3,1> a_hat;
 		Eigen::Matrix<double,3,1> w_hat2;
@@ -152,33 +150,29 @@ private:
 				double dt = prop_data.at(i+1).timestamp-prop_data.at(i).timestamp;
 
 				// Corrected imu measurements
-				w_hat = prop_data.at(i).wm - temp_imu->bias_g();
-				a_hat = prop_data.at(i).am - temp_imu->bias_a();
-				w_hat2 = prop_data.at(i+1).wm - temp_imu->bias_g();
-				a_hat2 = prop_data.at(i+1).am - temp_imu->bias_a();
+				w_hat = prop_data.at(i).wm - w_hat;
+				a_hat = prop_data.at(i).am - a_hat;
+				w_hat2 = prop_data.at(i+1).wm - w_hat;
+				a_hat2 = prop_data.at(i+1).am - a_hat;
 
 				// Compute the new state mean value
 				Eigen::Vector4d new_q;
 				Eigen::Vector3d new_v, new_p;
-				predict_mean_rk4(temp_imu->quat(), temp_imu->pos(), temp_imu->vel(), input_values->params.n_gravity, dt, w_hat, a_hat, w_hat2, a_hat2, new_q, new_v, new_p);
+				predict_mean_rk4(imu_value.block(0,0,4,1), imu_value.block(4,0,3,1), imu_value.block(7,0,3,1), input_values->params.n_gravity, dt, w_hat, a_hat, w_hat2, a_hat2, new_q, new_v, new_p);
 
-				//Now replace imu estimate and fej with propagated values
-				Eigen::Matrix<double,16,1> imu_x = temp_imu->value();
-				imu_x.block(0,0,4,1) = new_q;
-				imu_x.block(4,0,3,1) = new_p;
-				imu_x.block(7,0,3,1) = new_v;
-				temp_imu->set_value(imu_x);
-				temp_imu->set_fej(imu_x);
+				imu_value.block(0,0,4,1) = new_q;
+				imu_value.block(4,0,3,1) = new_p;
+				imu_value.block(7,0,3,1) = new_v;
 			}
 		}
 
 		Eigen::Matrix<double,13,1> state_plus = Eigen::Matrix<double,13,1>::Zero();
-		state_plus.block(0,0,4,1) = temp_imu->quat();
-		state_plus.block(4,0,3,1) = temp_imu->pos();
-		state_plus.block(7,0,3,1) = temp_imu->vel();
+		state_plus.block(0,0,4,1) = imu_value.block(0,0,4,1);
+		state_plus.block(4,0,3,1) = imu_value.block(4,0,3,1);
+		state_plus.block(7,0,3,1) = imu_value.block(7,0,3,1);
 
-		if (prop_data.size() > 1) state_plus.block(10,0,3,1) = prop_data.at(prop_data.size()-2).wm - temp_imu->bias_g();
-		else if (!prop_data.empty()) state_plus.block(10,0,3,1) = prop_data.at(prop_data.size()-1).wm - temp_imu->bias_g();
+		if (prop_data.size() > 1) state_plus.block(10,0,3,1) = prop_data.at(prop_data.size()-2).wm - w_hat;
+		else if (!prop_data.empty()) state_plus.block(10,0,3,1) = prop_data.at(prop_data.size()-1).wm - w_hat;
 
 		_m_imu_raw->put(new imu_raw_type{
 			w_hat,
@@ -192,112 +186,57 @@ private:
 		});
     }
 
-	std::vector<ov_msckf::Propagator::IMUDATA> select_imu_readings(const std::vector<ov_msckf::Propagator::IMUDATA>& imu_data, double time0, double time1) {
-
-		// Our vector imu readings
-		std::vector<ov_msckf::Propagator::Propagator::IMUDATA> prop_data;
-
-		// Ensure we have some measurements in the first place!
-		if(imu_data.empty()) {
-			printf(YELLOW "Propagator::select_imu_readings(): No IMU measurements. IMU-CAMERA are likely messed up!!!\n" RESET);
+	// Select IMU readings based on timestamp similar to how OpenVINS selects IMU values to propagate
+	std::vector<imu_type> select_imu_readings(const std::vector<imu_type>& imu_data, double time_begin, double time_end) {
+		std::vector<imu_type> prop_data;
+		if (imu_data.size() < 2) {
 			return prop_data;
 		}
 
-		// Loop through and find all the needed measurements to propagate with
-		// Note we split measurements based on the given state time, and the update timestamp
-		for(size_t i=0; i<imu_data.size()-1; i++) {
+		for (unsigned i = 0; i < imu_data.size()-1; i++) {
 
-			// START OF THE INTEGRATION PERIOD
-			// If the next timestamp is greater then our current state time
-			// And the current is not greater then it yet...
-			// Then we should "split" our current IMU measurement
-			if(imu_data.at(i+1).timestamp > time0 && imu_data.at(i).timestamp < time0) {
-				ov_msckf::Propagator::IMUDATA data = interpolate_data(imu_data.at(i),imu_data.at(i+1), time0);
+			// If time_begin comes inbetween two IMUs (A and B), interpolate A forward to time_begin
+			if (imu_data.at(i+1).timestamp > time_begin && imu_data.at(i).timestamp < time_begin) {
+				imu_type data = interpolate_imu(imu_data.at(i), imu_data.at(i+1), time_begin);
 				prop_data.push_back(data);
-				//printf("propagation #%d = CASE 1 = %.3f => %.3f\n", (int)i,data.timestamp-prop_data.at(0).timestamp,time0-prop_data.at(0).timestamp);
 				continue;
 			}
 
-			// MIDDLE OF INTEGRATION PERIOD
-			// If our imu measurement is right in the middle of our propagation period
-			// Then we should just append the whole measurement time to our propagation vector
-			if(imu_data.at(i).timestamp >= time0 && imu_data.at(i+1).timestamp <= time1) {
+			// IMU is within time_begin and time_end
+			if (imu_data.at(i).timestamp >= time_begin && imu_data.at(i+1).timestamp <= time_end) {
 				prop_data.push_back(imu_data.at(i));
-				//printf("propagation #%d = CASE 2 = %.3f\n",(int)i,imu_data.at(i).timestamp-prop_data.at(0).timestamp);
 				continue;
 			}
 
-			// END OF THE INTEGRATION PERIOD
-			// If the current timestamp is greater then our update time
-			// We should just "split" the NEXT IMU measurement to the update time,
-			// NOTE: we add the current time, and then the time at the end of the interval (so we can get a dt)
-			// NOTE: we also break out of this loop, as this is the last IMU measurement we need!
-			if(imu_data.at(i+1).timestamp > time1) {
-				// If we have a very low frequency IMU then, we could have only recorded the first integration (i.e. case 1) and nothing else
-				// In this case, both the current IMU measurement and the next is greater than the desired intepolation, thus we should just cut the current at the desired time
-				// Else, we have hit CASE2 and this IMU measurement is not past the desired propagation time, thus add the whole IMU reading
-				if(imu_data.at(i).timestamp > time1) {
-					ov_msckf::Propagator::IMUDATA data = interpolate_data(imu_data.at(i-1), imu_data.at(i), time1);
-					prop_data.push_back(data);
-					//printf("propagation #%d = CASE 3.1 = %.3f => %.3f\n", (int)i,imu_data.at(i).timestamp-prop_data.at(0).timestamp,imu_data.at(i).timestamp-time0);
-				} else {
-					prop_data.push_back(imu_data.at(i));
-					//printf("propagation #%d = CASE 3.2 = %.3f => %.3f\n", (int)i,imu_data.at(i).timestamp-prop_data.at(0).timestamp,imu_data.at(i).timestamp-time0);
-				}
-				// If the added IMU message doesn't end exactly at the camera time
-				// Then we need to add another one that is right at the ending time
-				if(prop_data.at(prop_data.size()-1).timestamp != time1) {
-					ov_msckf::Propagator::IMUDATA data = interpolate_data(imu_data.at(i), imu_data.at(i+1), time1);
-					prop_data.push_back(data);
-					//printf("propagation #%d = CASE 3.3 = %.3f => %.3f\n", (int)i,data.timestamp-prop_data.at(0).timestamp,data.timestamp-time0);
-				}
+			// IMU is past time_end
+			if (imu_data.at(i+1).timestamp > time_end) {
+				imu_type data = interpolate_imu(imu_data.at(i), imu_data.at(i+1), time_end);
+				prop_data.push_back(data);
 				break;
 			}
-
 		}
-
-		// Check that we have at least one measurement to propagate with
-		if(prop_data.empty()) {
-			printf(YELLOW "Propagator::select_imu_readings(): No IMU measurements to propagate with (%d of 2). IMU-CAMERA are likely messed up!!!\n" RESET, (int)prop_data.size());
-			return prop_data;
-		}
-
-		// If we did not reach the whole integration period (i.e., the last inertial measurement we have is smaller then the time we want to reach)
-		// Then we should just "stretch" the last measurement to be the whole period (case 3 in the above loop)
-		//if(time1-imu_data.at(imu_data.size()-1).timestamp > 1e-3) {
-		//    printf(YELLOW "Propagator::select_imu_readings(): Missing inertial measurements to propagate with (%.6f sec missing). IMU-CAMERA are likely messed up!!!\n" RESET, (time1-imu_data.at(imu_data.size()-1).timestamp));
-		//    return prop_data;
-		//}
 
 		// Loop through and ensure we do not have an zero dt values
 		// This would cause the noise covariance to be Infinity
-		for (size_t i=0; i < prop_data.size()-1; i++) {
+		for (size_t i = 0; i < prop_data.size()-1; i++) {
 			if (std::abs(prop_data.at(i+1).timestamp-prop_data.at(i).timestamp) < 1e-12) {
-				printf(YELLOW "Propagator::select_imu_readings(): Zero DT between IMU reading %d and %d, removing it!\n" RESET, (int)i, (int)(i+1));
 				prop_data.erase(prop_data.begin()+i);
 				i--;
 			}
 		}
 
-		// Check that we have at least one measurement to propagate with
-		if(prop_data.size() < 2) {
-			printf(YELLOW "Propagator::select_imu_readings(): No IMU measurements to propagate with (%d of 2). IMU-CAMERA are likely messed up!!!\n" RESET, (int)prop_data.size());
-			return prop_data;
-		}
-
-		// Success :D
 		return prop_data;
 	}
 
-	static ov_msckf::Propagator::IMUDATA interpolate_data(const ov_msckf::Propagator::IMUDATA imu_1, const ov_msckf::Propagator::IMUDATA imu_2, double timestamp) {
-		// time-distance lambda
-		double lambda = (timestamp - imu_1.timestamp) / (imu_2.timestamp - imu_1.timestamp);
-		//cout << "lambda - " << lambda << endl;
-		// interpolate between the two times
-		ov_msckf::Propagator::IMUDATA data;
+	// For when an integration time ever falls inbetween two imu measurements (modeled after OpenVINS)
+	static imu_type interpolate_imu(const imu_type imu_1, imu_type imu_2, double timestamp) {
+		imu_type data;
 		data.timestamp = timestamp;
+
+		double lambda = (timestamp - imu_1.timestamp) / (imu_2.timestamp - imu_1.timestamp);
 		data.am = (1 - lambda) * imu_1.am + lambda * imu_2.am;
 		data.wm = (1 - lambda) * imu_1.wm + lambda * imu_2.wm;
+
 		return data;
 	}
 
