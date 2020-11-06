e705bc27974ae062ab6ec8275bbe497912f2dc9a
Hyjale <saishidual@gmail.com>
2020-11-06 17:26:24 -0600

    address charmoniums comments

--- scripts/install_opencv.sh
+++ scripts/install_opencv.sh
@@ -5,21 +5,15 @@ git clone --branch 3.4.6 https://github.com/opencv/opencv_contrib/  "${temp_dir}
 if ! nvcc --version &> /dev/null
 then
     echo "nvcc not found"
-    cmake \
-	    -S "${temp_dir}/opencv" \
-	    -B "${temp_dir}/opencv/build" \
-	    -D CMAKE_BUILD_TYPE=RelWithDebInfo \
-	    -D CMAKE_INSTALL_PREFIX=/usr/local \
-	    -D BUILD_TESTS=OFF \
-	    -D BUILD_PERF_TESTS=OFF \
-	    -D BUILD_EXAMPLES=OFF \
-	    -D BUILD_JAVA=OFF \
-	    -D WITH_OPENGL=ON \
-	    -D WITH_VTK=ON \
-	    -D OPENCV_EXTRA_MODULES_PATH="${temp_dir}/opencv_contrib/modules"    
 else
     echo "nvcc found"
-    cmake \
+    extra_cmake_args="
+	    -D WITH_CUDA=ON
+	    -D WITH_CUBLAS=1
+	    -D CUDA_NVCC_FLAGS=-allow-unsupported-compiler
+	    -D BUILD_opencv_cudacodec=OFF"
+fi
+cmake \
 	    -S "${temp_dir}/opencv" \
 	    -B "${temp_dir}/opencv/build" \
 	    -D CMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -30,12 +24,8 @@ else
 	    -D BUILD_JAVA=OFF \
 	    -D WITH_OPENGL=ON \
 	    -D WITH_VTK=ON \
-	    -D WITH_CUDA=ON \
-	    -D WITH_CUBLAS=1 \
-	    -D CUDA_NVCC_FLAGS=-allow-unsupported-compiler \
-	    -D BUILD_opencv_cudacodec=OFF \
-	    -D OPENCV_EXTRA_MODULES_PATH="${temp_dir}/opencv_contrib/modules"
-fi
+	    -D OPENCV_EXTRA_MODULES_PATH="${temp_dir}/opencv_contrib/modules" \
+	    ${extra_cmake_args}
 sudo make -C "${temp_dir}/opencv/build" "-j$(nproc)" install
 
 # Remove temp cmake files
