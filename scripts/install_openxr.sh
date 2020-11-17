c0d8ebeb59619489e0cf2c840c2b0abbdb3b278b
Rishi Desai <desai.rishi1@gmail.com>
2020-11-16 20:43:26 -0600

    updated openxr, vulkan headers, and gtest to use illixr_nproc

--- scripts/install_openxr.sh
+++ scripts/install_openxr.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/KhronosGroup/OpenXR-SDK.git "${temp_dir}/OpenXR-SDK"
 cmake -S "${temp_dir}/OpenXR-SDK" -B "${temp_dir}/OpenXR-SDK/build"
-sudo make -C "${temp_dir}/OpenXR-SDK/build" "-j$(nproc)" install
+sudo make -C "${temp_dir}/OpenXR-SDK/build" "-j${illixr_nproc}" install
