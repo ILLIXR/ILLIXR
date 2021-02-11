8ad619a3f3b87330bd8089d323e79c9c19f81ebb
Boyuan-Tian <546600519@qq.com>
2021-02-11 12:26:47 -0600

    fix compiler errors

--- offload_data/plugin.cpp
+++ offload_data/plugin.cpp
@@ -103,14 +103,14 @@ private:
 			meta_file << "total number: " << _time_seq.size() << std::endl;
 
 			meta_file << "raw time: " << std::endl;
-			for (int i = 0; i < _time_seq.size(); i++)
-				meta_file << _time_seq[i] << " ";
+			for (int& it : _time_seq)
+				meta_file << it << " ";
 			meta_file << std::endl << std::endl << std::endl;
 
 			meta_file << "ordered time: " << std::endl;
 			std::sort(_time_seq.begin(), _time_seq.end(), [](int x, int y) {return x > y;});
-			for (int i = 0; i < _time_seq.size(); i++)
-				meta_file << _time_seq[i] << " ";
+			for (int& it : _time_seq)
+				meta_file << it << " ";
 		}
 		meta_file.close();
 	}
@@ -120,16 +120,17 @@ private:
 		stbi_flip_vertically_on_write(true);
 
 		std::cout << "Writing offloaded images to disk ... " << std::endl;
-		for (int i = 0; i < _offload_data_container.size(); i++)
+		img_idx = 0;
+		for (auto& container_it : _offload_data_container)
 		{
 			// Get collecting time for each frame
-			_time_seq.push_back(_offload_data_container[i]->offload_time);
+			_time_seq.push_back(container_it->offload_time);
 
-			std::string image_name = obj_dir + std::to_string(i) + ".png";
-			std::string pose_name = obj_dir + std::to_string(i) + ".txt";
+			std::string image_name = obj_dir + std::to_string(img_idx) + ".png";
+			std::string pose_name = obj_dir + std::to_string(img_idx) + ".txt";
 
 			// Write image
-			is_success = stbi_write_png(image_name.c_str(), ILLIXR::FB_WIDTH, ILLIXR::FB_HEIGHT, 3, _offload_data_container[i]->image, 0);
+			is_success = stbi_write_png(image_name.c_str(), ILLIXR::FB_WIDTH, ILLIXR::FB_HEIGHT, 3, container_it->image, 0);
 			if (!is_success)
 			{
 				std::cerr << "Image create failed !!! " << std::endl;
@@ -140,41 +141,41 @@ private:
 			std::ofstream pose_file (pose_name);
 			if (pose_file.is_open())
 			{
-				std::time_t pose_time = std::chrono::system_clock::to_time_t(_offload_data_container[i]->pose_time);
+				std::time_t pose_time = std::chrono::system_clock::to_time_t(container_it->pose_time);
 
 				// Transfer timestamp to duration
-				auto duration = (_offload_data_container[i]->pose_time).time_since_epoch().count();
+				auto duration = (container_it->pose_time).time_since_epoch().count();
 
 				// Write time data
 				pose_file << "cTime: " << std::ctime(&pose_time);
 				pose_file << "strTime: " << duration << std::endl;
 
 				// Write position coordinates in x y z
-				int pose_size = _offload_data_container[i]->position.size();
+				int pose_size = container_it->position.size();
 				pose_file << "pos: ";
 				for (int pos_idx = 0; pos_idx < pose_size; pos_idx++)
-					pose_file << _offload_data_container[i]->position(pos_idx) << " ";
+					pose_file << container_it->position(pos_idx) << " ";
 				pose_file << std::endl;
 
 				// Write quaternion in w x y z
 				pose_file << "latest_pose_orientation: ";
-				pose_file << _offload_data_container[i]->latest_quaternion.w() << " ";
-				pose_file << _offload_data_container[i]->latest_quaternion.x() << " ";
-				pose_file << _offload_data_container[i]->latest_quaternion.y() << " ";
-				pose_file << _offload_data_container[i]->latest_quaternion.z() << std::endl;
+				pose_file << container_it->latest_quaternion.w() << " ";
+				pose_file << container_it->latest_quaternion.x() << " ";
+				pose_file << container_it->latest_quaternion.y() << " ";
+				pose_file << container_it->latest_quaternion.z() << std::endl;
 
 				pose_file << "render_pose_orientation: ";
-				pose_file << _offload_data_container[i]->render_quaternion.w() << " ";
-				pose_file << _offload_data_container[i]->render_quaternion.x() << " ";
-				pose_file << _offload_data_container[i]->render_quaternion.y() << " ";
-				pose_file << _offload_data_container[i]->render_quaternion.z();
+				pose_file << container_it->render_quaternion.w() << " ";
+				pose_file << container_it->render_quaternion.x() << " ";
+				pose_file << container_it->render_quaternion.y() << " ";
+				pose_file << container_it->render_quaternion.z();
 			}
 			pose_file.close();
 
 			// Print progress
-			percent = (100 * (i + 1) / _offload_data_container.size());
+			percent = (100 * (img_idx + 1) / _offload_data_container.size());
 			std::cout << "\r" << "[" << std::string(percent / 2, (char)61u) << std::string(100 / 2 - percent / 2, ' ') << "] ";
-			std::cout << percent << "%" << " [Image " << i++ << " of " << _offload_data_container.size() << "]";
+			std::cout << percent << "%" << " [Image " << img_idx++ << " of " << _offload_data_container.size() << "]";
 			std::cout.flush();
 		}
 		std::cout << std::endl;
