73349fe6cdfa8f32f5ed47f4f8711476e0bc0c6c
Samuel Grayson <sam@samgrayson.me>
2021-03-03 20:59:52 -0600

    Fix offline_cam

--- offline_cam/plugin.cpp
+++ offline_cam/plugin.cpp
@@ -20,11 +20,13 @@ public:
 		, dataset_first_time{_m_sensor_data.cbegin()->first}
 		, last_ts{0}
 		, _m_rtc{pb->lookup_impl<realtime_clock>()}
-		, next_row{_m_sensor_data.cbegin()}
+		// , next_row{_m_sensor_data.cbegin()}
     { }
 
 	virtual skip_option _p_should_skip() override {
-		if (next_row != _m_sensor_data.end() && next_row->second.cam0 && next_row->second.cam1) {
+		if (true
+			// next_row != _m_sensor_data.end() && next_row->second.cam0 && next_row->second.cam1
+			) {
 			return skip_option::run;
 		} else {
 			return skip_option::stop;
@@ -34,43 +36,43 @@ public:
 	virtual void _p_one_iteration() override {
 		std::cerr << "offline_cam::_p_one_iteration()\n";
 		duration time_since_start = _m_rtc->time_since_start();
-// 		ullong lookup_time = std::chrono::nanoseconds{time_since_start}.count() + dataset_first_time;
+		ullong lookup_time = std::chrono::nanoseconds{time_since_start}.count() + dataset_first_time;
 		
-// 		auto after_row = _m_sensor_data.upper_bound(lookup_time);
-// 		std::map<ullong, sensor_types>::const_iterator nearest_row;
+		auto after_row = _m_sensor_data.upper_bound(lookup_time);
+		std::map<ullong, sensor_types>::const_iterator nearest_row;
 
-// 		if (after_row == _m_sensor_data.cend() || !(after_row->second.cam0 && after_row->second.cam1)) {
-// #ifndef NDEBUG
-// 			std::cerr << "Time " << lookup_time << " (" << _m_rtc->time_since_start().count() << " + " << dataset_first_time << ") after last datum " << _m_sensor_data.rbegin()->first << std::endl;
-// #endif
-// 			after_row--;
-// 			stop();
-// 			return;
-// 		} else if (after_row == _m_sensor_data.cbegin()) {
-// #ifndef NDEBUG
-// 			std::cerr << "Time " << lookup_time << " (" << _m_rtc->time_since_start().count() << " + " << dataset_first_time << ") before first datum " << _m_sensor_data.cbegin()->first << std::endl;
-// #endif
-// 		} else {
-// 			// "std::map::upper_bound" returns an iterator to the first pair whose key is GREATER than the argument.
-// 			// I already know we aren't at the begin()
+		if (after_row == _m_sensor_data.cend() || !(after_row->second.cam0 && after_row->second.cam1)) {
+#ifndef NDEBUG
+			std::cerr << "Time " << lookup_time << " (" << _m_rtc->time_since_start().count() << " + " << dataset_first_time << ") after last datum " << _m_sensor_data.rbegin()->first << std::endl;
+#endif
+			after_row--;
+			stop();
+			return;
+		} else if (after_row == _m_sensor_data.cbegin()) {
+#ifndef NDEBUG
+			std::cerr << "Time " << lookup_time << " (" << _m_rtc->time_since_start().count() << " + " << dataset_first_time << ") before first datum " << _m_sensor_data.cbegin()->first << std::endl;
+#endif
+		} else {
+			// "std::map::upper_bound" returns an iterator to the first pair whose key is GREATER than the argument.
+			// I already know we aren't at the begin()
 
-// 			// Most recent row
-// 			after_row--;
-// 			nearest_row = after_row /* after_row - 1 */;
+			// Earliest after
+			// nearest_row = after_row;
 
-// 			// Nearest row
-// 			// auto after_row2 = after_row;
-// 			// auto prev_row = after_row; prev_row--;
-// 			// // stop using after_row
-// 			// if (after_row2->first - lookup_time < lookup_time - prev_row->first) {
-// 			// 	nearest_row = after_row2;
-// 			// } else {
-// 			// 	nearest_row = prev_row;
-// 			// }
-// 		}
+			// Most recent row
+			after_row--;
+			nearest_row = after_row /* after_row - 1 */;
 
-		auto nearest_row = next_row;
-		next_row++;
+			// Nearest row
+			// auto after_row2 = after_row;
+			// auto prev_row = after_row; prev_row--;
+			// // stop using after_row
+			// if (after_row2->first - lookup_time < lookup_time - prev_row->first) {
+			// 	nearest_row = after_row2;
+			// } else {
+			// 	nearest_row = prev_row;
+			// }
+		}
 
 		if (nearest_row->second.cam0 && nearest_row->second.cam1 && last_ts != nearest_row->first) {
 			if (nearest_row->first - last_ts > 75 * 1000 * 1000) {
@@ -83,9 +85,9 @@ public:
 			auto img0 = nearest_row->second.cam0.value().load();
 			auto img1 = nearest_row->second.cam1.value().load();
 
-			std::this_thread::sleep_for(
-				std::chrono::nanoseconds{nearest_row->first} - std::chrono::nanoseconds{dataset_first_time} - _m_rtc->time_since_start() - std::chrono::milliseconds{4}
-			);
+			// std::this_thread::sleep_for(
+			// 	std::chrono::nanoseconds{nearest_row->first} - std::chrono::nanoseconds{dataset_first_time} - _m_rtc->time_since_start() - std::chrono::milliseconds{4}
+			// );
 
 			_m_cam_publisher.put(new (_m_cam_publisher.allocate()) cam_type {
 				time_since_start + _m_rtc->get_start(),
@@ -120,7 +122,7 @@ private:
 	ullong dataset_first_time;
 	ullong last_ts;
 	std::shared_ptr<realtime_clock> _m_rtc;
-	std::map<ullong, sensor_types>::const_iterator next_row;
+	// std::map<ullong, sensor_types>::const_iterator next_row;
 };
 
 PLUGIN_MAIN(offline_cam);
