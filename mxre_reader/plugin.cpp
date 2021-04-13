d409d06486f6cf990f3fc38c6f0ee9b19de79bb0
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2021-04-13 16:57:40 -0500

    Feature: Offloaded SLAM (#266)
    * fix two typos at the end of plugin.cpp
    
    * merge
    
    * 4/7 built offloaded slam with some unidentified bugs
    Co-authored-by: Jeffrey Zhang <JeffreyZh4ng@users.noreply.github.com>
    
    * revert unnecessary changes happen to be committed
    
    * revert unncessary config file committed
    
    * 4/8 offloaded slam pass

--- mxre_reader/plugin.cpp
+++ mxre_reader/plugin.cpp
@@ -6,6 +6,7 @@
 #include "common/pose_prediction.hpp"
 #include "common/math_util.hpp"
 #include "common/phonebook.hpp"
+#include <iostream>
 
 class mxre_reader : public threadloop {
   public:
@@ -13,67 +14,75 @@ class mxre_reader : public threadloop {
       : threadloop{name_, pb_}
       , sb{pb->lookup_impl<switchboard>()}
       , _m_pose{sb->publish<pose_type>("slow_pose")}
-    	, _m_imu_integrator_input{sb->publish<imu_integrator_input>("imu_integrator_input")}
+      , _m_imu_raw{sb->publish<imu_raw_type>("imu_raw")}
+    	// , _m_imu_integrator_input{sb->publish<imu_integrator_input>("imu_integrator_input")}
     {}
 
     virtual void _p_thread_setup() {
-      illixrSink.setup("sink", MX_DTYPE_CVMAT);
+      illixrSink.setup("sink");
     }
-
+       
     virtual void _p_one_iteration() {
-      illixrSink.recv(&recvFrame);
-
-      const auto& cached_state = vio_output->W_State_Blkf_;
-      const auto& w_pose_blkf_trans = cached_state.pose_.translation().transpose();
-      const auto& w_pose_blkf_rot = cached_state.pose_.rotation().quaternion();
-      const auto& w_vel_blkf = cached_state.velocity_.transpose();
-      const auto& imu_bias_gyro = cached_state.imu_bias_.gyroscope().transpose();
-      const auto& imu_bias_acc = cached_state.imu_bias_.accelerometer().transpose();
+      //TODO: mitigate memory leak
+      mxre::kimera_type::kimera_output recvKimeraOutput;
+      illixrSink.recv_kimera_output(&recvKimeraOutput);
 
       // Get the pose returned from SLAM
-      Eigen::Quaternionf quat = Eigen::Quaternionf{w_pose_blkf_rot(0), w_pose_blkf_rot(1), w_pose_blkf_rot(2), w_pose_blkf_rot(3)};
-      Eigen::Quaterniond doub_quat = Eigen::Quaterniond{w_pose_blkf_rot(0), w_pose_blkf_rot(1), w_pose_blkf_rot(2), w_pose_blkf_rot(3)};
-      Eigen::Vector3f pos  = w_pose_blkf_trans.cast<float>();
-
-      assert(isfinite(quat.w()));
-      assert(isfinite(quat.x()));
-      assert(isfinite(quat.y()));
-      assert(isfinite(quat.z()));
-      assert(isfinite(pos[0]));
-      assert(isfinite(pos[1]));
-      assert(isfinite(pos[2]));
+      Eigen::Quaternionf quat = Eigen::Quaternionf{recvKimeraOutput.quat[0], recvKimeraOutput.quat[1], recvKimeraOutput.quat[2], recvKimeraOutput.quat[3]};
+      Eigen::Quaterniond doub_quat = Eigen::Quaterniond{recvKimeraOutput.quat[0], recvKimeraOutput.quat[1], recvKimeraOutput.quat[2], recvKimeraOutput.quat[3]};
+      Eigen::Vector3f pos = Eigen::Vector3f(recvKimeraOutput.pose_type_position[0], recvKimeraOutput.pose_type_position[1], recvKimeraOutput.pose_type_position[2]);
       
       _m_pose->put(new pose_type{
-        .sensor_time = imu_cam_buffer->time,
+        .sensor_time = recvKimeraOutput.sensor_time,
         .position = pos,
         .orientation = quat,
       });
 
-      _m_imu_integrator_input->put(new imu_integrator_input{
-        .last_cam_integration_time = (double(imu_cam_buffer->dataset_time) / NANO_SEC),
-        .t_offset = -0.05,
-
-        .params = {
-          .gyro_noise = kimera_pipeline_params.imu_params_.gyro_noise_,
-          .acc_noise = kimera_pipeline_params.imu_params_.acc_noise_,
-          .gyro_walk = kimera_pipeline_params.imu_params_.gyro_walk_,
-          .acc_walk = kimera_pipeline_params.imu_params_.acc_walk_,
-          .n_gravity = kimera_pipeline_params.imu_params_.n_gravity_,
-          .imu_integration_sigma = kimera_pipeline_params.imu_params_.imu_integration_sigma_,
-          .nominal_rate = kimera_pipeline_params.imu_params_.nominal_rate_,
-        },
-
-        .biasAcc =imu_bias_acc,
-        .biasGyro = imu_bias_gyro,
-        .position = w_pose_blkf_trans,
-        .velocity = w_vel_blkf,
-        .quat = doub_quat,
+      _m_imu_raw->put(new imu_raw_type{
+        Eigen::Matrix<double,3,1>{0, 0, 0},
+        Eigen::Matrix<double,3,1>{0, 0, 0},
+        Eigen::Matrix<double,3,1>{0, 0, 0},
+        Eigen::Matrix<double,3,1>{0, 0, 0},
+        Eigen::Matrix<double,3,1>{recvKimeraOutput.position[0], recvKimeraOutput.position[1], recvKimeraOutput.position[2]}, // Position
+        Eigen::Matrix<double,3,1>{recvKimeraOutput.velocity[0], recvKimeraOutput.velocity[1], recvKimeraOutput.velocity[2]}, // Velocity
+        Eigen::Quaterniond{recvKimeraOutput.orientation[0], recvKimeraOutput.orientation[1], recvKimeraOutput.orientation[2], recvKimeraOutput.orientation[3]}, // Eigen Quat
+        recvKimeraOutput.sensor_time
       });
+
+      // _m_imu_integrator_input->put(new imu_integrator_input{
+      //   .last_cam_integration_time = (double(recvKimeraOutput.last_cam_integration_time) / NANO_SEC),
+      //   .t_offset = -0.05,
+
+      //   .params = {
+      //     .gyro_noise = recvKimeraOutput.imu_params_gyro_noise,
+      //     .acc_noise = recvKimeraOutput.imu_params_acc_noise,
+      //     .gyro_walk = recvKimeraOutput.imu_params_gyro_walk,
+      //     .acc_walk = recvKimeraOutput.imu_params_acc_walk,
+      //     .n_gravity = Eigen::Matrix<double,3,1>{recvKimeraOutput.imu_params_n_gravity[0], recvKimeraOutput.imu_params_n_gravity[1], recvKimeraOutput.imu_params_n_gravity[2]},
+      //     .imu_integration_sigma = recvKimeraOutput.imu_params_imu_integration_sigma,
+      //     .nominal_rate = recvKimeraOutput.imu_params_nominal_rate,
+      //   },
+
+      //   .biasAcc = Eigen::Vector3d{recvKimeraOutput.imu_params_n_gravity[0], recvKimeraOutput.imu_params_n_gravity[1], recvKimeraOutput.imu_params_n_gravity[2]},
+      //   .biasGyro = Eigen::Vector3d{recvKimeraOutput.biasGyro[0], recvKimeraOutput.biasGyro[1], recvKimeraOutput.biasGyro[2]},
+      //   .position = Eigen::Matrix<double,3,1>{recvKimeraOutput.position[0], recvKimeraOutput.position[1], recvKimeraOutput.position[2]},
+      //   .velocity = Eigen::Matrix<double,3,1>{recvKimeraOutput.velocity[0], recvKimeraOutput.velocity[1], recvKimeraOutput.velocity[2]},
+      //   .quat = Eigen::Quaterniond{recvKimeraOutput.orientation[0], recvKimeraOutput.orientation[1], recvKimeraOutput.orientation[2], recvKimeraOutput.orientation[3]},
+      // });
+
+      // std::cerr << "Pose: " << recvKimeraOutput.pose_type_position[1] << ", " << recvKimeraOutput.pose_type_position[1] << ", " << recvKimeraOutput.pose_type_position[2] << std::endl;
+      // std::cerr << "Rot: " << recvKimeraOutput.quat[0] << ", " << recvKimeraOutput.quat[1] << ", " << recvKimeraOutput.quat[2] << ", " << recvKimeraOutput.quat[3] << std::endl;
+      // auto curr_time = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now());
+      // std::cerr << "Latency: " << std::chrono::duration_cast<std::chrono::milliseconds>(curr_time - recvKimeraOutput.sensor_time).count() << std::endl << std::endl;
     }
 
   private:
     const std::shared_ptr<switchboard> sb;
     std::unique_ptr<writer<pose_type>> _m_pose;
-	  std::unique_ptr<writer<imu_integrator_input>> _m_imu_integrator_input;
+    std::unique_ptr<writer<imu_raw_type>> _m_imu_raw;
+	  // std::unique_ptr<writer<imu_integrator_input>> _m_imu_integrator_input;
 
-    mxre::types::ILLIXRSink<cv::Mat> illixrSink;
+    mxre::kernels::ILLIXRSink<mxre::kimera_type::kimera_output> illixrSink;
+};
+
+PLUGIN_MAIN(mxre_reader)
