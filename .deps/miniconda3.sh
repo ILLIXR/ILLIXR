8e251fb08e87303bd6a68c020dae2a1f57a6fb37
Samuel Grayson <sam@samgrayson.me>
2021-04-27 01:48:12 -0500

    wip (revert)

---
+++ .deps/miniconda3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### INSTALL ###
+export dep_name='miniconda3'
+export src_dir='/opt/ILLIXR/miniconda3'
+export dep_ver='py38_4.9.2'
+export timestamp='Tue 06 Apr 2021 07:44:14 PM UTC'
