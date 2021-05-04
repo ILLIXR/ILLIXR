41e0b31b1052dead454cca522cdc77f7dfeef0cb
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2021-05-04 11:14:55 -0500

    output pose and ground truth in gtsam

--- gtsam_integrator/plugin.cpp
+++ gtsam_integrator/plugin.cpp
@@ -39,7 +39,10 @@ public:
 		, _m_imu_cam{sb->subscribe_latest<imu_cam_type>("imu_cam")}
 		, _m_in{sb->subscribe_latest<imu_integrator_seq>("imu_integrator_seq")}
 		, _m_imu_integrator_input{sb->subscribe_latest<imu_integrator_input>("imu_integrator_input")}
-		, _m_imu_raw{sb->publish<imu_raw_type>("imu_raw")}
+        , _m_true_pose{sb->subscribe_latest<pose_type>("true_pose")}
+		, _m_imu_raw{sb->publish<imu_raw_type>("imu_raw")},
+        , _m_start_of_time{std::chrono::high_resolution_clock::now()}
+		, first_true_pose_flag(false)
 		, _seq_expect(1)
 	{}
 
@@ -84,9 +87,17 @@ private:
 	std::unique_ptr<reader_latest<imu_cam_type>> _m_imu_cam;
 	std::unique_ptr<reader_latest<imu_integrator_seq>> _m_in;
 	std::unique_ptr<reader_latest<imu_integrator_input>> _m_imu_integrator_input;
+	
+    std::unique_ptr<reader_latest<pose_type>> _m_true_pose;
+
+	mutable Eigen::Quaternionf offset {Eigen::Quaternionf::Identity()};
+	mutable std::shared_mutex offset_mutex;
 
 	// Write IMU Biases for PP
 	std::unique_ptr<writer<imu_raw_type>> _m_imu_raw;
+	double first_true_pose[7];
+    time_type _m_start_of_time;
+	bool first_true_pose_flag;
 
 	std::vector<imu_type> _imu_vec;
   	PimUniquePtr pim_ = NULL;
@@ -109,6 +120,53 @@ private:
 			imu_iterator = _imu_vec.erase(imu_iterator);
 		}
 	}
+
+	void _set_offset(const Eigen::Quaternionf& raw_o_times_offset){
+        Eigen::Quaternionf raw_o = raw_o_times_offset * offset.inverse();
+        //std::cout << "pose_prediction: set_offset" << std::endl;
+        offset = raw_o.inverse();
+    }
+
+	Eigen::Quaternionf _apply_offset(const Eigen::Quaternionf& orientation) const {
+        std::shared_lock lock {offset_mutex};
+        return orientation * offset;
+    }
+
+	pose_type _correct_pose(const pose_type pose) const {
+        pose_type swapped_pose;
+
+        // Step 1: Compensate starting point to (0, 0, 0), pos only
+        auto input_pose = pose_type{
+            .sensor_time = pose.sensor_time,
+            .position = Eigen::Vector3f{
+                pose.position(0) - first_true_pose[0],
+                pose.position(1) - first_true_pose[1],
+                pose.position(2) - first_true_pose[2],
+            },
+            .orientation = pose.orientation,
+        };
+
+        // Step 3: Swap axis for both position and orientation
+        // Step 3.1: Swap for position
+        // This uses the OpenVINS standard output coordinate system.
+        // This is a mapping between the OV coordinate system and the OpenGL system.
+        swapped_pose.position.x() = -input_pose.position.x();
+        swapped_pose.position.z() = input_pose.position.z();
+        swapped_pose.position.y() = -input_pose.position.y();
+		// swapped_pose.position.x() = input_pose.position.x();
+        // swapped_pose.position.z() = input_pose.position.z();
+        // swapped_pose.position.y() = input_pose.position.y();
+		swapped_pose.orientation=input_pose.orientation;
+        // Step 3.2: Swap for orientation
+        // There is a slight issue with the orientations: basically,
+        // the output orientation acts as though the "top of the head" is the
+        // forward direction, and the "eye direction" is the up direction.
+        Eigen::Quaternionf raw_o (input_pose.orientation.w(), -input_pose.orientation.y(), input_pose.orientation.z(), -input_pose.orientation.x());
+
+        swapped_pose.orientation = _apply_offset(raw_o);
+
+        return swapped_pose;
+    }
 
 	// Timestamp we are propagating the biases to (new IMU reading time)
 	void propagate_imu_values(double timestamp, time_type real_time) {
@@ -133,12 +191,12 @@ private:
 			last_imu_offset = input_values->t_offset;
 		}
 
-// #ifndef NDEBUG
+#ifndef NDEBUG
 		if (input_values->last_cam_integration_time > last_cam_time) {
 			std::cout << "New slow pose has arrived!\n";
 			last_cam_time = input_values->last_cam_integration_time;
 		}
-// #endif
+#endif
 		pim_->resetIntegrationAndSetBias(imu_bias);
 
 		double time_begin = input_values->last_cam_integration_time + last_imu_offset;
@@ -152,9 +210,9 @@ private:
    		ImuBias prev_bias = pim_->biasHat();
 		ImuBias bias = pim_->biasHat();
 
-// #ifndef NDEBUG
+#ifndef NDEBUG
 		std::cout << "Integrating over " << prop_data.size() << " IMU samples\n";
-// #endif
+#endif
 
 		for (unsigned i = 0; i < prop_data.size()-1; i++) {
 			const gtsam::Vector3& measured_acc = prop_data.at(i).am;
@@ -183,7 +241,27 @@ private:
 				<< out_pose.y() << ", "
 				<< out_pose.z() << std::endl;
 #endif
+		std::cout<<"OUR SCHEME POSE!!!: "<<" "<</*real_time<<" "<<*/out_pose.translation().x()<<" "<<out_pose.translation().y()<<" "<<out_pose.translation().z()<<" "<<out_pose.rotation().toQuaternion().x()<<" "<<out_pose.rotation().toQuaternion().y()<<" "<<out_pose.rotation().toQuaternion().z()<<" "<<out_pose.rotation().toQuaternion().w()<<" "<<std::endl;
+        const pose_type *true_pose = _m_true_pose->get_latest_ro();
+		if (!first_true_pose_flag){
+			first_true_pose[0]=true_pose->position[0];
+			first_true_pose[1]=true_pose->position[1];
+			first_true_pose[2]=true_pose->position[2];
+			first_true_pose[3]=true_pose->orientation.x();
+			first_true_pose[4]=true_pose->orientation.y();
+			first_true_pose[5]=true_pose->orientation.z();
+			first_true_pose[6]=true_pose->orientation.w();
+		}
 
+		pose_type corrected_true_pose=_correct_pose(*true_pose);
+		if (!first_true_pose_flag){
+			_set_offset(corrected_true_pose.orientation);
+			first_true_pose_flag=true;
+		}
+
+        //std::cout<<"True POSE!!!: "<</*true_pose->sensor_time<<*/true_pose->position[0]-first_true_pose[0]<<true_pose->position[1]-first_true_pose[1]<<true_pose->position[2]-first_true_pose[2]<<true_pose->orientation.x()-first_true_pose[3]<<true_pose->orientation.y()-first_true_pose[4]<<true_pose->orientation.z()-first_true_pose[5]<<true_pose->orientation.w()-first_true_pose[6]<<std::endl;
+        std::cout<<"True POSE!!!: "<<" "<</*true_pose->sensor_time<<" "<<*/corrected_true_pose.position[0]<<" "<<corrected_true_pose.position[1]<<" "<<corrected_true_pose.position[2]<<" "<<corrected_true_pose.orientation.x()<<" "<<corrected_true_pose.orientation.y()<<" "<<corrected_true_pose.orientation.z()<<" "<<corrected_true_pose.orientation.w()<<" "<<std::endl;
+       
 		_m_imu_raw->put(new imu_raw_type{
 			prev_bias.gyroscope(),
 			prev_bias.accelerometer(),
