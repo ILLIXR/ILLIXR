3bfa254fc6af673c5625d57982269708eb9bca37
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2021-06-02 22:46:04 -0400

    Miscellaneous changes (#303)
    - Committing to mitigate CI initialization bug (gtsam preintegration)
    - Refactored spacing in 'common/dynamic_lib.hpp'

--- common/dynamic_lib.hpp
+++ common/dynamic_lib.hpp
@@ -25,28 +25,28 @@ Usage:
 
 class dynamic_lib {
 private:
-	dynamic_lib(
-	    void_ptr&& handle
+    dynamic_lib(
+        void_ptr&& handle
         , const std::string& lib_name = ""
-	) : _m_handle{std::move(handle)}
+    ) : _m_handle{std::move(handle)}
       , _m_lib_name{std::move(lib_name)}
-	{ }
+    { }
 
 public:
-	dynamic_lib(dynamic_lib&& other)
-		: _m_handle{std::move(other._m_handle)}
+    dynamic_lib(dynamic_lib&& other)
+        : _m_handle{std::move(other._m_handle)}
         , _m_lib_name{std::move(other._m_lib_name)}
-	{ }
+    { }
 
-	dynamic_lib& operator=(dynamic_lib&& other) {
-		if (this != &other) {
-			_m_handle   = std::move(other._m_handle);
-			_m_lib_name = std::move(other._m_lib_name);
-		}
-		return *this;
-	}
+    dynamic_lib& operator=(dynamic_lib&& other) {
+        if (this != &other) {
+            _m_handle   = std::move(other._m_handle);
+            _m_lib_name = std::move(other._m_lib_name);
+        }
+        return *this;
+    }
 
-	~dynamic_lib() {
+    ~dynamic_lib() {
 #ifndef NDEBUG
         if (_m_lib_name.size() > 0U) {
             std::cout << "[dynamic_lib] Destructing library : " << _m_lib_name << std::endl;
@@ -54,12 +54,12 @@ public:
 #endif /// NDEBUG
     }
 
-	static dynamic_lib create(const std::string& path) {
-		return dynamic_lib::create(std::string_view{path.c_str()});
-	}
+    static dynamic_lib create(const std::string& path) {
+        return dynamic_lib::create(std::string_view{path.c_str()});
+    }
 
-	static dynamic_lib create(const std::string_view& path) {
-		char* error;
+    static dynamic_lib create(const std::string_view& path) {
+        char* error;
 
         const std::size_t path_basename_end        {path.find_last_of("/")};
         const std::size_t path_basename_begin_tmp  {path.rfind("/", path_basename_end - 1U)};
@@ -80,19 +80,19 @@ public:
         std::cout << "[dynamic_lib] Opening library : " << path_basename << std::endl;
 #endif /// NDEBUG
 
-		// dlopen man page says that it can set errno sp
-		RAC_ERRNO_MSG("dynamic_lib before dlopen");
-		void* handle = dlopen(path.data(), RTLD_LAZY | RTLD_LOCAL);
-		RAC_ERRNO_MSG("dynamic_lib after dlopen");
+        // dlopen man page says that it can set errno sp
+        RAC_ERRNO_MSG("dynamic_lib before dlopen");
+        void* handle = dlopen(path.data(), RTLD_LAZY | RTLD_LOCAL);
+        RAC_ERRNO_MSG("dynamic_lib after dlopen");
 
-		if ((error = dlerror()) || !handle) {
-			throw std::runtime_error{
-				"dlopen(\"" + std::string{path} + "\"): " + (error == nullptr ? "NULL" : std::string{error})
-			};
+        if ((error = dlerror()) || !handle) {
+            throw std::runtime_error{
+                "dlopen(\"" + std::string{path} + "\"): " + (error == nullptr ? "NULL" : std::string{error})
+            };
         }
 
-		return dynamic_lib{
-		    void_ptr{handle, [](void* handle) {
+        return dynamic_lib{
+            void_ptr{handle, [](void* handle) {
                 RAC_ERRNO();
 
                 char* error;
@@ -107,33 +107,33 @@ public:
                     throw std::runtime_error{msg_error};
 #endif /// NDEBUG
                 }
-		    }}
-		    , path_basename /// Keep the dynamic lib name for debugging
-		};
-	}
+            }}
+            , path_basename /// Keep the dynamic lib name for debugging
+        };
+    }
 
-	const void* operator[](const std::string& symbol_name) const {
-		RAC_ERRNO_MSG("dynamic_lib at start of operator[]");
+    const void* operator[](const std::string& symbol_name) const {
+        RAC_ERRNO_MSG("dynamic_lib at start of operator[]");
 
-		char* error;		
-		void* symbol = dlsym(_m_handle.get(), symbol_name.c_str());
-		if ((error = dlerror())) {
-			throw std::runtime_error{
-				"dlsym(\"" + symbol_name + "\"): " + (error == nullptr ? "NULL" : std::string{error})
-			};
+        char* error;
+        void* symbol = dlsym(_m_handle.get(), symbol_name.c_str());
+        if ((error = dlerror())) {
+            throw std::runtime_error{
+                "dlsym(\"" + symbol_name + "\"): " + (error == nullptr ? "NULL" : std::string{error})
+            };
         }
-		return symbol;
-	}
+        return symbol;
+    }
 
-	template <typename T>
-	const T get(const std::string& symbol_name) const {
-		const void* obj = (*this)[symbol_name];
-		// return reinterpret_cast<const T>((*this)[symbol_name]);
-		return (const T) obj;
-	}
+    template <typename T>
+    const T get(const std::string& symbol_name) const {
+        const void* obj = (*this)[symbol_name];
+        // return reinterpret_cast<const T>((*this)[symbol_name]);
+        return (const T) obj;
+    }
 
 private:
-	void_ptr _m_handle;
+    void_ptr _m_handle;
     std::string _m_lib_name;
 };
 
