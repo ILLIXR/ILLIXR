e3a616e3ad0183734a0d08daeb555b4c27fef6d4
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2021-11-03 02:24:50 -0500

    Updated install script to pull and run 18.04.6

--- qemu/run.sh
+++ qemu/run.sh
@@ -8,10 +8,10 @@ if [ ! -d $QEMU_PATH ]; then
     exit -1
 fi
 
-# Download ubuntu-18.04.5-desktop-amd64.iso if it doesn't exist already and if there is no illixr image created
-if [ ! -f ubuntu-18.04.5-desktop-amd64.iso ]; then
+# Download ubuntu-18.04.6-desktop-amd64.iso if it doesn't exist already and if there is no illixr image created
+if [ ! -f ubuntu-18.04.6-desktop-amd64.iso ]; then
 	if [ ! -f illixr.qcow2 ]; then
-    	wget https://releases.ubuntu.com/18.04/ubuntu-18.04.5-desktop-amd64.iso
+    	wget https://releases.ubuntu.com/18.04/ubuntu-18.04.6-desktop-amd64.iso
     fi
 fi
 
@@ -20,7 +20,7 @@ if [ ! -f illixr.qcow2 ]; then
     $QEMU_PATH/qemu-img create -f qcow2 illixr.qcow2 30G
 fi
 
-if [ ! -f ubuntu-18.04.5-desktop-amd64.iso ]; then
+if [ ! -f ubuntu-18.04.6-desktop-amd64.iso ]; then
 	# Ubuntu image doesn't exist anymore, so launch without the CDROM option
     $QEMU_PATH/x86_64-softmmu/qemu-system-x86_64 -enable-kvm -M q35 -smp 2 -m 4G \
         -hda illixr.qcow2 \
@@ -31,12 +31,12 @@ if [ ! -f ubuntu-18.04.5-desktop-amd64.iso ]; then
 else
 	# Ubuntu image exists, so launch with CDROM option (user may be installing Ubuntu or just never removed the image)
 	echo "Running with CDROM"
-	echo "Once you've finished installing Ubuntu, it's safe to delete ubuntu-18.04.5-desktop-amd64.iso"
+	echo "Once you've finished installing Ubuntu, it's safe to delete ubuntu-18.04.6-desktop-amd64.iso"
 	$QEMU_PATH/x86_64-softmmu/qemu-system-x86_64 -enable-kvm -M q35 -smp 2 -m 4G \
         -hda illixr.qcow2 \
         -net nic,model=virtio \
         -net user,hostfwd=tcp::2222-:22 \
         -vga virtio \
-        -cdrom ubuntu-18.04.5-desktop-amd64.iso \
+        -cdrom ubuntu-18.04.6-desktop-amd64.iso \
         -display sdl,gl=on
 fi
