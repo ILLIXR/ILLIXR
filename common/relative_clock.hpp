42001d6758b7145f12a8b0b978d6d5dbc7dd63f6
Samuel Grayson <sam@samgrayson.me>
2022-01-10 12:31:50 -0600

    Started time_point

--- common/relative_clock.hpp
+++ common/relative_clock.hpp
@@ -6,6 +6,33 @@
 namespace ILLIXR {
 
 /**
+ * Mimick of `std::chrono::time_point<Clock, Rep>` [1].
+ *
+ * Can't use `std::chrono::time_point<Clock, Rep>`, because the `Clock` must satisfy the named
+ * requirements of the Clock interface [2], but `RelativeClock` cannot for the aforementioned
+ * reasons. Instead, we will mimick the interface of [1] here.
+ *
+ * [1]: https://en.cppreference.com/w/cpp/chrono/time_point
+ * [2]: https://en.cppreference.com/w/cpp/named_req/Clock
+ */
+class time_point<class duration = std::chrono::duration<long, std::nano>> {
+public:
+	constexpr time_point() { }
+	constexpr explicit time_point(const duration& time_since_epoch) : _m_time_since_epoch{time_since_epoch} { }
+	template<class Duration2>
+	constexpr time_point(const time_point<duration2>& t) : _m_time_since_epoch{std::chrono::duration_cast<duration>(t._m_time_since_epoch)} { }
+	duration time_since_epoch() const { return _m_time_since_epoch; }
+private:
+	duration _m_time_since_epoch;
+};
+
+template<class D1, class D2>
+constexpr typename std::common_type<D1,D2>::type
+operator-(const time_point<D1>& lhs, const time_point<D2>& rhs) {
+	return lhs.time_since_epoch() - rhs.time_since_epoch();
+}
+
+/**
  * @brief Relative clock for all of ILLIXR
  *
  * Please use this instead of std::chrono clocks; this way, you can fake real time without changing your code.
@@ -20,10 +47,11 @@ namespace ILLIXR {
  */
 class RelativeClock : public phonebook::service {
 public:
+
 	using rep = long;
 	using period = std::nano;
 	using duration = std::chrono::duration<rep, period>;
-	using time_point = std::chrono::time_point<RelativeClock>;
+	using time_point = time_point<duration>;
 	static constexpr bool is_steady = true;
 	static_assert(std::chrono::steady_clock::is_steady);
 
@@ -47,7 +75,6 @@ private:
 	std::chrono::steady_clock::time_point _m_start;
 };
 
-using time_point = RelativeClock::time_point;
 using duration = RelativeClock::duration;
 
 template <typename unit = std::ratio<1>> double duration2double(duration dur) {
