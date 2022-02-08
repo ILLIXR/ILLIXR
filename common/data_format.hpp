4ee04ea83d10f2e93ca93f4f9a74cc756f59a3fd
Qinjun Jiang <qinjunj2@illinois.edu>
2022-02-08 17:06:06 -0600

    WIP separate imu and cam

--- common/data_format.hpp
+++ common/data_format.hpp
@@ -24,22 +24,48 @@ namespace ILLIXR {
 	// Data type that combines the IMU and camera data at a certain timestamp.
 	// If there is only IMU data for a certain timestamp, img0 and img1 will be null
 	// time is the current UNIX time where dataset_time is the time read from the csv
-	struct imu_cam_type : public switchboard::event {
+	// struct imu_cam_type : public switchboard::event {
+	// 	time_point time;
+	// 	Eigen::Vector3f angular_v;
+	// 	Eigen::Vector3f linear_a;
+	// 	std::optional<cv::Mat> img0;
+	// 	std::optional<cv::Mat> img1;
+	// 	imu_cam_type(time_point time_,
+	// 				 Eigen::Vector3f angular_v_,
+	// 				 Eigen::Vector3f linear_a_,
+	// 				 std::optional<cv::Mat> img0_,
+	// 				 std::optional<cv::Mat> img1_)
+	// 		: time{time_}
+	// 		, angular_v{angular_v_}
+	// 		, linear_a{linear_a_}
+	// 		, img0{img0_}
+	// 		, img1{img1_}
+	// 	{ }
+	// };
+	struct cam_type : switchboard::event {
+		time_point time;
+		cv::Mat img0;
+		cv::Mat img1;
+		cam_type(
+				 time_point _time,
+				 cv::Mat _img0,
+				 cv::Mat _img1)
+			: time{_time}
+			, img0{_img0}
+			, img1{_img1}
+		{ }
+	};
+
+	struct imu_type : switchboard::event {
 		time_point time;
 		Eigen::Vector3f angular_v;
 		Eigen::Vector3f linear_a;
-		std::optional<cv::Mat> img0;
-		std::optional<cv::Mat> img1;
-		imu_cam_type(time_point time_,
+		imu_type(time_point time_,
 					 Eigen::Vector3f angular_v_,
-					 Eigen::Vector3f linear_a_,
-					 std::optional<cv::Mat> img0_,
-					 std::optional<cv::Mat> img1_)
+					 Eigen::Vector3f linear_a_)
 			: time{time_}
 			, angular_v{angular_v_}
 			, linear_a{linear_a_}
-			, img0{img0_}
-			, img1{img1_}
 		{ }
 	};
 
---
+++ offline_imu/plugin.cpp
@@ -0,0 +1,89 @@
+#include "common/switchboard.hpp"
+#include "common/data_format.hpp"
+#include "data_loading.hpp"
+#include "common/data_format.hpp"
+#include "common/threadloop.hpp"
+#include "common/relative_clock.hpp"
+#include "common/managed_thread.hpp"
+
+using namespace ILLIXR;
+
+class offline_imu : public ILLIXR::threadloop {
+public:
+	offline_imu(std::string name_, phonebook* pb_)
+		: threadloop{name_, pb_}
+		, _m_sensor_data{load_data()}
+		, _m_sensor_data_it{_m_sensor_data.cbegin()}
+		, _m_sb{pb->lookup_impl<switchboard>()}
+		, _m_imu_cam{_m_sb->get_writer<imu_type>("imu")}
+		, dataset_first_time{_m_sensor_data_it->first}
+		, dataset_now{0}
+		, imu_cam_log{record_logger_}
+		, _m_rtc{pb->lookup_impl<RelativeClock>()}
+		, _m_log{"metrics/offline_imu.csv"}
+	{
+		_m_log << "offline_imu_put\n";
+	}
+
+protected:
+	// virtual void _p_thread_setup() override {
+	// 	if (is_dynamic_scheduler() || is_static_scheduler()) {
+	// 		set_priority(get_tid(), 5);
+	// 	}
+	// }
+	virtual skip_option _p_should_skip() override {
+		if (_m_sensor_data_it != _m_sensor_data.end()) {
+			assert(dataset_now < _m_sensor_data_it->first);
+			dataset_now = _m_sensor_data_it->first;
+			// Sleep for the difference between the current IMU vs 1st IMU and current UNIX time vs UNIX time the component was init
+			std::this_thread::sleep_for(
+				std::chrono::nanoseconds{dataset_now - dataset_first_time}
+				- _m_rtc->now().time_since_epoch()
+			);
+
+			return skip_option::run;
+
+		} else {
+			return skip_option::stop;
+		}
+	}
+
+	virtual void _p_one_iteration() override {
+		assert(_m_sensor_data_it != _m_sensor_data.end());
+		//std::cerr << " IMU time: " << std::chrono::time_point<std::chrono::nanoseconds>(std::chrono::nanoseconds{dataset_now}).time_since_epoch().count() << std::endl;
+		time_point real_now(std::chrono::nanoseconds{dataset_now - dataset_first_time});
+		// CPU_TIMER_TIME_EVENT_INFO(false, false, "entry", cpu_timer::make_type_eraser<FrameInfo>(std::to_string(id), "imu", 0, real_now));
+		const sensor_types& sensor_datum = _m_sensor_data_it->second;
+
+		_m_log << std::chrono::nanoseconds{std::chrono::steady_clock::now().time_since_epoch()}.count() << '\n';
+
+		_m_imu_cam.put(_m_imu_cam.allocate<imu_type>(
+			imu_type {
+				real_now,
+				(sensor_datum.imu0.angular_v).cast<float>(),
+				(sensor_datum.imu0.linear_a).cast<float>()
+			}
+		));
+		++_m_sensor_data_it;
+	}
+
+private:
+	const std::map<ullong, sensor_types> _m_sensor_data;
+	std::map<ullong, sensor_types>::const_iterator _m_sensor_data_it;
+	const std::shared_ptr<switchboard> _m_sb;
+	switchboard::writer<imu_type> _m_imu_cam;
+
+	// Timestamp of the first IMU value from the dataset
+	ullong dataset_first_time;
+	// Current IMU timestamp
+	ullong dataset_now;
+
+	record_coalescer imu_cam_log;
+
+	std::shared_ptr<RelativeClock> _m_rtc;
+
+	std::ofstream _m_log;
+};
+
+PLUGIN_MAIN(offline_imu)
+
