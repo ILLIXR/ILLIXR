5af76af0fbbc424369aee8df1a43944960639fc2
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2022-04-07 12:57:49 -0500

    Server fix for zed pt1

--- offloaded_vio/server_rx/plugin.cpp
+++ offloaded_vio/server_rx/plugin.cpp
@@ -37,14 +37,12 @@ public:
 
 private:
 	void ReceiveVioInput(const vio_input_proto::IMUCamVec& vio_input) {	
-		std::cout << "Num imu vals: " << vio_input.imu_cam_data_size() << std::endl;
-
+		
 		// Loop through all IMU values first then the cam frame	
 		for (int i = 0; i < vio_input.imu_cam_data_size(); i++) {
 			vio_input_proto::IMUCamData curr_data = vio_input.imu_cam_data(i);
 
 			if (curr_data.rows() == -1 && curr_data.cols() == -1) {
-				std::cout << "IMU: " << i << std::endl;
 				std::optional<cv::Mat> cam0 = std::nullopt;
 				std::optional<cv::Mat> cam1 = std::nullopt;
 
@@ -60,41 +58,18 @@ private:
 				));
 
 			} else {
-				std::cout << "IMU: " << i << std::endl;
-				std::cout << "Cam Frame" << std::endl;
+				size_t img0_size = curr_data.rows() * curr_data.cols();
+				char *img0_buffer = new char[img0_size + 1];
+				memcpy(img0_buffer, curr_data.img0_data().c_str(), img0_size + 1);
+				img0_buffer[img0_size] = '\0';
 
-				unsigned char* img0_data = (unsigned char*) curr_data.img0_data().c_str();
-				unsigned char* img1_data = (unsigned char*) curr_data.img1_data().c_str();
+				size_t img1_size = curr_data.rows() * curr_data.cols();
+				char *img1_buffer = new char[img1_size + 1];
+				memcpy(img1_buffer, curr_data.img1_data().c_str(), img1_size + 1);
+				img1_buffer[img1_size] = '\0';
 
-				cv::Mat img0(curr_data.rows(), curr_data.cols(), 0, img0_data);
-				cv::Mat img1(curr_data.rows(), curr_data.cols(), 0, img1_data);	
-
-				// std::vector<uint8_t> img0_vec(curr_data.img0_data().begin(),curr_data.img0_data().end());
-				// std::vector<uint8_t> img1_vec(curr_data.img1_data().begin(),curr_data.img1_data().end());
-
-				// // Verified that these dont have to be pointers
-				// cv::Mat img0(curr_data.rows(), curr_data.cols(), CV_8UC1, img0_vec.data());
-				// cv::Mat img1(curr_data.rows(), curr_data.cols(), CV_8UC1, img1_vec.data());
-
-				// std::cout << "TEST " << curr_data.img0_data().size() << std::endl;
-
-				// const std::string::size_type img0_size = curr_data.img0_data().size();
-				// char *img0_buffer = new char[img0_size + 1];
-				// memcpy(img0_buffer, curr_data.img0_data().c_str(), img0_size + 1);
-				// img0_buffer[img0_size] = '\0';
-
-				// const std::string::size_type img1_size = curr_data.img1_data().size();
-				// char *img1_buffer = new char[img1_size + 1];
-				// memcpy(img1_buffer, curr_data.img1_data().c_str(), img1_size + 1);
-				// img1_buffer[img1_size] = '\0';
-
-				// cv::Mat img0(curr_data.rows(), curr_data.cols(), CV_8UC1, img0_buffer);
-				// cv::Mat img1(curr_data.rows(), curr_data.cols(), CV_8UC1, img1_buffer);
-
-				cv::imshow("test1", img0);
-				cv::waitKey(0);
-				cv::imshow("test2", img1);
-				cv::waitKey(0);
+				cv::Mat img0(curr_data.rows(), curr_data.cols(), CV_8UC1, img0_buffer);
+				cv::Mat img1(curr_data.rows(), curr_data.cols(), CV_8UC1, img1_buffer);
 
 				std::optional<cv::Mat> cam0 = std::make_optional<cv::Mat>(img0);
 				std::optional<cv::Mat> cam1 = std::make_optional<cv::Mat>(img1);
