e3d7ec04cf48c9866ffa0d1673e0c33b70e798d5
Qinjun Jiang <qinjunj2@illinois.edu>
2022-05-19 14:41:56 -0500

    simplify load_camera_images

--- debugview/plugin.cpp
+++ debugview/plugin.cpp
@@ -260,39 +260,24 @@ public:
 	bool load_camera_images() {
         RAC_ERRNO_MSG("debugview at start of load_camera_images");
 
-		cam_buffer = _m_cam.size() == 0 ? nullptr : _m_cam.dequeue(); 
-		if (cam_buffer == nullptr) {
+		cam = _m_cam.size() == 0 ? nullptr : _m_cam.dequeue(); 
+		if (cam == nullptr) {
 			return false;
 		}
 
-		if (true) {
-			glBindTexture(GL_TEXTURE_2D, camera_textures[0]);
-			cv::Mat img0{cam_buffer->img0.clone()};
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, img0.cols, img0.rows, 0, GL_RED, GL_UNSIGNED_BYTE, img0.ptr());
-			camera_texture_sizes[0] = Eigen::Vector2i(img0.cols, img0.rows);
-			GLint swizzleMask[] = {GL_RED, GL_RED, GL_RED, GL_RED};
-			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
-		} else {
-			std::cerr << "img0 has no value!" << std::endl;
-			glBindTexture(GL_TEXTURE_2D, camera_textures[0]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, TEST_PATTERN_WIDTH, TEST_PATTERN_HEIGHT, 0, GL_RED, GL_UNSIGNED_BYTE, &(test_pattern[0][0]));
-			glFlush();
-			camera_texture_sizes[0] = Eigen::Vector2i(TEST_PATTERN_WIDTH, TEST_PATTERN_HEIGHT);
-		}
-		
-		if (true) {
-			glBindTexture(GL_TEXTURE_2D, camera_textures[1]);
-			cv::Mat img1{cam_buffer->img1.clone()};    /// <- Adding this here to simulate the copy
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, img1.cols, img1.rows, 0, GL_RED, GL_UNSIGNED_BYTE, img1.ptr());
-			camera_texture_sizes[1] = Eigen::Vector2i(img1.cols, img1.rows);
-			GLint swizzleMask[] = {GL_RED, GL_RED, GL_RED, GL_RED};
-			glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
-		} else {
-			glBindTexture(GL_TEXTURE_2D, camera_textures[1]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, TEST_PATTERN_WIDTH, TEST_PATTERN_HEIGHT, 0, GL_RED, GL_UNSIGNED_BYTE, &(test_pattern[0][0]));
-			glFlush();
-			camera_texture_sizes[1] = Eigen::Vector2i(TEST_PATTERN_WIDTH, TEST_PATTERN_HEIGHT);
-		}
+		glBindTexture(GL_TEXTURE_2D, camera_textures[0]);
+		cv::Mat img0{cam->img0.clone()};
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, img0.cols, img0.rows, 0, GL_RED, GL_UNSIGNED_BYTE, img0.ptr());
+		camera_texture_sizes[0] = Eigen::Vector2i(img0.cols, img0.rows);
+		GLint swizzleMask[] = {GL_RED, GL_RED, GL_RED, GL_RED};
+		glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask);
+
+		glBindTexture(GL_TEXTURE_2D, camera_textures[1]);
+		cv::Mat img1{cam->img1.clone()};    /// <- Adding this here to simulate the copy
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, img1.cols, img1.rows, 0, GL_RED, GL_UNSIGNED_BYTE, img1.ptr());
+		camera_texture_sizes[1] = Eigen::Vector2i(img1.cols, img1.rows);
+		GLint swizzleMask1[] = {GL_RED, GL_RED, GL_RED, GL_RED};
+		glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask1);
 
 		RAC_ERRNO_MSG("debugview at end of load_camera_images");
 		return true;
@@ -450,7 +435,7 @@ private:
 	Eigen::Vector3f tracking_position_offset = Eigen::Vector3f{0.0f, 0.0f, 0.0f};
 
 	// switchboard::ptr<const imu_cam_type> last_datum_with_images;
-	switchboard::ptr<const cam_type> cam_buffer;
+	switchboard::ptr<const cam_type> cam;
 	// std::vector<std::optional<cv::Mat>> camera_data = {std::nullopt, std::nullopt};
 	GLuint camera_textures[2];
 	Eigen::Vector2i camera_texture_sizes[2] = {Eigen::Vector2i::Zero(), Eigen::Vector2i::Zero()};
