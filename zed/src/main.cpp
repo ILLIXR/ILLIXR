4b0871d968eab61b3d87a8f00637103e33b773e1
Jeffrey Zhang <jeffrey.zh4ng@gmail.com>
2022-05-26 17:10:47 -0500

    Change zed to new datatype

--- zed/src/main.cpp
+++ zed/src/main.cpp
@@ -223,7 +223,6 @@ protected:
             time_point{},
             time_point{},
             time_point{},
-            _m_clock->now().time_since_epoch().count(),
             av,
             la,
             img0,
