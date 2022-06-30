1d407cc0c27a0c90f844cbf03f0e9d0752fda8c5
William Sentosa <sentosa2@illinois.edu>
2022-06-30 18:25:45 -0500

    add modules related to networking

---
+++ common/network/timestamp.hpp
@@ -0,0 +1,33 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef TIMESTAMP_HPP
+#define TIMESTAMP_HPP
+
+#include <cstdint>
+#include <ctime>
+
+#include "exception.hpp"
+
+uint64_t raw_timestamp( void )
+{
+    timespec ts;
+    SystemCall( "clock_gettime", clock_gettime( CLOCK_REALTIME, &ts ) );
+
+    uint64_t millis = ts.tv_nsec / 1000000;
+    millis += uint64_t( ts.tv_sec ) * 1000;
+
+    return millis;
+}
+
+uint64_t initial_timestamp( void )
+{
+    static uint64_t initial_value = raw_timestamp();
+    return initial_value;
+}
+
+uint64_t timestamp( void )
+{
+    return raw_timestamp() - initial_timestamp();
+}
+
+#endif /* TIMESTAMP_HH */
