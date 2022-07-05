a4b1f75aaedd83738b70f740d9cd20269abb0ef3
Muhammad Huzaifa <huzaifa2@illinois.edu>
2022-07-05 01:35:08 -0500

    Add display parameters to global_module_defs

--- common/global_module_defs.hpp
+++ common/global_module_defs.hpp
@@ -1,28 +1,34 @@
-/*
-This is the file where default values are defined
-*/
+// Common parameters. Ultimately, these need to be moved to a yaml file.
 
 #pragma once
 
 #include <string>
 #include <stdexcept>
 
+#include "relative_clock.hpp"
 
 namespace ILLIXR {
 	
-#ifndef FB_WIDTH
-#define FB_WIDTH FB_WIDTH
+// Display width in pixels
+static constexpr unsigned display_width = 2560;
 
-//Setting default Framebuffer width
-static constexpr int FB_WIDTH = 2560; //Pixels
-#endif
+// Display height in pixels
+static constexpr unsigned display_height = 1440;
 
-#ifndef FB_HEIGHT
-#define FB_HEIGHT FB_HEIGHT
+// Display refresh rate in Hz
+static constexpr unsigned display_frequency = 120;
 
-//Setting default framebuffer height
-static constexpr int FB_HEIGHT = 1440; //Pixels
-#endif //FB_HEIGHT
+// Display period in nanoseconds
+static constexpr duration display_period = freq2period(display_frequency);
+
+// Display horizontal field-of-view in degrees
+static constexpr float display_fov_x = 45.0;
+
+// Display vertical field-of-view in degrees
+static constexpr float display_fov_y = 45.0;
+
+// Inter-pupilary distance (ipd) in meters
+static constexpr float ipd = 0.064f;
 
 /**
  * @brief Convert a string containing a (python) boolean to the bool type
