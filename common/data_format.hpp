606ad1611942fa7aff63858e2b195966005d28fd
William Sentosa <sentosa2@illinois.edu>
2022-07-21 17:52:34 -0500

    add put_to_consume metric, add frame_info metric that shows the freshness of the frame and whether a frame is dropped, change server_tx client_rx communication using tcp socket

--- common/data_format.hpp
+++ common/data_format.hpp
@@ -18,157 +18,336 @@
 
 namespace ILLIXR {
 
-using ullong = unsigned long long;
+	using ullong = unsigned long long;
 
-struct cam_type : switchboard::event {
-    time_point time;
-    cv::Mat    img0;
-    cv::Mat    img1;
+	// Data type that combines the IMU and camera data at a certain timestamp.
+	// If there is only IMU data for a certain timestamp, img0 and img1 will be null
+	// time is the current UNIX time where dataset_time is the time read from the csv
+	struct imu_cam_type : public switchboard::event {
+		time_point time;
+		Eigen::Vector3f angular_v;
+		Eigen::Vector3f linear_a;
+		std::optional<cv::Mat> img0;
+		std::optional<cv::Mat> img1;
+		imu_cam_type(time_point time_,
+					 Eigen::Vector3f angular_v_,
+					 Eigen::Vector3f linear_a_,
+					 std::optional<cv::Mat> img0_,
+					 std::optional<cv::Mat> img1_)
+			: time{time_}
+			, angular_v{angular_v_}
+			, linear_a{linear_a_}
+			, img0{img0_}
+			, img1{img1_}
+		{ }
+	};
 
-    cam_type(time_point _time, cv::Mat _img0, cv::Mat _img1)
-        : time{_time}
-        , img0{_img0}
-        , img1{_img1} { }
-};
+	struct imu_cam_type_prof : public switchboard::event {
+		int frame_id;
+		time_point time;
+		time_point start_time; // Time that the device sent the packet
+		time_point rec_time; // Time the device received the packet
+		time_point dataset_time; // Time from dataset
+		long int created_time; // Time the imu and camera data is put to the switchboard
+		Eigen::Vector3f angular_v;
+		Eigen::Vector3f linear_a;
+		std::optional<cv::Mat> img0;
+		std::optional<cv::Mat> img1;
+		imu_cam_type_prof(int frame_id_,
+					 time_point time_,
+					 time_point start_time_,
+					 time_point rec_time_,
+					 time_point dataset_time_,
+					 long int created_time_,
+					 Eigen::Vector3f angular_v_,
+					 Eigen::Vector3f linear_a_,
+					 std::optional<cv::Mat> img0_,
+					 std::optional<cv::Mat> img1_)
+			: frame_id{frame_id_}
+			, time{time_}
+			, start_time{start_time_}
+			, rec_time{rec_time_}
+			, dataset_time{dataset_time_}
+			, created_time{created_time_} 
+			, angular_v{angular_v_}
+			, linear_a{linear_a_}
+			, img0{img0_}
+			, img1{img1_}
+		{ }
+	};
 
-struct imu_type : switchboard::event {
-    time_point      time;
-    Eigen::Vector3d angular_v;
-    Eigen::Vector3d linear_a;
+	struct connection_signal : public switchboard::event {
+		bool start;
 
-    imu_type(time_point time_, Eigen::Vector3d angular_v_, Eigen::Vector3d linear_a_)
-        : time{time_}
-        , angular_v{angular_v_}
-        , linear_a{linear_a_} { }
-};
+		connection_signal(bool start_)
+			: start{start_}
+		{ }
+	};
 
-class rgb_depth_type : public switchboard::event {
-    [[maybe_unused]] time_point time;
-    cv::Mat                     rgb;
-    cv::Mat                     depth;
+	struct imu_type {
+		time_point timestamp;
+		Eigen::Matrix<double, 3, 1> wm;
+		Eigen::Matrix<double, 3, 1> am;
 
-public:
-    rgb_depth_type(time_point _time, cv::Mat _rgb, cv::Mat _depth)
-        : time{_time}
-        , rgb{_rgb}
-        , depth{_depth} { }
-};
 
-// Values needed to initialize the IMU integrator
-typedef struct {
-    double                      gyro_noise;
-    double                      acc_noise;
-    double                      gyro_walk;
-    double                      acc_walk;
-    Eigen::Matrix<double, 3, 1> n_gravity;
-    double                      imu_integration_sigma;
-    double                      nominal_rate;
-} imu_params;
+		imu_type(
+				time_point timestamp_,
+				Eigen::Matrix<double, 3, 1> wm_,
+				Eigen::Matrix<double, 3, 1> am_
+				)
+			: timestamp{timestamp_}
+			, wm{wm_}
+			, am{am_}
+		{ }
+	};
 
-// IMU biases, initialization params, and slow pose needed by the IMU integrator
-struct imu_integrator_input : public switchboard::event {
-    time_point last_cam_integration_time;
-    duration   t_offset;
-    imu_params params;
+    class rgb_depth_type : public switchboard::event {
+        [[maybe_unused]] time_point time;
+        std::optional<cv::Mat> rgb;
+        std::optional<cv::Mat> depth;
+	public:
+		rgb_depth_type(time_point _time,
+					   std::optional<cv::Mat> _rgb,
+					   std::optional<cv::Mat> _depth
+					   )
+			: time{_time}
+			, rgb{_rgb}
+			, depth{_depth}
+		{ }
+    };
 
-    Eigen::Vector3d             biasAcc;
-    Eigen::Vector3d             biasGyro;
-    Eigen::Matrix<double, 3, 1> position;
-    Eigen::Matrix<double, 3, 1> velocity;
-    Eigen::Quaterniond          quat;
+	// Values needed to initialize the IMU integrator
+	typedef struct {
+		double gyro_noise;
+		double acc_noise;
+		double gyro_walk;
+		double acc_walk;
+		Eigen::Matrix<double,3,1> n_gravity;
+		double imu_integration_sigma;
+		double nominal_rate;
+	} imu_params;
 
-    imu_integrator_input(time_point last_cam_integration_time_, duration t_offset_, imu_params params_,
-                         Eigen::Vector3d biasAcc_, Eigen::Vector3d biasGyro_, Eigen::Matrix<double, 3, 1> position_,
-                         Eigen::Matrix<double, 3, 1> velocity_, Eigen::Quaterniond quat_)
-        : last_cam_integration_time{last_cam_integration_time_}
-        , t_offset{t_offset_}
-        , params{params_}
-        , biasAcc{biasAcc_}
-        , biasGyro{biasGyro_}
-        , position{position_}
-        , velocity{velocity_}
-        , quat{quat_} { }
-};
+	// IMU biases, initialization params, and slow pose needed by the IMU integrator
+	struct imu_integrator_input : public switchboard::event {
+		time_point last_cam_integration_time;
+		duration t_offset;
+		imu_params params;
+		
+		Eigen::Vector3d biasAcc;
+		Eigen::Vector3d biasGyro;
+		Eigen::Matrix<double,3,1> position;
+		Eigen::Matrix<double,3,1> velocity;
+		Eigen::Quaterniond quat;
+		long int timestamp;
 
-// Output of the IMU integrator to be used by pose prediction
-struct imu_raw_type : public switchboard::event {
-    // Biases from the last two IMU integration iterations used by RK4 for pose predict
-    Eigen::Matrix<double, 3, 1> w_hat;
-    Eigen::Matrix<double, 3, 1> a_hat;
-    Eigen::Matrix<double, 3, 1> w_hat2;
-    Eigen::Matrix<double, 3, 1> a_hat2;
+		imu_integrator_input()
+			: last_cam_integration_time{time_point{}}
+			, t_offset{duration(std::chrono::milliseconds{-50})}
+			, params{.gyro_noise = 0.00016968,
+				.acc_noise = 0.002,
+				.gyro_walk = 1.9393e-05,
+				.acc_walk = 0.003,
+				.n_gravity = Eigen::Matrix<double,3,1>(0.0, 0.0, -9.81),
+				.imu_integration_sigma = 1.0,
+				.nominal_rate = 200.0,}
+			, biasAcc{Eigen::Vector3d{0, 0, 0}}
+			, biasGyro{Eigen::Vector3d{0, 0, 0}}
+			, position{Eigen::Vector3d{0, 0, 0}}
+			, velocity{Eigen::Vector3d{0, 0, 0}}
+			, quat{Eigen::Quaterniond{1, 0, 0, 0}}
+			, timestamp{0}
+		{ }
+		imu_integrator_input(
+							 time_point last_cam_integration_time_,
+							 duration t_offset_,
+							 imu_params params_,
+							 Eigen::Vector3d biasAcc_,
+							 Eigen::Vector3d biasGyro_,
+							 Eigen::Matrix<double,3,1> position_,
+							 Eigen::Matrix<double,3,1> velocity_,
+							 Eigen::Quaterniond quat_
+							 )
+			: last_cam_integration_time{last_cam_integration_time_}
+			, t_offset{t_offset_}
+			, params{params_}
+			, biasAcc{biasAcc_}
+			, biasGyro{biasGyro_}
+			, position{position_}
+			, velocity{velocity_}
+			, quat{quat_}
+			, timestamp{0}
+		{ }
+	};
 
-    // Faster pose propagated forwards by the IMU integrator
-    Eigen::Matrix<double, 3, 1> pos;
-    Eigen::Matrix<double, 3, 1> vel;
-    Eigen::Quaterniond          quat;
-    time_point                  imu_time;
+	// Output of the IMU integrator to be used by pose prediction
+	struct imu_raw_type : public switchboard::event {
+		// Biases from the last two IMU integration iterations used by RK4 for pose predict
+		Eigen::Matrix<double,3,1> w_hat;
+		Eigen::Matrix<double,3,1> a_hat;
+		Eigen::Matrix<double,3,1> w_hat2;
+		Eigen::Matrix<double,3,1> a_hat2;
 
-    imu_raw_type(Eigen::Matrix<double, 3, 1> w_hat_, Eigen::Matrix<double, 3, 1> a_hat_, Eigen::Matrix<double, 3, 1> w_hat2_,
-                 Eigen::Matrix<double, 3, 1> a_hat2_, Eigen::Matrix<double, 3, 1> pos_, Eigen::Matrix<double, 3, 1> vel_,
-                 Eigen::Quaterniond quat_, time_point imu_time_)
-        : w_hat{w_hat_}
-        , a_hat{a_hat_}
-        , w_hat2{w_hat2_}
-        , a_hat2{a_hat2_}
-        , pos{pos_}
-        , vel{vel_}
-        , quat{quat_}
-        , imu_time{imu_time_} { }
-};
+		// Faster pose propagated forwards by the IMU integrator
+		Eigen::Matrix<double,3,1> pos;
+		Eigen::Matrix<double,3,1> vel;
+		Eigen::Quaterniond quat;
+		time_point imu_time;
+		imu_raw_type(Eigen::Matrix<double,3,1> w_hat_,
+					 Eigen::Matrix<double,3,1> a_hat_,
+					 Eigen::Matrix<double,3,1> w_hat2_,
+					 Eigen::Matrix<double,3,1> a_hat2_,
+					 Eigen::Matrix<double,3,1> pos_,
+					 Eigen::Matrix<double,3,1> vel_,
+					 Eigen::Quaterniond quat_,
+					 time_point imu_time_)
+			: w_hat{w_hat_}
+			, a_hat{a_hat_}
+			, w_hat2{w_hat2_}
+			, a_hat2{a_hat2_}
+			, pos{pos_}
+			, vel{vel_}
+			, quat{quat_}
+			, imu_time{imu_time_}
+		{ }
+	};
 
-struct pose_type : public switchboard::event {
-    time_point         sensor_time; // Recorded time of sensor data ingestion
-    Eigen::Vector3f    position;
-    Eigen::Quaternionf orientation;
+	struct pose_type : public switchboard::event {
+		time_point sensor_time; // Recorded time of sensor data ingestion
+		Eigen::Vector3f position;
+		Eigen::Quaternionf orientation;
+		pose_type()
+			: sensor_time{time_point{}}
+			, position{Eigen::Vector3f{0, 0, 0}}
+			, orientation{Eigen::Quaternionf{1, 0, 0, 0}}
+		{ }
+		pose_type(time_point sensor_time_,
+				  Eigen::Vector3f position_,
+				  Eigen::Quaternionf orientation_)
+			: sensor_time{sensor_time_}
+			, position{position_}
+			, orientation{orientation_}
+		{ }
+	};
 
-    pose_type()
-        : sensor_time{time_point{}}
-        , position{Eigen::Vector3f{0, 0, 0}}
-        , orientation{Eigen::Quaternionf{1, 0, 0, 0}} { }
+	struct pose_type_prof : public switchboard::event {
+		int frame_id;
+		time_point sensor_time; // Recorded time of sensor data ingestion
+		time_point start_time; // Recorded time of transfer start
+		time_point rec_time; // When the server received the packet (in server time)
+		time_point dataset_time; // Sensor time
+		Eigen::Vector3f position;
+		Eigen::Quaternionf orientation;
+		pose_type_prof()
+			: frame_id{0}
+			, sensor_time{time_point{}}
+			, start_time{time_point{}}
+			, rec_time{time_point{}}
+			, dataset_time{time_point{}}
+			, position{Eigen::Vector3f{0, 0, 0}}
+			, orientation{Eigen::Quaternionf{1, 0, 0, 0}}
+		{ }
+		pose_type_prof(int frame_id_,
+				  time_point sensor_time_,
+				  time_point start_time_,
+				  time_point rec_time_,
+				  time_point dataset_time_,
+				  Eigen::Vector3f position_,
+				  Eigen::Quaternionf orientation_)
+			: frame_id{frame_id_}
+			, sensor_time{sensor_time_}
+			, start_time{start_time_}
+			, rec_time{rec_time_}
+			, dataset_time{dataset_time_}
+			, position{position_}
+			, orientation{orientation_}
+		{ }
+	};
 
-    pose_type(time_point sensor_time_, Eigen::Vector3f position_, Eigen::Quaternionf orientation_)
-        : sensor_time{sensor_time_}
-        , position{position_}
-        , orientation{orientation_} { }
-};
+	typedef struct {
+		pose_type pose;
+		time_point predict_computed_time; // Time at which the prediction was computed
+		time_point predict_target_time; // Time that prediction targeted.
+	} fast_pose_type;
 
-typedef struct {
-    pose_type  pose;
-    time_point predict_computed_time; // Time at which the prediction was computed
-    time_point predict_target_time;   // Time that prediction targeted.
-} fast_pose_type;
+	// Using arrays as a swapchain
+	// Array of left eyes, array of right eyes
+	// This more closely matches the format used by Monado
+	struct rendered_frame : public switchboard::event {
+		std::array<GLuint, 2> texture_handles; // Does not change between swaps in swapchain
+		std::array<GLuint, 2> swap_indices; // Which element of the swapchain
+		fast_pose_type render_pose; // The pose used when rendering this frame.
+		time_point sample_time;
+		time_point render_time;
+		rendered_frame() { }
+		rendered_frame(std::array<GLuint, 2>&& texture_handles_, 
+		               std::array<GLuint, 2>&& swap_indices_,
+		               fast_pose_type render_pose_,
+                       time_point sample_time_,
+                       time_point render_time_)
+            : texture_handles{std::move(texture_handles_)}
+			, swap_indices{std::move(swap_indices_)}
+			, render_pose(render_pose_)
+            , sample_time(sample_time_)
+            , render_time(render_time_)
+        { }
+	};
+	
+	struct hologram_input : public switchboard::event {
+		int seq;
+		hologram_input() { }
+		hologram_input(int seq_) : seq{seq_} { }
+	};
 
-// Used to identify which graphics API is being used (for swapchain construction)
-enum class graphics_api { OPENGL, VULKAN, TBD };
+	typedef struct {
+		int seq;		
+	} imu_integrator_seq;
 
-// Used to distinguish between different image handles
-enum class swapchain_usage { LEFT_SWAPCHAIN, RIGHT_SWAPCHAIN, LEFT_RENDER, RIGHT_RENDER, NA };
+	/* I use "accel" instead of "3-vector" as a datatype, because
+	this checks that you meant to use an acceleration in a certain
+	place. */
+	struct accel { };
 
-typedef struct vk_image_handle {
-    int      file_descriptor;
-    int64_t  format;
-    size_t   allocation_size;
-    uint32_t width;
-    uint32_t height;
+	// High-level HMD specification, timewarp plugin
+	// may/will calculate additional HMD info based on these specifications
+	struct hmd_physical_info {
+		float   ipd;
+		int		displayPixelsWide;
+		int		displayPixelsHigh;
+		float	chromaticAberration[4];
+		float	K[11];
+		int		visiblePixelsWide;
+		int		visiblePixelsHigh;
+		float	visibleMetersWide;
+		float	visibleMetersHigh;
+		float	lensSeparationInMeters;
+		float	metersPerTanAngleAtCenter;
+	};
 
-    vk_image_handle(int fd_, int64_t format_, size_t alloc_size, uint32_t width_, uint32_t height_)
-        : file_descriptor{fd_}
-        , format{format_}
-        , allocation_size{alloc_size}
-        , width{width_}
-        , height{height_} { }
-} vk_image_handle;
-
-// This is used to share swapchain images between ILLIXR and Monado.
-// When Monado uses its GL pipeline, it's enough to just share a context during creation.
-// Otherwise, file descriptors are needed to share the images.
-struct image_handle : public switchboard::event {
-    graphics_api type;
-
-    union {
-        GLuint          gl_handle;
-        vk_image_handle vk_handle;
+    struct texture_pose : public switchboard::event {
+        int seq; /// TODO: Should texture_pose.seq be a long long
+		duration offload_duration;
+        unsigned char *image;
+        time_point pose_time;
+        Eigen::Vector3f position;
+        Eigen::Quaternionf latest_quaternion;
+        Eigen::Quaternionf render_quaternion;
+        texture_pose() { }
+        texture_pose(
+            int seq_,
+            duration offload_duration_,
+            unsigned char *image_,
+            time_point pose_time_,
+            Eigen::Vector3f position_,
+			Eigen::Quaternionf latest_quaternion_,
+            Eigen::Quaternionf render_quaternion_
+        ) : seq{seq_}
+          , offload_duration{offload_duration_}
+          , image{image_}
+          , pose_time{pose_time_}
+          , position{position_}
+          , latest_quaternion{latest_quaternion_}
+          , render_quaternion{render_quaternion_}
+        { }
     };
 
     uint32_t        num_images;
--- offload_vio/device_rx/plugin.cpp
+++ offload_vio/device_rx/plugin.cpp
@@ -23,7 +23,7 @@ public:
 		, sb{pb->lookup_impl<switchboard>()}
 		, _m_pose{sb->get_writer<pose_type>("slow_pose")}
 		, _m_imu_integrator_input{sb->get_writer<imu_integrator_input>("imu_integrator_input")}
-		, client_addr(CLIENT_IP, CLIENT_PORT_2)
+		, server_addr(SERVER_IP, SERVER_PORT_2)
     { 
 		pose_type datum_pose_tmp{
             time_point{},
@@ -43,26 +43,46 @@ public:
 		roundtrip_csv.open(data_path + "/roundtrip_time.csv");
 		hashed.open(data_path + "/hash_device_rx.txt");
 
-		socket.bind(client_addr);
+		socket.set_reuseaddr();
+		socket.bind(Address(CLIENT_IP, CLIENT_PORT_2));
+		is_socket_connected = false;
 	}
 
 	virtual skip_option _p_should_skip() override {
-        return skip_option::run;
+        if (!is_socket_connected) {
+			cout << "device_rx: Connecting to " << server_addr.str(":") << endl;
+			socket.connect(server_addr);
+			cout << "device_rx: Connected to " << server_addr.str(":") << endl;
+			is_socket_connected = true;
+		}
+		return skip_option::run;
     }
 
 	void _p_one_iteration() override {
-		// May need to change the default UDP socket buffer size, e.g.,: 
-		// sudo sysctl -w net.core.rmem_max=26214400
-		// sudo sysctl -w net.core.rmem_default=26214400
-		pair<Address, string> datagram = socket.recv_from(); /* Blocking operation, wait for data to come */
-		string datagram_str = datagram.second;
-		vio_output_proto::VIOOutput vio_output;
-		bool success = vio_output.ParseFromString(datagram_str);
-		if (success) {
-			// cout << "Received vio output (" << datagram.size() << " bytes) from " << client_addr.str(":") << endl;
-			ReceiveVioOutput(vio_output, datagram_str);
-		} else {
-			cout << "Cannot parse VIO output!!" << endl;
+		if (is_socket_connected) {
+			auto now = timestamp();
+			string delimitter = "END!";
+			string recv_data = socket.read(); /* Blocking operation, wait for the data to come */
+			if (recv_data.size() > 0) {
+				buffer_str = buffer_str + recv_data;
+				string::size_type end_position = buffer_str.find(delimitter);
+				while (end_position != string::npos) {
+					string before = buffer_str.substr(0, end_position);
+					buffer_str = buffer_str.substr(end_position + delimitter.size());
+			
+					// process the data
+					vio_output_proto::VIOOutput vio_output;
+					bool success = vio_output.ParseFromString(before);
+					if (success) {
+						// cout << "Received vio output (" << datagram.size() << " bytes) from " << client_addr.str(":") << endl;
+						ReceiveVioOutput(vio_output, before);
+					} else {
+						cout << "client_rx: Cannot parse VIO output!!" << endl;
+					}
+					end_position = buffer_str.find(delimitter);
+				}
+				cout << "Recv time = " << timestamp() - now << ", size = " << recv_data.size() << endl;
+			}
 		}
 	}
 
@@ -99,34 +119,34 @@ private:
         switchboard::ptr<pose_type> datum_pose = _m_pose.allocate<pose_type>(std::move(datum_pose_tmp));
         _m_pose.put(std::move(datum_pose));
 
-        vio_output_proto::IMUIntInput imu_int_input = vio_output.imu_int_input();
+		vio_output_proto::IMUIntInput imu_int_input = vio_output.imu_int_input();
 
-        imu_integrator_input datum_imu_int_tmp{
-            time_point{std::chrono::nanoseconds{imu_int_input.last_cam_integration_time()}},
-            duration(std::chrono::nanoseconds{imu_int_input.t_offset()}),
-            imu_params{
-                imu_int_input.imu_params().gyro_noise(),
-                imu_int_input.imu_params().acc_noise(),
-                imu_int_input.imu_params().gyro_walk(),
-                imu_int_input.imu_params().acc_walk(),
-                Eigen::Matrix<double, 3, 1>{
-                    imu_int_input.imu_params().n_gravity().x(),
-                    imu_int_input.imu_params().n_gravity().y(),
-                    imu_int_input.imu_params().n_gravity().z(),
-                },
-                imu_int_input.imu_params().imu_integration_sigma(),
-                imu_int_input.imu_params().nominal_rate(),
-            },
-            Eigen::Vector3d{imu_int_input.biasacc().x(), imu_int_input.biasacc().y(), imu_int_input.biasacc().z()},
-            Eigen::Vector3d{imu_int_input.biasgyro().x(), imu_int_input.biasgyro().y(), imu_int_input.biasgyro().z()},
-            Eigen::Matrix<double, 3, 1>{imu_int_input.position().x(), imu_int_input.position().y(),
-                                        imu_int_input.position().z()},
-            Eigen::Matrix<double, 3, 1>{imu_int_input.velocity().x(), imu_int_input.velocity().y(),
-                                        imu_int_input.velocity().z()},
-            Eigen::Quaterniond{imu_int_input.rotation().w(), imu_int_input.rotation().x(), imu_int_input.rotation().y(),
-                               imu_int_input.rotation().z()}};
+		imu_integrator_input datum_imu_int_tmp{
+			time_point{std::chrono::nanoseconds{imu_int_input.last_cam_integration_time()}},
+			duration(std::chrono::nanoseconds{imu_int_input.t_offset()}),
+			imu_params{
+				imu_int_input.imu_params().gyro_noise(),
+				imu_int_input.imu_params().acc_noise(),
+				imu_int_input.imu_params().gyro_walk(),
+				imu_int_input.imu_params().acc_walk(),
+				Eigen::Matrix<double,3,1>{
+					imu_int_input.imu_params().n_gravity().x(),
+					imu_int_input.imu_params().n_gravity().y(),
+					imu_int_input.imu_params().n_gravity().z(),
+				},
+				imu_int_input.imu_params().imu_integration_sigma(),
+				imu_int_input.imu_params().nominal_rate(),
+			},
+			Eigen::Vector3d{imu_int_input.biasacc().x(), imu_int_input.biasacc().y(), imu_int_input.biasacc().z()},
+			Eigen::Vector3d{imu_int_input.biasgyro().x(), imu_int_input.biasgyro().y(), imu_int_input.biasgyro().z()},
+			Eigen::Matrix<double,3,1>{imu_int_input.position().x(), imu_int_input.position().y(), imu_int_input.position().z()},
+			Eigen::Matrix<double,3,1>{imu_int_input.velocity().x(), imu_int_input.velocity().y(), imu_int_input.velocity().z()},
+			Eigen::Quaterniond{imu_int_input.rotation().w(), imu_int_input.rotation().x(), imu_int_input.rotation().y(), imu_int_input.rotation().z()}
+		};
 
-        switchboard::ptr<imu_integrator_input> datum_imu_int =
+		datum_imu_int_tmp.timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+		switchboard::ptr<imu_integrator_input> datum_imu_int = 
             _m_imu_integrator_input.allocate<imu_integrator_input>(std::move(datum_imu_int_tmp));
         _m_imu_integrator_input.put(std::move(datum_imu_int));
     }
@@ -135,8 +155,10 @@ private:
     switchboard::writer<pose_type>            _m_pose;
     switchboard::writer<imu_integrator_input> _m_imu_integrator_input;
 
-	UDPSocket socket;
-	Address client_addr;
+	TCPSocket socket;
+	bool is_socket_connected;
+	Address server_addr;
+	string buffer_str;
 
 	const string data_path = filesystem::current_path().string() + "/recorded_data";
 	std::ofstream pose_transfer_csv;
