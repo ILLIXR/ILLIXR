b17098328b75e4bc231396d24d22229e5d3ae545
Qinjun Jiang <qinjunj2@illinois.edu>
2022-08-03 09:14:44 -0500

    WIP add prints, test aws server, packet dropping, and zed freq

--- ground_truth_slam/plugin.cpp
+++ ground_truth_slam/plugin.cpp
@@ -1,3 +1,8 @@
+#include <chrono>
+#include <iomanip>
+#include <thread>
+#include <filesystem>
+#include <fstream>
 #include "common/plugin.hpp"
 
 #include "common/data_format.hpp"
@@ -21,21 +26,28 @@ public:
 		, _m_sensor_data{load_data()}
 		, _m_dataset_first_time{_m_sensor_data.cbegin()->first}
 		, _m_first_time{true}
-	{ }
+	{
+		if (!std::filesystem::exists(data_path)) {
+			if (!std::filesystem::create_directory(data_path)) {
+				std::cerr << "Failed to create data directory.";
+			}
+		}
+		truth_csv.open(data_path + "/truth.csv");
+	}
 
+<<<<<<< HEAD
     virtual void start() override {
         plugin::start();
-        sb->schedule<imu_cam_type>(id, "imu_cam", [this](switchboard::ptr<const imu_cam_type> datum, std::size_t) {
-            this->feed_ground_truth(datum);
-        });
-    }
+			this->feed_ground_truth(datum);
+		});
+	}
 
-    void feed_ground_truth(switchboard::ptr<const imu_cam_type> datum) {
-        ullong rounded_time = datum->time.time_since_epoch().count() + _m_dataset_first_time;
-        auto   it           = _m_sensor_data.find(rounded_time);
+	void feed_ground_truth(switchboard::ptr<const imu_cam_type_prof> datum) {
+		ullong rounded_time = datum->time.time_since_epoch().count() + _m_dataset_first_time;
+		auto it = _m_sensor_data.find(rounded_time);
+>>>>>>> WIP add prints, test aws server, packet dropping, and zed freq
 
         if (it == _m_sensor_data.end()) {
-#ifndef NDEBUG
             std::cout << "True pose not found at timestamp: " << rounded_time << std::endl;
 #endif
             return;
@@ -83,6 +95,14 @@ public:
 		}
 
 		_m_true_pose.put(std::move(true_pose));
+		truth_csv << datum->time.time_since_epoch().count() << ","
+				  << true_pose->position.x() << ","
+				  << true_pose->position.y() << ","
+				  << true_pose->position.z() << ","
+				  << true_pose->orientation.w() << ","
+				  << true_pose->orientation.x() << ","
+				  << true_pose->orientation.y() << ","
+				  << true_pose->orientation.z() << std::endl;
 	}
 
 private:
@@ -90,9 +110,12 @@ private:
 	switchboard::writer<pose_type> _m_true_pose;
 
     switchboard::writer<switchboard::event_wrapper<Eigen::Vector3f>> _m_ground_truth_offset;
-    const std::map<ullong, sensor_types>                             _m_sensor_data;
-    ullong                                                           _m_dataset_first_time;
-    bool                                                             _m_first_time;
+	const std::map<ullong, sensor_types> _m_sensor_data;
+    ullong _m_dataset_first_time;
+    bool _m_first_time;
+
+	const std::string data_path = std::filesystem::current_path().string() + "/recorded_data";
+	std::ofstream truth_csv;
 };
 
 PLUGIN_MAIN(ground_truth_slam);
