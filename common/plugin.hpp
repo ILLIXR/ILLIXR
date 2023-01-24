afc3d05d1d181dbd47c421cc13a578282f3ca5e4
Mark Van Moer <mvanmoer@illinois.edu>
2023-01-24 09:37:43 -0600

    Mimicked deletions from PR 221, but not additions.

--- common/plugin.hpp
+++ common/plugin.hpp
@@ -1,22 +1,10 @@
 #pragma once
 
 #include "phonebook.hpp"
-#include "record_logger.hpp"
 
 namespace ILLIXR {
 
 using plugin_id_t = std::size_t;
-
-/*
- * This gets included, but it is functionally 'private'. Hence the double-underscores.
- */
-const record_header __plugin_start_header{
-    "plugin_name",
-    {
-        {"plugin_id", typeid(plugin_id_t)},
-        {"plugin_name", typeid(std::string)},
-    },
-};
 
 /**
  * @brief A dynamically-loadable plugin for Spindle.
@@ -30,13 +18,7 @@ public:
      * methods (due to structure of C++). See `threadloop` for an example of
      * this use-case.
      */
-    virtual void start() {
-        record_logger_->log(record{__plugin_start_header,
-                                   {
-                                       {id},
-                                       {name},
-                                   }});
-    }
+    virtual void start() { }
 
     /**
      * @brief A method which Spindle calls when it stops the component.
@@ -53,7 +35,6 @@ public:
     plugin(const std::string& name_, phonebook* pb_)
         : name{name_}
         , pb{pb_}
-        , record_logger_{pb->lookup_impl<record_logger>()}
         , gen_guid_{pb->lookup_impl<gen_guid>()}
         , id{gen_guid_->get()} { }
 
@@ -66,9 +47,7 @@ public:
 protected:
     std::string                          name;
     const phonebook*                     pb;
-    const std::shared_ptr<record_logger> record_logger_;
     const std::shared_ptr<gen_guid>      gen_guid_;
-    const std::size_t                    id;
 };
 
 #define PLUGIN_MAIN(plugin_class)                                \
