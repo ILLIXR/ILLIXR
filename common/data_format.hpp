31eb0eff3c47c4ee04fc25aa416f665cb38a2cee
Mark Van Moer <mvanmoer@illinois.edu>
2023-01-30 19:45:06 -0600

    added gpu_log class needed by timewarp_gl/plugin.cpp

--- common/data_format.hpp
+++ common/data_format.hpp
@@ -203,4 +203,13 @@ struct texture_pose : public switchboard::event {
         , latest_quaternion{latest_quaternion_}
         , render_quaternion{render_quaternion_} { }
 };
+
+class gpu_log {
+public:
+    GLuint64 elapsed_time;
+    gpu_log(GLuint64 elapsed_time_)
+        : elapsed_time{elapsed_time_}
+    {}
+};
+
 } // namespace ILLIXR
