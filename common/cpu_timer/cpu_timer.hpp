2c86b67dc1034c30bcdab061f9e611b242819704
Mark Van Moer <mvanmoer@illinois.edu>
2023-03-30 13:43:59 -0500

    Removed what appears to be old-style metrics code

--- common/cpu_timer/cpu_timer.hpp
+++ common/cpu_timer/cpu_timer.hpp
@@ -48,44 +48,6 @@
 #include "cpu_timer_internal.hpp"
 #include "global_state.hpp"
 
-#include <cstring>
-
-/**
- * @brief A C++ translation of [clock_gettime][1]
- *
- * [1]: https://linux.die.net/man/3/clock_gettime
- *
- */
-static inline std::chrono::nanoseconds cpp_clock_gettime(clockid_t clock_id) {
-    /* This ensures the compiler won't reorder this function call; Pretend like it has memory side-effects. */
-    asm volatile(""
-                 : /* OutputOperands */
-                 : /* InputOperands */
-                 : "memory" /* Clobbers */);
-    struct timespec ts;
-
-    RAC_ERRNO_MSG("cpu_timer before clock_gettime");
-
-    if (clock_gettime(clock_id, &ts)) {
-        throw std::runtime_error{std::string{"clock_gettime returned "} + strerror(errno)};
-    }
-    RAC_ERRNO_MSG("cpu_timer after clock_gettime");
-
-    asm volatile(""
-                 : /* OutputOperands */
-                 : /* InputOperands */
-                 : "memory" /* Clobbers */);
-    return std::chrono::seconds{ts.tv_sec} + std::chrono::nanoseconds{ts.tv_nsec};
-}
-
-/**
- * @brief Gets the CPU time for the calling thread.
- */
-static inline std::chrono::nanoseconds thread_cpu_time() {
-    RAC_ERRNO_MSG("cpu_timer before cpp_clock_gettime");
-    return cpp_clock_gettime(CLOCK_THREAD_CPUTIME_ID);
-}
-
 namespace cpu_timer {
 
 using Frames       = std::deque<detail::Frame>;
