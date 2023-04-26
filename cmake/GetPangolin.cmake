670bad0410717f7486ca4eb614a8e13ac92d2faa
astro-friedel <friededn@yahoo.com>
2023-04-26 12:24:06 -0500

    fixes for outdated, but needed pangolin

--- cmake/GetPangolin.cmake
+++ cmake/GetPangolin.cmake
@@ -5,6 +5,7 @@ if(NOT Pangolin_FOUND)
             GIT_REPOSITORY https://github.com/stevenlovegrove/Pangolin.git
             GIT_TAG dd801d244db3a8e27b7fe8020cd751404aa818fd
             PREFIX ${CMAKE_BINARY_DIR}/_deps/Pangolin
+            PATCH_COMMAND patch -p0 src/config.h.in < ${PROJECT_SOURCE_DIR}/cmake/pangolin.patch
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_CXX_FLAGS="-L${CMAKE_INSTALL_PREFIX}/lib" -DCMAKE_BUILD_TYPE=Release
             )
     set(Pangolin_EXTERNAL Yes)
