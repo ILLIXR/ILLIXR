be501b9ea98e8280a020ca30fc2ea72520fc2483
astro-friedel <friededn@yahoo.com>
2023-05-02 13:03:17 -0500

    more reconfiguration for opencv

--- cmake/GetDBoW2.cmake
+++ cmake/GetDBoW2.cmake
@@ -1,5 +1,10 @@
 find_package(DBoW2 QUIET)
 
+set(DBOW_CMAKE_ARGS "")
+if(HAVE_CENTOS)
+    set(DBOW_CMAKE_ARGS "-DOpenCV_DIR=${OpenCV_DIR}")
+endif()
+
 if(DBoW2_LIBRARIES)
     set(DBoW2_VERSION "0.0")
 else()
@@ -7,7 +12,8 @@ else()
             GIT_REPOSITORY https://github.com/dorian3d/DBoW2.git
             GIT_TAG 3924753db6145f12618e7de09b7e6b258db93c6e
             PREFIX ${CMAKE_BINARY_DIR}/_deps/DBoW2
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            DEPENDS ${OpenCV_DEP_STR}
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${DBOW_CMAKE_ARGS}
             )
     set(DBoW2_DEP_STR "DBoW2")
     set(DBoW2_EXTERNAL Yes)
