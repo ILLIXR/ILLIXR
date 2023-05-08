55bd6ee1be251c85da47ad617db2168be8e971c8
Jeffrey Liu <jeffliu604@gmail.com>
2023-05-08 16:45:00 -0500

    Remove unnecessary semaphores

--- timewarp_gl/plugin.cpp
+++ timewarp_gl/plugin.cpp
@@ -139,26 +139,15 @@ public:
                                        [this](switchboard::ptr<const semaphore_handle> handle, std::size_t) {
                                            // We need one semaphore to indicate when the reprojection is ready, and another when
                                            // it's done
-                                           static bool left_lsr_ready = false, right_lsr_ready = false;
                                            static bool left_lsr_complete = false, right_lsr_complete = false;
                                            switch (handle->usage) {
-                                           case semaphore_usage::LEFT_LSR_READY: {
-                                               _m_semaphore_handles[0][0] = *handle;
-                                               left_lsr_ready             = true;
-                                               break;
-                                           }
-                                           case semaphore_usage::RIGHT_LSR_READY: {
-                                               _m_semaphore_handles[1][0] = *handle;
-                                               right_lsr_ready            = true;
-                                               break;
-                                           }
                                            case semaphore_usage::LEFT_LSR_COMPLETE: {
-                                               _m_semaphore_handles[0][1] = *handle;
+                                               _m_semaphore_handles[0] = *handle;
                                                left_lsr_complete          = true;
                                                break;
                                            }
                                            case semaphore_usage::RIGHT_LSR_COMPLETE: {
-                                               _m_semaphore_handles[1][1] = *handle;
+                                               _m_semaphore_handles[1] = *handle;
                                                right_lsr_complete         = true;
                                                break;
                                            }
@@ -168,7 +157,7 @@ public:
                                            }
                                            }
 
-                                           if (left_lsr_ready && right_lsr_ready && left_lsr_complete && right_lsr_complete) {
+                                           if (left_lsr_complete && right_lsr_complete) {
                                                this->semaphore_handles_ready = true;
                                            }
                                        });
@@ -205,8 +194,8 @@ private:
 
     // Semaphores to synchronize between Monado and ILLIXR
     // Left and right; ready and complete.
-    std::array<std::array<semaphore_handle, 2>, 2> _m_semaphore_handles;
-    std::array<std::array<GLuint, 2>, 2>           _m_semaphores;
+    std::array<semaphore_handle, 2> _m_semaphore_handles;
+    std::array<GLuint, 2>           _m_semaphores;
 #endif
 
     // Switchboard plug for sending hologram calls
@@ -374,24 +363,12 @@ private:
         glImportSemaphoreFdEXT(semaphore_handle, GL_HANDLE_TYPE_OPAQUE_FD_EXT, vk_handle.vk_handle);
 
         switch (vk_handle.usage) {
-        case semaphore_usage::LEFT_LSR_READY: {
-            _m_semaphores[0][0] = semaphore_handle;
-            std::cout << "LEFT LSR READY HANDLE" << std::endl;
-            break;
-        }
-        case semaphore_usage::RIGHT_LSR_READY: {
-            _m_semaphores[1][0] = semaphore_handle;
-            std::cout << "RIGHT LSR READY HANDLE" << std::endl;
-            break;
-        }
         case semaphore_usage::LEFT_LSR_COMPLETE: {
-            _m_semaphores[0][1] = semaphore_handle;
-            std::cout << "LEFT LSR COMPLETE HANDLE" << std::endl;
+            _m_semaphores[0] = semaphore_handle;
             break;
         }
         case semaphore_usage::RIGHT_LSR_COMPLETE: {
-            _m_semaphores[1][1] = semaphore_handle;
-            std::cout << "RIGHT LSR COMPLETE HANDLE" << std::endl;
+            _m_semaphores[1] = semaphore_handle;
             break;
         }
         default: {
@@ -633,6 +610,8 @@ public:
         [[maybe_unused]] const bool gl_result = static_cast<bool>(glXMakeCurrent(dpy, root, glc));
         assert(gl_result && "glXMakeCurrent should not fail");
 
+        assert(semaphore_handles_ready.load());
+
         if (!rendering_ready) {
             assert(image_handles_ready);
 
@@ -655,10 +634,8 @@ public:
                 image_handle image = _m_eye_output_handles[eye];
                 ImportVulkanImage(image.vk_handle, image.usage);
 
-                // Each eye also has an associated semaphore for ready and complete
-                for (int usage = 0; usage < 2; usage++) {
-                    ImportVulkanSemaphore(_m_semaphore_handles[eye][usage]);
-                }
+                // Each eye also has an associated semaphore for complete.
+                ImportVulkanSemaphore(_m_semaphore_handles[eye]);
 #else
                 GLuint eye_output_texture;
                 glGenTextures(1, &eye_output_texture);
@@ -689,6 +666,9 @@ public:
     }
 
     void warp(switchboard::ptr<const rendered_frame> most_recent_frame) {
+        [[maybe_unused]] const bool gl_result_0 = static_cast<bool>(glXMakeCurrent(dpy, root, glc));
+        assert(gl_result_0 && "glXMakeCurrent should not fail");
+
         assert(semaphore_handles_ready && rendering_ready);
         std::cout << "TIMEWARP STARTING" << std::endl;
 
@@ -749,11 +729,6 @@ public:
         // Loop over each eye.
         for (int eye = 0; eye < HMD::NUM_EYES; eye++) {
             // Choose the appropriate texture to render to
-#ifdef ILLIXR_MONADO
-            // GLenum src_layout = GL_LAYOUT_COLOR_ATTACHMENT_EXT;
-            // glWaitSemaphoreEXT(_m_semaphores[eye][0], 0, nullptr, 1,
-            // &_m_eye_swapchains[eye][most_recent_frame->swapchain_indices[eye]], &src_layout);
-#endif
             glBindFramebuffer(GL_FRAMEBUFFER, _m_eye_framebuffers[eye]);
             glViewport(0, 0, display_params::width_pixels * 0.5, display_params::height_pixels);
             glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -810,7 +785,7 @@ public:
 
 #ifdef ILLIXR_MONADO
             GLenum dst_layout = GL_LAYOUT_SHADER_READ_ONLY_EXT;
-            glSignalSemaphoreEXT(_m_semaphores[eye][1], 0, nullptr, 1, &_m_eye_output_textures[eye], &dst_layout);
+            glSignalSemaphoreEXT(_m_semaphores[eye], 0, nullptr, 1, &_m_eye_output_textures[eye], &dst_layout);
 #endif
         }
 
