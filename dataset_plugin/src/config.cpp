f01307ff24b87e44f134dd4f184b7c5827917cc5
wermos <63574588+wermos@users.noreply.github.com>
2023-07-08 00:32:21 +0530

    Made a bunch of fixes and partially completed two functions.
    Added a missing include, fixed a bunch of copy-paste errors, and partially filled out the`initIMUConfig` and
    `initGroundTruthConfig` functions.

--- dataset_plugin/src/config.cpp
+++ dataset_plugin/src/config.cpp
@@ -1,14 +1,59 @@
+#include <iostream> // for std::cerr
 #include <string>
 
 #include "config.hpp"
 
 void ConfigParser::initIMUConfig() {
+    // parsing datatype-related info.
+    const char* datatype_env_var = std::getenv("ILLIXR_DATASET_IMU_DATATYPE");
+    if (!datatype_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_IMU_DATATYPE`." << std::endl;
+        ILLIXR::abort();
+    }
 
+    std::string datatype{datatype_env_var};
+
+    if (datatype == "float") {
+        config.imu_config.use_double = false;
+    }
+    // no need to do anything otherwise, since the other option is to use `double`, which is
+    // the default choice.
+
+    // parsing timestamp unit-related info.
+    const char* timestamp_units_env_var = std::getenv("ILLIXR_DATASET_IMU_TIMESTAMP_UNITS");
+    if (!timestamp_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_IMU_TIMESTAMP_UNITS`." << std::endl;
+        ILLIXR::abort();
+    }
+
+    std::string timestamp_units{timestamp_units_env_var};
+
+    if (timestamp_units == "seconds") {
+        config.imu_config.timestamp_unit = TimestampUnit::second;
+    } else if (timestamp_units == "milliseconds") {
+        config.imu_config.timestamp_unit = TimestampUnit::millisecond;
+    } else if (timestamp_units == "microseconds") {
+        config.imu_config.timestamp_unit = TimestampUnit::microsecond;
+    } else {
+        // nanoseconds
+        config.imu_config.timestamp_unit = TimestampUnit::nanosecond;
+    }
+
+    // parsing path-related info.
+    const char* path_env_var = std::getenv("ILLIXR_DATASET_IMU_PATH");
+    if (!path_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_IMU_PATH`." << std::endl;
+        ILLIXR::abort();
+    }
+
+    config.imu_config.path_list = convertPathStringToPathList(path_env_var);
+
+    // -----------------------------------------------------------------------------------------
 
     // parsing format-related info TODO: Finish this
-    const char* format_env_var = std::getenv("ILLIXR_DATASET_POSE_PATH");
-    if (!datatype_env_var) {
-        std::cerr << "Error: Please define `ILLIXR_DATASET_POSE_PATH`." << std::endl;
+    const char* format_env_var = std::getenv("ILLIXR_DATASET_IMU_FORMAT");
+    if (!format_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_IMU_FORMAT`." << std::endl;
         ILLIXR::abort();
     }
 }
@@ -16,7 +61,7 @@ void ConfigParser::initIMUConfig() {
 void ConfigParser::initImageConfig() {
     // parsing timestamp unit-related info.
     const char* timestamp_units_env_var = std::getenv("ILLIXR_DATASET_IMAGE_TIMESTAMP_UNITS");
-    if (!timestamp_env_var) {
+    if (!timestamp_units_env_var) {
         std::cerr << "Error: Please define `ILLIXR_DATASET_IMAGE_TIMESTAMP_UNITS`." << std::endl;
         ILLIXR::abort();
     }
@@ -82,7 +127,7 @@ void ConfigParser::initPoseConfig() {
 
     // parsing timestamp unit-related info.
     const char* timestamp_units_env_var = std::getenv("ILLIXR_DATASET_POSE_TIMESTAMP_UNITS");
-    if (!timestamp_env_var) {
+    if (!timestamp_units_env_var) {
         std::cerr << "Error: Please define `ILLIXR_DATASET_POSE_TIMESTAMP_UNITS`." << std::endl;
         ILLIXR::abort();
     }
@@ -102,22 +147,70 @@ void ConfigParser::initPoseConfig() {
 
     // parsing path-related info.
     const char* path_env_var = std::getenv("ILLIXR_DATASET_POSE_PATH");
-    if (!datatype_env_var) {
+    if (!path_env_var) {
         std::cerr << "Error: Please define `ILLIXR_DATASET_POSE_PATH`." << std::endl;
         ILLIXR::abort();
     }
 
-    config.pose_config.pose_path_list = convertPathStringToPathList(path_env_var);
+    config.pose_config.path_list = convertPathStringToPathList(path_env_var);
 }
 
 void ConfigParser::initGroundTruthConfig() {
+    // parsing datatype-related info.
+    const char* datatype_env_var = std::getenv("ILLIXR_DATASET_GROUND_TRUTH_DATATYPE");
+    if (!datatype_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_GROUND_TRUTH_DATATYPE`." << std::endl;
+        ILLIXR::abort();
+    }
 
+    std::string datatype{datatype_env_var};
+
+    if (datatype == "float") {
+        config.imu_config.use_double = false;
+    }
+    // no need to do anything otherwise, since the other option is to use `double`, which is
+    // the default choice.
+
+    // parsing timestamp unit-related info.
+    const char* timestamp_units_env_var = std::getenv("ILLIXR_DATASET_GROUND_TRUTH_TIMESTAMP_UNITS");
+    if (!timestamp_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_DATASET_GROUND_TRUTH_TIMESTAMP_UNITS`." << std::endl;
+        ILLIXR::abort();
+    }
+
+    std::string timestamp_units{timestamp_units_env_var};
+
+    if (timestamp_units == "seconds") {
+        config.ground_truth_config.timestamp_unit = TimestampUnit::second;
+    } else if (timestamp_units == "milliseconds") {
+        config.ground_truth_config.timestamp_unit = TimestampUnit::millisecond;
+    } else if (timestamp_units == "microseconds") {
+        config.ground_truth_config.timestamp_unit = TimestampUnit::microsecond;
+    } else {
+        // nanoseconds
+        config.ground_truth_config.timestamp_unit = TimestampUnit::nanosecond;
+    }
+
+    // parsing path-related info.
+    const char* path_env_var = std::getenv("ILLIXR_GROUND_TRUTH_PATH");
+    if (!path_env_var) {
+        std::cerr << "Error: Please define `ILLIXR_GROUND_TRUTH_PATH`." << std::endl;
+        ILLIXR::abort();
+    }
+
+    config.ground_truth_config.path = path_env_var;
+
+    // -----------------------------------------------------------------------------------------
+
+    // parsing format-related info not done
+
+    // parsing name-related info not done
 }
 
 void ConfigParser::initFromConfig() {
     // delimiter
-    const char* delimiter = std::getenv("ILLIXR_DATASET_DELIMITER");
-    if (!delimiter) {
+    const char* delimiter_env_var = std::getenv("ILLIXR_DATASET_DELIMITER");
+    if (!delimiter_env_var) {
         std::cerr << "Error: Please define `ILLIXR_DATASET_DELIMITER`." << std::endl;
         ILLIXR::abort();
     }
@@ -125,13 +218,13 @@ void ConfigParser::initFromConfig() {
     config.delimiter = delimiter[0];
 
     // root path
-    const char* root_path = std::getenv("ILLIXR_DATASET_ROOT_PATH");
-    if (!root_path) {
+    const char* root_path_env_var = std::getenv("ILLIXR_DATASET_ROOT_PATH");
+    if (!root_path_env_var) {
         std::cerr << "Error: Please define `ILLIXR_DATASET_ROOT_PATH`." << std::endl;
         ILLIXR::abort();
     }
 
-    config.root_path = root_path;
+    config.root_path = root_path_env_var;
 
     initIMUConfig();
 
