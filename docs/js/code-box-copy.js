40b5aabaf35b34d3c9caf77fc5c44aa2091054f6
astro-friedel <friedel@illinois.edu>
2023-07-19 14:17:07 -0500

    moved js and css files for plugin selector to new location

--- docs/js/code-box-copy.js
+++ docs/js/code-box-copy.js
@@ -71,3 +71,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         });
     };
 })(jQuery);
+
+$(document).ready(function () {
+    $('.code-box-copy').codeBoxCopy();
+});
