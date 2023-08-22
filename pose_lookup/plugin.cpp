30d7d8bacaf649de953fe7e934aa62852fab103c
Mark Van Moer <mvanmoer@illinois.edu>
2023-08-22 11:22:24 -0500

    Added explicit "[pose_lookup]" to log message.

--- pose_lookup/plugin.cpp
+++ pose_lookup/plugin.cpp
@@ -42,7 +42,7 @@ public:
         const switchboard::ptr<const switchboard::event_wrapper<time_point>> estimated_vsync =
             _m_vsync_estimate.get_ro_nullable();
         if (estimated_vsync == nullptr) {
-            spdlog::get("illixr")->info("Vsync estimation not valid yet, returning fast_pose for now()");
+            spdlog::get("illixr")->info("[pose_lookup] Vsync estimation not valid yet, returning fast_pose for now()");
             return get_fast_pose(_m_clock->now());
         } else {
             return get_fast_pose(**estimated_vsync);
@@ -130,14 +130,14 @@ public:
 
         if (nearest_row == _m_sensor_data.cend()) {
 #ifndef NDEBUG
-            spdlog::get("illixr")->debug("Time {} ({} + {}) after last datum {}", lookup_time,
+            spdlog::get("illixr")->debug("[pose_lookup] Time {} ({} + {}) after last datum {}", lookup_time,
                                      std::chrono::nanoseconds(time.time_since_epoch()).count(), dataset_first_time,
                                      _m_sensor_data.rbegin()->first);
 #endif
             nearest_row--;
         } else if (nearest_row == _m_sensor_data.cbegin()) {
 #ifndef NDEBUG
-            spdlog::get("illixr")->debug("Time {} ({} + {}) before first datum {}", lookup_time,
+            spdlog::get("illixr")->debug("[pose_lookup] Time {} ({} + {}) before first datum {}", lookup_time,
                                      std::chrono::nanoseconds(time.time_since_epoch()).count(), dataset_first_time,
                                      _m_sensor_data.cbegin()->first);
 #endif
