89731f5b826c617c4cb8587c24c955b2b84caeb7
Henry Che <daihung.che@gmail.com>
2023-08-25 13:47:46 -0500

    Remove pose prediction

--- openni/plugin.cpp
+++ openni/plugin.cpp
@@ -1,6 +1,5 @@
 #include "../common/data_format.hpp"
 #include "../common/phonebook.hpp"
-#include "../common/pose_prediction.hpp"
 #include "../common/relative_clock.hpp"
 #include "../common/switchboard.hpp"
 #include "../common/threadloop.hpp"
