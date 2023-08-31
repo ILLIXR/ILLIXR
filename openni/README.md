4282afcb20490dd6d92655f4f8fa5849900fd8eb
Henry Che <daihung.che@gmail.com>
2023-08-30 21:30:31 -0500

    Fix sleep time

--- openni/README.md
+++ openni/README.md
@@ -1,7 +1,37 @@
 # OpenNI
 
-## Debugging
+## Installation
 
+OpenNI should be install when running `install_deps.sh`. This will download OpenNI to `/opt/ILLIXR/`
+
+### Note: 
+
+If OpenNI is installed anywhere else, `OPENNI_ROOT` needs to be passed in as an environment variable before running ILLIXR. For example:
+
+```
+export OPENNI_ROOT=/path/to/openni
+./runner.sh configs/native-gl.yaml
+```
+
+### Important!
+
+Make sure to clean `openni` everytime you make any new changes related to OpenNI
+
+```
+cd ILLIXR/openni && make clean
+```
+
+## Set Camera Mode
+
+If you wish to switch up the video modes of OpenNI, update these two macros in `openni/plugin.cpp` accordingly:
+```
+#define RGB_MODE 0
+#define DEPTH_MODE 0
+```
+
+You can see the list of available modes once you run ILLIXR the first time. 
+
+## Debugging
 
 ```
 Device open failed: 	DeviceOpen using default: no devices found
@@ -18,10 +48,4 @@ Device open failed: 	Could not open "1d27/0601@3/2": Failed to open the USB devi
 ```
 **Solution**: Fix by running with sudo 
 
-### Note
 
-Make sure to clean openni everytime you make new changes
-
-```
-cd ILLIXR/openni && make clean
-```
--- openni/plugin.cpp
+++ openni/plugin.cpp
@@ -10,6 +10,9 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <OpenNI.h>
 
+#define RGB_MODE 0
+#define DEPTH_MODE 0
+
 using namespace ILLIXR;
 
 class openni_plugin : public ILLIXR::threadloop {
@@ -26,16 +29,16 @@ public:
     }
 
     virtual ~openni_plugin() override {
-        color.destroy();
-        depth.destroy();
+        _color.destroy();
+        _depth.destroy();
     }
 
 protected:
     virtual skip_option _p_should_skip() override {
         auto now = std::chrono::steady_clock::now();
-        cam_time = std::chrono::time_point_cast<std::chrono::milliseconds>(now).time_since_epoch().count();
-        if (cam_time > last_ts) {
-            std::this_thread::sleep_for(std::chrono::milliseconds{2});
+        _cam_time = std::chrono::time_point_cast<std::chrono::milliseconds>(now).time_since_epoch().count();
+        if (_cam_time > _last_ts) {
+            std::this_thread::sleep_for(std::chrono::milliseconds{_time_sleep});
             return skip_option::run;
         } else {
             return skip_option::skip_and_yield;
@@ -46,90 +49,93 @@ protected:
         RAC_ERRNO_MSG("openni at start of _p_one_iteration");
 
         // read cam data
-        color.readFrame(&colorFrame);
-        depth.readFrame(&depthFrame);
+        _color.readFrame(&_color_frame);
+        _depth.readFrame(&_depth_frame);
         // get timestamp
-        assert(colorFrame.getTimestamp() != depthFrame.getTimestamp());
+        assert(_color_frame.getTimestamp() != _depth_frame.getTimestamp());
 
         // convert to cv format
         cv::Mat colorMat;
-        colorMat.create(colorFrame.getHeight(), colorFrame.getWidth(), CV_8UC3);
-        const openni::RGB888Pixel* colorBuffer = (const openni::RGB888Pixel*) colorFrame.getData();
-        memcpy(colorMat.data, colorBuffer, 3 * colorFrame.getHeight() * colorFrame.getWidth() * sizeof(uint8_t));
+        colorMat.create(_color_frame.getHeight(), _color_frame.getWidth(), CV_8UC3);
+        const openni::RGB888Pixel* colorBuffer = (const openni::RGB888Pixel*) _color_frame.getData();
+        memcpy(colorMat.data, colorBuffer, 3 * _color_frame.getHeight() * _color_frame.getWidth() * sizeof(uint8_t));
         cv::cvtColor(colorMat, colorMat, cv::COLOR_BGR2BGRA);
         colorMat.convertTo(colorMat, CV_8UC4);
 
         cv::Mat depthMat;
-        depthMat.create(depthFrame.getHeight(), depthFrame.getWidth(), CV_16UC1);
-        const openni::DepthPixel* depthBuffer = (const openni::DepthPixel*) depthFrame.getData();
-        memcpy(depthMat.data, depthBuffer, depthFrame.getHeight() * depthFrame.getWidth() * sizeof(uint16_t));
+        depthMat.create(_depth_frame.getHeight(), _depth_frame.getWidth(), CV_16UC1);
+        const openni::DepthPixel* depthBuffer = (const openni::DepthPixel*) _depth_frame.getData();
+        memcpy(depthMat.data, depthBuffer, _depth_frame.getHeight() * _depth_frame.getWidth() * sizeof(uint16_t));
 
-        assert(cam_time);
+        assert(_cam_time);
         if (_m_first_time == 0) {
-            _m_first_time      = cam_time;
+            _m_first_time      = _cam_time;
             _m_first_real_time = _m_clock->now();
         }
-        time_point cam_time_point{_m_first_real_time + std::chrono::nanoseconds(cam_time - _m_first_time)};
-        _m_rgb_depth.put(_m_rgb_depth.allocate(cam_time_point, colorMat, depthMat));
+        time_point _cam_time_point{_m_first_real_time + std::chrono::nanoseconds(_cam_time - _m_first_time)};
+        _m_rgb_depth.put(_m_rgb_depth.allocate(_cam_time_point, colorMat, depthMat));
 
-        last_ts = cam_time;
+        _last_ts = _cam_time;
         RAC_ERRNO_MSG("openni at end of _p_one_iteration");
     }
 
     bool camera_initialize() {
         // initialize openni
-        rc = openni::OpenNI::initialize();
-        if (rc != openni::STATUS_OK)
+        _device_status = openni::OpenNI::initialize();
+        if (_device_status != openni::STATUS_OK)
             std::cout << "Initialize failed: " << openni::OpenNI::getExtendedError() << std::endl;
 
-        // open device
-        rc = device.open(openni::ANY_DEVICE);
-        if (rc != openni::STATUS_OK)
+        // open _device
+        _device_status = _device.open(openni::ANY_DEVICE);
+        if (_device_status != openni::STATUS_OK)
             std::cout << "Device open failed: " << openni::OpenNI::getExtendedError() << std::endl;
 
         /*_____________________________ DEPTH ___________________________*/
-        // create depth channel
-        rc = depth.create(device, openni::SENSOR_DEPTH);
-        if (rc != openni::STATUS_OK)
+        // create _depth channel
+        _device_status = _depth.create(_device, openni::SENSOR_DEPTH);
+        if (_device_status != openni::STATUS_OK)
             printf("Couldn't find depth stream:\n%s\n", openni::OpenNI::getExtendedError());
 
-        // get depth options
-        const openni::SensorInfo*               depthInfo  = device.getSensorInfo(openni::SENSOR_DEPTH);
+        // get _depth options
+        const openni::SensorInfo*               depthInfo  = _device.getSensorInfo(openni::SENSOR_DEPTH);
         const openni::Array<openni::VideoMode>& modesDepth = depthInfo->getSupportedVideoModes();
         for (int i = 0; i < modesDepth.getSize(); i++) {
             printf("Depth Mode %i: %ix%i, %i fps, %i format\n", i, modesDepth[i].getResolutionX(),
                    modesDepth[i].getResolutionY(), modesDepth[i].getFps(), modesDepth[i].getPixelFormat());
         }
-        rc = depth.setVideoMode(modesDepth[0]);
-        if (openni::STATUS_OK != rc)
+        _device_status = _depth.setVideoMode(modesDepth[DEPTH_MODE]);
+        if (openni::STATUS_OK != _device_status)
             std::cout << "error: depth fromat not supprted..." << std::endl;
-        // start depth stream
-        rc = depth.start();
-        if (rc != openni::STATUS_OK)
-            std::cout << "Couldn't start the depth stream" << openni::OpenNI::getExtendedError() << std::endl;
+        // start _depth stream
+        _device_status = _depth.start();
+        if (_device_status != openni::STATUS_OK)
+            std::cout << "Couldn't start the _depth stream" << openni::OpenNI::getExtendedError() << std::endl;
 
         /*_____________________________ COLOR ___________________________*/
-        // create color channel
-        rc = color.create(device, openni::SENSOR_COLOR);
-        if (rc != openni::STATUS_OK)
+        // create _color channel
+        _device_status = _color.create(_device, openni::SENSOR_COLOR);
+        if (_device_status != openni::STATUS_OK)
             printf("Couldn't find color stream:\n%s\n", openni::OpenNI::getExtendedError());
 
-        // get color options
-        const openni::SensorInfo*               colorInfo  = device.getSensorInfo(openni::SENSOR_COLOR);
+        // get _color options
+        const openni::SensorInfo*               colorInfo  = _device.getSensorInfo(openni::SENSOR_COLOR);
         const openni::Array<openni::VideoMode>& modesColor = colorInfo->getSupportedVideoModes();
         for (int i = 0; i < modesColor.getSize(); i++) {
             printf("Color Mode %i: %ix%i, %i fps, %i format\n", i, modesColor[i].getResolutionX(),
                    modesColor[i].getResolutionY(), modesColor[i].getFps(), modesColor[i].getPixelFormat());
         }
-        rc = color.setVideoMode(modesColor[0]);
-        if (openni::STATUS_OK != rc)
+        _device_status = _color.setVideoMode(modesColor[RGB_MODE]);
+        if (openni::STATUS_OK != _device_status)
             std::cout << "error: color format not supprted..." << std::endl;
-        // start color stream
-        rc = color.start();
-        if (rc != openni::STATUS_OK)
+        // start _color stream
+        _device_status = _color.start();
+        if (_device_status != openni::STATUS_OK)
             printf("Couldn't start color stream:\n%s\n", openni::OpenNI::getExtendedError());
 
-        return depth.isValid() && color.isValid();
+        int min_fps = std::min(modesColor[RGB_MODE].getFps(), modesDepth[DEPTH_MODE].getFps());
+        _time_sleep = static_cast<uint64_t>((1.0f / min_fps)*1000);
+        
+        return _depth.isValid() && _color.isValid();
     }
 
 private:
@@ -139,17 +145,17 @@ private:
     switchboard::writer<rgb_depth_type>        _m_rgb_depth;
 
     // OpenNI
-    openni::Status        rc = openni::STATUS_OK;
-    openni::Device        device;
-    openni::VideoStream   depth, color;
-    openni::VideoFrameRef depthFrame, colorFrame;
-    cv::VideoCapture      capture;
+    openni::Status        _device_status = openni::STATUS_OK;
+    openni::Device        _device;
+    openni::VideoStream   _depth, _color;
+    openni::VideoFrameRef _depth_frame, _color_frame;
 
     // timestamp
-    uint64_t   cam_time;
-    uint64_t   last_ts = 0;
+    uint64_t   _cam_time;
+    uint64_t   _last_ts = 0;
     uint64_t   _m_first_time;
     time_point _m_first_real_time;
+    uint64_t   _time_sleep;
 };
 
 PLUGIN_MAIN(openni_plugin);
