a70212224b3e324872eab2afdcb39069908a7b20
shg8 <me@shg.life>
2023-09-19 17:07:27 -0500

    Add decoding extensions for offloading client

--- plugins/offload_rendering_client/plugin.cpp
+++ plugins/offload_rendering_client/plugin.cpp
@@ -3,21 +3,18 @@
 #include "illixr/switchboard.hpp"
 #include "illixr/threadloop.hpp"
 #include "illixr/vk/vulkan_utils.hpp"
+#include "illixr/vk/vk_extension_request.h"
 
 #include <cstdlib>
 #include <set>
 
 using namespace ILLIXR;
 
-class offload_rendering_client : public threadloop {
+class offload_rendering_client : public threadloop, public vulkan::vk_extension_request {
 public:
     offload_rendering_client(const std::string& name, phonebook* pb)
         : threadloop{name, pb} {
-        std::vector<char *> instance_extensions;
-        instance_extensions.push_back(VK_KHR_VIDEO_QUEUE_EXTENSION_NAME);
-        instance_extensions.push_back(VK_KHR_VIDEO_DECODE_QUEUE_EXTENSION_NAME);
-        // set env
-//        setenv("ILLIXR_VULKAN_INSTANCE_EXTENSIONS",  , 1);
+
     }
 
     void start() override {
@@ -26,6 +23,18 @@ public:
 
     void stop() override {
         threadloop::stop();
+    }
+
+    std::vector<const char*> get_required_instance_extensions() override {
+        return {};
+    }
+
+    std::vector<const char*> get_required_devices_extensions() override {
+        std::vector<const char*> device_extensions;
+        device_extensions.push_back(VK_KHR_VIDEO_QUEUE_EXTENSION_NAME);
+        device_extensions.push_back(VK_KHR_VIDEO_DECODE_QUEUE_EXTENSION_NAME);
+        device_extensions.push_back(VK_KHR_VIDEO_DECODE_H264_EXTENSION_NAME);
+        return device_extensions;
     }
 
 protected:
