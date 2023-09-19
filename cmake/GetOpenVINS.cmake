2c5055c754ba3b864c5ad2ec82750d5b28cc83fc
astro-friedel <friedel@illinois.edu>
2023-09-19 13:49:59 -0500

    updated repo versions for spdlog

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -7,7 +7,7 @@ endif()
 
 EXTERNALPROJECT_ADD(OpenVINS
         GIT_REPOSITORY https://github.com/ILLIXR/open_vins.git   # Git repo for source code
-        GIT_TAG c038ca757f857dc9683c82618f2f757ad9b034dc         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+        GIT_TAG 4bcb20790a7574aade4157bd9ced550de4de06bb         # sha5 hash for specific commit to pull (if there is no specific tag to use)
         PREFIX ${CMAKE_BINARY_DIR}/_deps/OpenVINS                # the build directory
         DEPENDS ${OpenCV_DEP_STR}   # dependencies of this module
         #arguments to pass to CMake
