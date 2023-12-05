d8bceafa82467a74f6dbb85e2a22876911bbb43d
Qinjun Jiang <qinjunj2@illinois.edu>
2023-12-05 15:46:41 -0600

    collect all ground poses

--- ground_truth_slam/plugin.cpp
+++ ground_truth_slam/plugin.cpp
@@ -19,18 +19,23 @@ using namespace ILLIXR;
 #define ViconRoom2Medium    1413393885975760384
 #define ViconRoom2Hard      1413394881555760384
 
-class ground_truth_slam : public plugin {
+#define dataset_walking     1700613045229490665
+#define dataset_static      1700611471945221229
+#define dataset_bs          1700612128609292316
+
+class ground_truth_slam : public threadloop {
 public:
     ground_truth_slam(std::string name_, phonebook* pb_)
-        : plugin{name_, pb_}
+        : threadloop{name_, pb_}
         , sb{pb->lookup_impl<switchboard>()}
         , _m_true_pose{sb->get_writer<pose_type>("true_pose")}
         , _m_ground_truth_offset{sb->get_writer<switchboard::event_wrapper<Eigen::Vector3f>>("ground_truth_offset")}
         , _m_sensor_data{load_data()}
+        , _m_sensor_data_it{_m_sensor_data.cbegin()}
         // The relative-clock timestamp of each IMU is the difference between its dataset time and the IMU dataset_first_time.
         // Therefore we need the IMU dataset_first_time to reproduce the real dataset time.
         // TODO: Change the hardcoded number to be read from some configuration variables in the yaml file.
-        , _m_dataset_first_time{ViconRoom1Medium}
+        , _m_dataset_first_time{1700611471945221229}
         , _m_first_time{true} {
         if (!std::filesystem::exists(data_path)) {
             if (!std::filesystem::create_directory(data_path)) {
@@ -38,36 +43,28 @@ public:
             }
         }
         truth_csv.open(data_path + "/truth.csv");
+        std::cout << "Number of ground truth poses " << _m_sensor_data.size() << std::endl;
     }
 
-    virtual void start() override {
-        plugin::start();
-        sb->schedule<imu_type>(id, "imu", [this](switchboard::ptr<const imu_type> datum, std::size_t) {
-            this->feed_ground_truth(datum);
-        });
+protected:
+    virtual skip_option _p_should_skip() override {
+        if (_m_sensor_data_it != _m_sensor_data.end())
+            return skip_option::run;
+        else
+            return skip_option::stop;
     }
 
-    void feed_ground_truth(switchboard::ptr<const imu_type> datum) {
-        ullong rounded_time = datum->time.time_since_epoch().count() + _m_dataset_first_time;
-        auto   it           = _m_sensor_data.find(rounded_time);
-        if (it == _m_sensor_data.end()) {
-#ifndef NDEBUG
-            std::cout << "True pose not found at timestamp: " << rounded_time << std::endl;
-#endif
+    virtual void _p_one_iteration() override {
+        assert(_m_sensor_data_it != _m_sensor_data.end());
+        time_point relative_time(std::chrono::duration<long, std::nano>{_m_sensor_data_it->first - _m_dataset_first_time});
+        if (relative_time.time_since_epoch().count() < 0) {
+            _m_sensor_data_it++;
             return;
         }
-
-        switchboard::ptr<pose_type> true_pose =
-            _m_true_pose.allocate<pose_type>(pose_type{time_point{datum->time}, it->second.position, it->second.orientation});
-
-#ifndef NDEBUG
-        std::cout << "Ground truth pose was found at T: " << rounded_time << " | "
-                  << "Pos: (" << true_pose->position[0] << ", " << true_pose->position[1] << ", " << true_pose->position[2]
-                  << ")"
-                  << " | "
-                  << "Quat: (" << true_pose->orientation.w() << ", " << true_pose->orientation.x() << ", "
-                  << true_pose->orientation.y() << "," << true_pose->orientation.z() << ")" << std::endl;
-#endif
+        switchboard::ptr<pose_type> true_pose = _m_true_pose.allocate<pose_type>(
+                                                pose_type{relative_time,
+                                                _m_sensor_data_it->second.position,
+                                                _m_sensor_data_it->second.orientation});
 
         /// Ground truth position offset is the first ground truth position
         if (_m_first_time) {
@@ -77,9 +74,12 @@ public:
         }
 
         _m_true_pose.put(std::move(true_pose));
-        truth_csv << datum->time.time_since_epoch().count() << "," << true_pose->position.x() << "," << true_pose->position.y()
+        std::cout << "One GT published\n";
+        truth_csv << relative_time.time_since_epoch().count() << "," << true_pose->position.x() << "," << true_pose->position.y()
                   << "," << true_pose->position.z() << "," << true_pose->orientation.w() << "," << true_pose->orientation.x()
                   << "," << true_pose->orientation.y() << "," << true_pose->orientation.z() << std::endl;
+
+        _m_sensor_data_it++;
     }
 
 private:
@@ -88,6 +88,7 @@ private:
 
     switchboard::writer<switchboard::event_wrapper<Eigen::Vector3f>> _m_ground_truth_offset;
     const std::map<ullong, sensor_types>                             _m_sensor_data;
+    std::map<ullong, sensor_types>::const_iterator                   _m_sensor_data_it;
     ullong                                                           _m_dataset_first_time;
     bool                                                             _m_first_time;
 
