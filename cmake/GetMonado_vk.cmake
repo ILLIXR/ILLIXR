9652553f4083f46133a870945543feca0eeb67ff
shg8 <me@shg.life>
2024-05-25 20:25:59 -0500

    Bump monado hash

--- cmake/GetMonado_vk.cmake
+++ cmake/GetMonado_vk.cmake
@@ -19,7 +19,7 @@ get_external_for_plugin(OpenXR_APP)
 
 ExternalProject_Add(MonadoVK
         GIT_REPOSITORY https://github.com/ILLIXR/monado_vulkan_integration.git   # Git repo for source code
-        GIT_TAG a9e6713b473c67b28f7162809feefbcc4775d15f       # sha5 hash for specific commit to pull (if there is no specific tag to use)
+        GIT_TAG 3e1c7a3bf2d4813cd953e4dccfe2fb8848f749eb       # sha5 hash for specific commit to pull (if there is no specific tag to use)
 
         PREFIX ${CMAKE_BINARY_DIR}/_deps/monado_vk             # the build directory
         DEPENDS ${MONADO_VK_DEPS} ${Vulkan_DEP_STR}                             # dependencies of this module
