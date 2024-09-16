906f9861aab44d1016e653b46a745c2810ffd11d
astro-friedel <friedel@illinois.edu>
2024-09-16 13:01:08 -0500

    moved webcam plugin to be a standalone

---
+++ plugins/webcam/plugin.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "illixr/opencv_data_types.hpp"
+#include "illixr/threadloop.hpp"
+
+#include <opencv4/opencv2/videoio.hpp>
+
+namespace ILLIXR {
+class webcam : public threadloop {
+public:
+    [[maybe_unused]] webcam(const std::string& name_, phonebook* pb_);
+    void _p_one_iteration() override;
+
+private:
+    const std::shared_ptr<switchboard> _switchboard;
+    switchboard::writer<monocular_cam_type>    _frame_pub;
+    cv::VideoCapture _capture;
+    bool _load_video;
+};
+
+}
