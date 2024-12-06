69b0a19ff46c994730ceafcfda3fd69298152ad8
wermos <63574588+wermos@users.noreply.github.com>
2024-12-06 13:02:04 +0530

    Wrote up a full implementation of the `DataEmitter` class.

--- plugins/dataset_plugin/include/emitter.hpp
+++ plugins/dataset_plugin/include/emitter.hpp
@@ -6,6 +6,8 @@
 #include "dataset_loader.hpp"
 #include "illixr/common/data_format.hpp"
 
+#include <spdlog/spdlog.h> // for debug messages
+
 #include <functional> // for std::greater
 #include <queue>      // for std::priority_queue
 
@@ -28,9 +30,10 @@ class DataEmitter {
     //
     // When the time comes, we emit the relevant data on the relevant channel.
 public:
-    DataEmitter()
+    DataEmitter(std::chrono::nanoseconds starting_time)
         : data_list{}
-        , data{} {
+        , data{}
+        , starting_time{starting_time} {
         // initializing the DatasetLoader loads in all the data from the dataset, so
         // we don't need to do anything special here.
 
@@ -59,25 +62,84 @@ public:
     }
 
     void emit(std::chrono::nanoseconds current_time) {
-        std::chrono::nanoseconds upper_bound_time = time_since_start + dataset_first_time;
+        // we assume that current_time is the time since the Unix Epoch.
 
-        std::chrono::nanoseconds lower_bound_time = upper_bound_time - error_cushion;
-
-        for (m_data_iterator = m_data.lower_bound(lower_bound_time); m_data_iterator != m_data.upper_bound(upper_bound_time);
-             ++m_data_iterator) {
-            GroundTruthData datum = m_data_iterator->second;
-
-            time_point expected_real_time_given_dataset_time(m_data_iterator->first - dataset_first_time);
-
-            m_ground_truth_publisher.put(m_ground_truth_publisher.allocate<ground_truth_type>(
-                ground_truth_type{expected_real_time_given_dataset_time, datum.data}));
+        std::chrono::nanoseconds dataset_time = (current_time - starting_time) + dataset_first_time;
+        if (data_list.empty()) {
+            // there is no more data left to emit, so we exit early
+            return;
         }
 
-        while (data_list.top.first - current_time >= error_cushion) {
-            // this means that
+        // we simply publish all the data that has timestamps <= `current_time`
+        while(data_list.top().timestamp < dataset_time && !data_list.empty()) {
+            DataEntry entry = priorityQueue.pop();
+
+            if (entry.type == IMAGE) {
+                emitImageData(dataset_time);
+            } else if (entry.type == IMU) {
+                emitIMUData(dataset_time);
+            } else if (entry.type == POSE) {
+                emitPoseData(dataset_time);
+            } else {
+                // ground truth
+                emitGroundTruthData(timestamp);
+            }
         }
-        emitImageData()
     }
+
+    // TODO: decide whether it is worth implementing this alternate version that drops data items that
+    // are too old and only publishes "fresh" ones that are in a "window of viability".
+    // void emit(std::chrono::nanoseconds current_time) {
+    //     // we assume that current_time is the time since the Unix Epoch.
+
+    //     // time that has elapsed since startup, plus offset (first time in the dataset).
+    //     std::chrono::nanoseconds lower_bound_time = (current_time - starting_time) + dataset_first_time;
+
+    //     std::chrono::nanoseconds upper_bound_time = lower_bound_time + error_cushion;
+
+    //     // if (currentTime - nextEntry.timestamp > EMISSION_TOLERANCE_NS) {
+    //     //     // Too late to emit, drop this entry
+    //     //     priorityQueue.pop(); // Drop the entry
+    //     //     continue;
+    //     // }
+
+    //     // print info about how many entries were dropped due to the thread waking up late.
+    //     int count = 0;
+    //     while (data_list.top().timestamp < lower_bound_time) {
+    //         data_list.pop();
+    //         count++;
+    //     }
+
+    //     spdlog::get("illixr")->info("[dataset_plugin] Dropped {} data items due to thread scheduling.", count);
+
+    //     // EMISSION
+    //     // if (!priorityQueue.empty()) {
+    //     //     DataEntry nextEntry = priorityQueue.top();
+    //     //     // Emit data based on nextEntry.type
+    //     //     priorityQueue.pop();
+    //     // }
+
+    //     if (data_list.empty()) {
+    //         // there is no more data left to emit.
+    //         return;
+    //     }
+
+    //     // we simply publish all the data that has timestamps 
+    //     for (auto it = m_data.lower_bound(lower_bound_time); it != m_data.upper_bound(upper_bound_time); ++it) {
+    //         auto [timestamp, type] = *it;
+
+    //         if (type == IMAGE) {
+    //             emitImageData(timestamp);
+    //         } else if (type == IMU) {
+    //             emitIMUData(timestamp);
+    //         } else if (type == POSE) {
+    //             emitPoseData(timestamp);
+    //         } else {
+    //             // ground truth
+    //             emitGroundTruthData(timestamp);
+    //         }
+    //     }
+    // }
 
     bool finished() {
         // have we run out of data to emit?
@@ -103,20 +165,81 @@ private:
         data_list.push(entry); // Automatically sorted by timestamp
     }
 
-    void emitImageData() { }
+    void emitImageData(std::chrono::nanoseconds timestamp) {
+        auto range = data.getImageData.equal_range(timestamp);
 
-    void emitIMUData() { }
+        for (auto it = range.first; it != range.second; ++it) {
+            ImageData datum = it->second;
 
-    void emitPoseData() { }
+            // we report the time relative to the first timestamp. That is, we report how long it has been since the
+            // first data item was emitted.
+            // For example, if the first item has a timestamp of 11000ns and the second item has a timestamp of 11334ns,
+            // we will publish 314ns as the timestamp.
+            time_point time(timestamp - dataset_first_time);
 
-    void emitGroundTruthData() { }
+            m_image_publisher.put(m_image_publisher.allocate<image_type>(
+                image_type{time, datum.loadImage(), datum.getChannel()}));
+        }
+    }
+
+    void emitIMUData(std::chrono::nanoseconds timestamp) {
+        auto range = data.getIMUData.equal_range(timestamp);
+
+        for (auto it = range.first; it != range.second; ++it) {
+            IMUData datum = it->second;
+
+            // we report the time relative to the first timestamp. That is, we report how long it has been since the
+            // first data item was emitted.
+            // For example, if the first item has a timestamp of 11000ns and the second item has a timestamp of 11334ns,
+            // we will publish 314ns as the timestamp.
+            time_point time(timestamp - dataset_first_time);
+
+            m_imu_publisher.put(m_imu_publisher.allocate<imu_type>(
+                imu_type{time, datum.angular_v, datum.linear_a, datum.channel}));
+        }
+    }
+
+    void emitPoseData(std::chrono::nanoseconds timestamp) {
+        auto range = data.getPoseData.equal_range(timestamp);
+
+        for (auto it = range.first; it != range.second; ++it) {
+            PoseData datum = it->second;
+
+            // we report the time relative to the first timestamp. That is, we report how long it has been since the
+            // first data item was emitted.
+            // For example, if the first item has a timestamp of 11000ns and the second item has a timestamp of 11334ns,
+            // we will publish 314ns as the timestamp.
+            time_point time(timestamp - dataset_first_time);
+
+            m_pose_publisher.put(m_pose_publisher.allocate<pose_type>(
+                pose_type{time, datum.position, datum.orientation}));
+        }
+    }
+
+    void emitGroundTruthData(std::chrono::nanoseconds timestamp) {
+        auto range = data.getImageData.equal_range(timestamp);
+
+        for (auto it = range.first; it != range.second; ++it) {
+            ImageData datum = it->second;
+
+            // we report the time relative to the first timestamp. That is, we report how long it has been since the
+            // first data item was emitted.
+            // For example, if the first item has a timestamp of 11000ns and the second item has a timestamp of 11334ns,
+            // we will publish 314ns as the timestamp.
+            time_point time(timestamp - dataset_first_time);
+
+            m_ground_truth_publisher.put(m_ground_truth_publisher.allocate<ground_truth_type>(
+                ground_truth_type{time, datum.data}));
+        }
+    }
 
     std::priority_queue<DataEntry, std::greater<std::chrono::nanoseconds>> data_list;
     // using the custom comparator means that the earliest timestamp appears earlier in the queue.
     DatasetLoader data;
 
     std::chrono::nanoseconds       dataset_first_time;
-    const std::chrono::nanoseconds error_cushion{500};
+    std::chrono::nanoseconds       time_since_start;
+    // const std::chrono::nanoseconds error_cushion{500};
 
     // writers
     using namespace ILLIXR;
@@ -126,35 +249,25 @@ private:
     switchboard::writer<ground_truth_type> m_ground_truth_publisher;
 };
 
-// EMISSION
-if (!priorityQueue.empty()) {
-    DataEntry nextEntry = priorityQueue.top();
-    // Emit data based on nextEntry.type
-    priorityQueue.pop();
-}
+// void processNextEmission() {
+//     uint64_t currentTime = getCurrentTime();
 
-// EMISSION WITH TOLERANCE CHECK
-const uint64_t EMISSION_TOLERANCE_NS = 1000000; // Example: 1 ms
+//     while (!priorityQueue.empty()) {
+//         DataEntry nextEntry = priorityQueue.top();
 
-void processNextEmission() {
-    uint64_t currentTime = getCurrentTime();
+//         if (currentTime < nextEntry.timestamp) {
+//             // Not yet time to emit
+//             break;
+//         }
 
-    while (!priorityQueue.empty()) {
-        DataEntry nextEntry = priorityQueue.top();
+//         if (currentTime - nextEntry.timestamp > EMISSION_TOLERANCE_NS) {
+//             // Too late to emit, drop this entry
+//             priorityQueue.pop(); // Drop the entry
+//             continue;
+//         }
 
-        if (currentTime < nextEntry.timestamp) {
-            // Not yet time to emit
-            break;
-        }
-
-        if (currentTime - nextEntry.timestamp > EMISSION_TOLERANCE_NS) {
-            // Too late to emit, drop this entry
-            priorityQueue.pop(); // Drop the entry
-            continue;
-        }
-
-        // Emit the data
-        emitData(nextEntry);
-        priorityQueue.pop(); // Remove the emitted entry
-    }
-}
+//         // Emit the data
+//         emitData(nextEntry);
+//         priorityQueue.pop(); // Remove the emitted entry
+//     }
+// }
