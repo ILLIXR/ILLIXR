73922119f7738b077cf82d9cbad1e2c0bc3bc22b
Douglas Friedel <friedel@illinois.edu>
2024-12-09 14:26:44 -0600

    Unify file io - issue #118 (#416)
    * centralized data loading to a single header.
    
    * [pre-commit.ci] Run clang-format
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ include/illixr/data_loading.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "illixr/csv_iterator.hpp"
+#include "illixr/data_format.hpp"
+#include "illixr/error_util.hpp"
+
+#include <eigen3/Eigen/Dense>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <spdlog/spdlog.h>
+#include <string>
+
+using namespace ILLIXR;
+
+template<typename T>
+static std::map<ullong, T> load_data(const std::string& spath, const std::string& plugin_name,
+                                     std::map<ullong, T> (*func)(std::ifstream&, const std::string&)) {
+    const char* illixr_data_c_str = std::getenv("ILLIXR_DATA");
+    if (!illixr_data_c_str) {
+        ILLIXR::abort("Please define ILLIXR_DATA");
+    }
+    const std::string subpath     = "/" + spath + "/data.csv";
+    std::string       illixr_data = std::string{illixr_data_c_str};
+
+    std::ifstream gt_file{illixr_data + subpath};
+
+    if (!gt_file.good()) {
+        spdlog::get("illixr")->error("[{0}] ${ILLIXR_DATA}{1} ({2}{1}) is not a good path", plugin_name, subpath, illixr_data);
+        ILLIXR::abort();
+    }
+
+    return func(gt_file, illixr_data + subpath);
+}
