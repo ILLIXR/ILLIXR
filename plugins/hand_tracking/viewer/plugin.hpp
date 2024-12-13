9280088bcd51527ddf835418b739237ebfad5e73
astro-friedel <friedel@illinois.edu>
2024-12-13 10:06:36 -0600

    reworked for new, better look

--- plugins/hand_tracking/viewer/plugin.hpp
+++ plugins/hand_tracking/viewer/plugin.hpp
@@ -10,9 +10,6 @@
 #include "illixr/hand_tracking_data.hpp"
 #include "illixr/camera_data.hpp"
 #include "illixr/data_format.hpp"
-#ifdef USE_ZED
-#include "illixr/zed_cam.hpp"
-#endif
 #include "imgui/imgui.h"
 
 #include <eigen3/Eigen/Core>
@@ -26,20 +23,19 @@ public:
     void make_gui(const switchboard::ptr<const HandTracking::ht_frame>& frame);
 
 private:
-    static void make_detection_table(const HandTracking::ht_detection& det, const image::image_type it);
+//    static void make_detection_table(const HandTracking::ht_detection& det, image::image_type it);
+    void make_detection_table(units::eyes eye, int idx, const std::string& label);
+    void make_position_table();
 
     std::shared_ptr<RelativeClock>          _clock;
     const std::shared_ptr<switchboard>      _switchboard;
     std::shared_ptr<HandTracking::ht_frame> _ht_frame;
-#ifdef USE_ZED
-    std::shared_ptr<cam_type_zed>           _zed_data;
-#endif
     std::shared_ptr<pose_type>              _pose;
     std::shared_ptr<camera_data>            _camera;
     GLFWwindow*                             _viewport{};
     uint count = 0;
 
-    GLuint          textures[6];
+    GLuint          textures[2];
     Eigen::Vector2i raw_size = Eigen::Vector2i::Zero();
     Eigen::Vector2i processed_size = Eigen::Vector2i::Zero();
     Eigen::Vector2i combined_size = Eigen::Vector2i::Zero();
@@ -49,6 +45,15 @@ private:
     Eigen::Matrix4f basicProjection;
     const HandTracking::ht_frame *current_frame;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    bool enabled_right = false;
+    std::string tab_label;
+    units::eyes single_eye = units::LEFT_EYE;
+    std::vector<ILLIXR::image::image_type> detections;
+
+    std::map<uint64_t, HandTracking::ht_frame> ht_frames;
+    static int requested_unit_;
+    static units::measurement_unit base_unit_;
 };
 
 }
