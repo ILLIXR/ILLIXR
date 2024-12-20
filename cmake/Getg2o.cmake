d35dbf1042b816c4913f634c3e0145123242bd17
Jeffrey Liu <jeffliu604@gmail.com>
2024-12-19 22:45:18 -0600

    Use updated g2o version

--- cmake/Getg2o.cmake
+++ cmake/Getg2o.cmake
@@ -5,7 +5,6 @@ find_package(g2o 1.0 QUIET)
 if (NOT g2o_FOUND)
     EXTERNALPROJECT_ADD(g2o
             GIT_REPOSITORY https://github.com/RainerKuemmerle/g2o.git   # Git repo for source code
-            GIT_TAG 20230223_git                                        # sha5 hash for specific commit to pull (if there is no specific tag to use)
             PREFIX ${CMAKE_BINARY_DIR}/_deps/g2o                        # the build directory
             # arguments to pass to CMake
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_CXX_FLAGS="-L${CMAKE_INSTALL_PREFIX}/lib" -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release
