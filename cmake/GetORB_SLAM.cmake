5ea4f59ea4ab152f8c401d947450d083eb090b70
Qinjun Jiang <qinjunj2@illinois.edu>
2025-01-23 13:03:17 -0600

    remove dependency on Pangolin

--- cmake/GetORB_SLAM.cmake
+++ cmake/GetORB_SLAM.cmake
@@ -1,7 +1,6 @@
 # module to download, build and install the ORM_SLAM ILLIXR plugin
 
 # get dependencies
-get_external_for_plugin(Pangolin)
 get_external_for_plugin(g2o)
 get_external_for_plugin(Sophus)
 get_external_for_plugin(DBoW2)
@@ -15,7 +14,7 @@ endif()
 
 EXTERNALPROJECT_ADD(ORB_Slam
         GIT_REPOSITORY https://github.com/ILLIXR/ORB_SLAM3.git   # Git repo for source code
-        GIT_TAG 9e630a604e73c4a6a0b87c56248b4aeee62baf8c         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+        GIT_TAG dc2658af09935d4b25c38057b73524ffd1b301d4         # sha5 hash for specific commit to pull (if there is no specific tag to use)
         PREFIX ${ORB_SLAM_SOURCE_DIR}                            # the build directory
         DEPENDS ${DBoW2_DEP_STR} ${Pangolin_DEP_STR} ${g2o_DEP_STR} ${Sophus_DEP_STR} ${OpenCV_DEP_STR}   # dependencies of this module
         # force serialized build, otherwise the machine might get slogged down
