3f55b48c8cdbdeb94500a80076fb2ba119006970
Qinjun Jiang <qinjunj2@illinois.edu>
2025-01-28 14:49:15 -0600

    cmake changes

--- cmake/GetVulkan.cmake
+++ cmake/GetVulkan.cmake
@@ -14,66 +14,51 @@ function(clearVars)
     endforeach()
 endfunction()
 
+message(STATUS "Checking for module 'vulkan' > 1.3.255")
+find_package(Vulkan 1.3.255 QUIET)
+
 if(NOT Vulkan_FOUND)
-    message(STATUS "Checking for module 'vulkan' > 1.3.255")
-    find_package(Vulkan REQUIRED)
-    if (NOT Vulkan_FOUND)
-        clearVars()
-        pkg_check_modules(Vulkan QUIET vulkan>=1.3.255)
-        if(Vulkan_FOUND AND Vulkan_LIBRARIES AND Vulkan_LIBRARY_DIRS)
-            message(Vulkan_LIBRARY_DIRS=${Vulkan_LIBRARY_DIRS})
-            set(Vulkan_LIBRARIES "${Vulkan_LIBRARY_DIRS}/lib${Vulkan_LIBRARIES}.so" CACHE INTERNAL "" FORCE)
-        endif()
-    endif()
-
-    # now look for glslang stuff
-    if(Vulkan_FOUND)
-        unset(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE CACHE)
-        unset(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE)
-	find_package(glslang)
-        if(glslang_FOUND)
-            find_program(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE glslangValidator HINTS ${CMAKE_INSTALL_PREFIX}/bin)
-        else()
-	    message(STATUS "glslang NOT FOUND!!!")
-            set(Vulkan_FOUND FALSE CACHE INTERNAL "" FORCE)
-            set(Vulkan_FOUND FALSE)
-            clearVars()
-        endif()
-    endif()
-
-    if(Vulkan_FOUND)
-        # message(STATUS "  Found Vulkan, version ${Vulkan_VERSION} ${Vulkan_LIBRARIES} ${Vulkan_INCLUDE_DIRS}")
-        # Since we now have both Vulkan and glslang found, they will use the system wide Vulkan
-        # set(Vulkan_DEP_STR Vulkan_ext)
-        # set(Vulkan_EXTERNAL Yes)
-        set(Vulkan_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
-        # set(Vulkan_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/_deps/vulkan/src/Vulkan_ext/Vulkan-Headers/include)
-        set(Vulkan_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libvulkan.so)
-        # set(Vulkan_LIBRARIES /usr/lib/x86_64-linux-gnu/libvulkan.so)
-        set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/glslangValidator)
-        # set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE /usr/bin/glslangValidator)
-        message(STATUS "  Found Vulkan, version ${Vulkan_VERSION} ${Vulkan_LIBRARIES} ${Vulkan_INCLUDE_DIRS}")
-
-        set(Vulkan_VERSION "${Vulkan_VERSION}" CACHE INTERNAL "" FORCE)
-    else()
-        message(STATUS "Building VULKAN from source")
-        EXTERNALPROJECT_ADD(Vulkan_ext
-                            GIT_REPOSITORY https://github.com/ILLIXR/ILLIXR-vulkan.git
-                            GIT_TAG ca2e256f2ef5bcdd0ffd05f7712bd03897d9c991
-                            PREFIX ${CMAKE_BINARY_DIR}/_deps/vulkan
-                            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DBUILD_PARALLEL_LEVEL=${BUILD_PARALLEL_LEVEL}
-                            BUILD_COMMAND cmake --build . --parallel ${BUILD_PARALLEL_LEVEL}
-                            INSTALL_COMMAND ""
-                            )
-        set(Vulkan_DEP_STR Vulkan_ext)
-        set(Vulkan_EXTERNAL Yes)
-        set(Vulkan_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
-        # set(Vulkan_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/_deps/vulkan/src/Vulkan_ext/Vulkan-Headers/include)
-        set(Vulkan_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libvulkan.so)
-        # set(Vulkan_LIBRARIES /usr/lib/x86_64-linux-gnu/libvulkan.so)
-        set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/glslangValidator)
-        # set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE /usr/bin/glslangValidator)
+    clearVars()
+    pkg_check_modules(Vulkan QUIET vulkan>=1.3.255)
+    if(Vulkan_FOUND AND Vulkan_LIBRARY_DIRS)
+        set(Vulkan_LIBRARIES "${Vulkan_LIBRARY_DIRS}/lib${Vulkan_LIBRARIES}.so" CACHE INTERNAL "" FORCE)
     endif()
 endif()
 
-# set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE} CACHE INTERNAL "" FORCE)
+if(Vulkan_FOUND)
+    find_package(glslang REQUIRED)
+    if(glslang_FOUND)
+        find_program(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE glslangValidator)
+        if (NOT Vulkan_GLSLANG_VALIDATOR_EXECUTABLE)
+            message(STATUS "Cannot find glslangValidator!")
+            set(Vulkan_FOUND FALSE CACHE INTERNAL "" FORCE)
+            set(Vulkan_FOUND FALSE)
+            clearVars()
+        else()
+            set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE} CACHE INTERNAL "" FORCE)
+            message(STATUS "glslangValidator is found at ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}")
+        endif()
+    else()
+        message(STATUS "glslang NOT FOUND!!!")
+        set(Vulkan_FOUND FALSE CACHE INTERNAL "" FORCE)
+        set(Vulkan_FOUND FALSE)
+        clearVars()
+    endif()
+endif()
+
+if (NOT Vulkan_FOUND)
+    message(STATUS "Building VULKAN from source")
+    EXTERNALPROJECT_ADD(Vulkan_ext
+                        GIT_REPOSITORY https://github.com/ILLIXR/ILLIXR-vulkan.git
+                        GIT_TAG ca2e256f2ef5bcdd0ffd05f7712bd03897d9c991
+                        PREFIX ${CMAKE_BINARY_DIR}/_deps/vulkan
+                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DBUILD_PARALLEL_LEVEL=${BUILD_PARALLEL_LEVEL}
+                        BUILD_COMMAND cmake --build . --parallel ${BUILD_PARALLEL_LEVEL}
+                        INSTALL_COMMAND cmake --install . --prefix ${CMAKE_INSTALL_PREFIX}
+                        )
+    set(Vulkan_DEP_STR Vulkan_ext)
+    set(Vulkan_EXTERNAL Yes)
+    set(Vulkan_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
+    set(Vulkan_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/libvulkan.so)
+    set(Vulkan_GLSLANG_VALIDATOR_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/glslangValidator CACHE INTERNAL "" FORCE)
+endif()
