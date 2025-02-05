b2aa7078f3eee566f9832403078f8b919657278c
Douglas Friedel <friedel@illinois.edu>
2025-02-05 17:19:46 -0600

    Adding documentation for policies (#419)
    * initial structure for new docs
    
    * initial version of some pages
    
    * added some guidelines
    
    * Updated code of conduct
    
    * update code review docs
    
    * updated opening and issue docs
    
    * updated pull request docs
    
    * initial version of style guide
    
    * initial version of modules docs
    
    * [pre-commit.ci] Run clang-format
    
    * updated include files style
    
    * more coding style updates
    
    * [pre-commit.ci] Run clang-format
    
    * more refactoring for style
    
    * [pre-commit.ci] Run clang-format
    
    * first round of applying coding standards
    
    * [pre-commit.ci] Run clang-format
    
    * updated file extensions to current practice
    
    * broke all plugins into header and source files
    
    * [pre-commit.ci] Run clang-format
    
    * [pre-commit.ci] Run clang-format
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ plugins/passthrough_integrator/plugin.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "illixr/data_format.hpp"
+#include "illixr/phonebook.hpp"
+#include "illixr/plugin.hpp"
+#include "illixr/switchboard.hpp"
+
+namespace ILLIXR {
+class passthrough_integrator : public plugin {
+public:
+    [[maybe_unused]] passthrough_integrator(const std::string& name, phonebook* pb);
+
+    void callback(const switchboard::ptr<const imu_type>& datum);
+
+private:
+    const std::shared_ptr<switchboard> switchboard_;
+
+    // IMU Data, Sequence Flag, and State Vars Needed
+    switchboard::reader<imu_integrator_input> imu_integrator_input_;
+
+    // IMU state
+    switchboard::writer<imu_raw_type> imu_raw_;
+};
+
+} // namespace ILLIXR
