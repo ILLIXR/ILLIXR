2b06829bb10c19cd25b60fafa88ccd891b09f2df
Jeffrey Liu <jeffliu604@gmail.com>
2025-02-17 16:56:19 -0600

    Remove deprecated logic for ILLIXR_COMPARE_IMAAGES

--- src/vulkan_display.hpp
+++ src/vulkan_display.hpp
@@ -162,9 +162,6 @@ private:
         // enable validation layers
         std::vector<const char*> validation_layers = {"VK_LAYER_KHRONOS_validation"};
 
-        if (std::getenv("ILLIXR_COMPARE_IMAGES") != nullptr && std::stoi(std::getenv("ILLIXR_COMPARE_IMAGES")))
-            validation_layers.push_back("VK_LAYER_LUNARG_screenshot");
-
         if (enable_validation_layers) {
             create_info.enabledLayerCount   = static_cast<uint32_t>(validation_layers.size());
             create_info.ppEnabledLayerNames = validation_layers.data();
