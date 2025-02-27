9bcf903fdf686fef1032b4818c6c31f1cb23d130
shg8 <me@shg8.com>
2025-02-26 21:04:38 -0600

    WIP

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -16,4 +16,12 @@ cmake --install .
 export LD_LIBRARY_PATH=/opt/ILLIXR/build/install/lib:$LD_LIBRARY_PATH
 export PATH=/opt/ILLIXR/build/install/bin:$PATH
 
+# Set up XDG runtime directory
+export XDG_RUNTIME_DIR=/tmp/runtime-docker
+mkdir -p ${XDG_RUNTIME_DIR}
+chmod 700 ${XDG_RUNTIME_DIR}
+
+# Set display variable if not already set
+export DISPLAY=${DISPLAY:-:1}
+
 ./main.opt.exe -y ../illixr.yaml
