7df1b29e4cb5a2e4b8973f58f483e4833ebdeaec
Jeffrey Liu <jeffliu604@gmail.com>
2025-03-26 14:18:17 -0500

    Update FFMPEG commit hash

--- cmake/GetILLIXR_FFmpeg.cmake
+++ cmake/GetILLIXR_FFmpeg.cmake
@@ -47,7 +47,7 @@ if(NOT (libavcodec_illixr_FOUND AND libavdevice_illixr_FOUND AND
     message("FFMPEG NOT FOUND, will build from source")
     EXTERNALPROJECT_ADD(ILLIXR_FFmpeg_ext
                         GIT_REPOSITORY https://github.com/ILLIXR/FFmpeg.git
-                        GIT_TAG 10c2519b7fcd5bddd4bc7275331cda10ef0ad120
+                        GIT_TAG 279b85f88a4052a2b9afd6bb59cfcd73e7b9d175
                         PREFIX ${CMAKE_BINARY_DIR}/_deps/ffmpeg
                         DEPENDS ${Vulkan_DEP_STR}
                         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release
