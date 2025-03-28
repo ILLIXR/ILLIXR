a1d77efe786c59c2325ce9f1f498953443cfb02f
astro-friedel <friedel@illinois.edu>
2025-03-28 14:57:48 -0600

    Removed v4.0 with mike 2.1.3

--- v4.0/ILLIXR_API/html/search/functions_14.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['v_5fdot_0',['v_dot',['../namespaceILLIXR.html#a262e769f9eff33421412d36cd4c20767',1,'ILLIXR']]],
-  ['velocity_1',['velocity',['../structILLIXR_1_1data__format_1_1ht_1_1velocity.html#a64fb1b91de2a33d7fe97f56479e403f7',1,'ILLIXR::data_format::ht::velocity::velocity()'],['../structILLIXR_1_1data__format_1_1ht_1_1velocity.html#a07c735f8308b406b35c76e7e2810a356',1,'ILLIXR::data_format::ht::velocity::velocity(units::measurement_unit unit)'],['../structILLIXR_1_1data__format_1_1ht_1_1velocity.html#a95d995d300391e72ab911b6a116124fd',1,'ILLIXR::data_format::ht::velocity::velocity(const hand_points &amp;h1, const hand_points &amp;h2, const float time)']]],
-  ['vk_5fresources_5finit_2',['vk_resources_init',['../classILLIXR_1_1offload__rendering__client__jetson.html#adeccc0ee8b97f2a1ab7e2d211f4563b9',1,'ILLIXR::offload_rendering_client_jetson']]]
-];
--- v4.0/ILLIXR_API/html/search/functions_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['fast_5fpose_5freliable_0',['fast_pose_reliable',['../classILLIXR_1_1offload__rendering__server.html#aca6f386ebf5e3aaa56781b5b93e0168c',1,'ILLIXR::offload_rendering_server']]],
-  ['fast_5fpose_5ftype_1',['fast_pose_type',['../structILLIXR_1_1data__format_1_1fast__pose__type.html#a8221182003133150595c08bc0ab1695e',1,'ILLIXR::data_format::fast_pose_type::fast_pose_type()'],['../structILLIXR_1_1data__format_1_1fast__pose__type.html#a3b0d105b577bd6eaf1729f84e01d6ce1',1,'ILLIXR::data_format::fast_pose_type::fast_pose_type(pose_type pose_, time_point predict_computed_time_, time_point predict_target_time_)']]],
-  ['flip_5fy_2',['flip_y',['../structILLIXR_1_1data__format_1_1ht_1_1hand__points.html#a763496a17cc58068dd7a30d7ecc27359',1,'ILLIXR::data_format::ht::hand_points::flip_y()'],['../structILLIXR_1_1data__format_1_1rect.html#addbda3371f040f5020fcdb1dcf938a1a',1,'ILLIXR::data_format::rect::flip_y()']]],
-  ['flush_3',['flush',['../classILLIXR_1_1record__coalescer.html#a66c75672ea93e6f1ff706453c957a729',1,'ILLIXR::record_coalescer']]]
-];
