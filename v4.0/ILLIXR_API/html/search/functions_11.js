deed48d905defb311f3b1a0204cc582533f1aa40
astro-friedel <friedel@illinois.edu>
2025-04-03 09:04:01 -0500

    added missing klinks

--- v4.0/ILLIXR_API/html/search/functions_11.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['true_5fpose_5freliable_0',['true_pose_reliable',['../classILLIXR_1_1offload__rendering__server.html#a2972885c4bb5f6d2dfd2e1e1ff28c50f',1,'ILLIXR::offload_rendering_server']]]
-];
--- v4.0/ILLIXR_API/html/search/variables_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['time_0',['time',['../structILLIXR_1_1data__format_1_1ht_1_1position.html#a0be9af15e82b7f9ff49eb7112190e18b',1,'ILLIXR::data_format::ht::position::time'],['../structILLIXR_1_1data__format_1_1cam__base__type.html#a38c6d85c01e35357fd04f9f7d1c5e9eb',1,'ILLIXR::data_format::cam_base_type::time']]],
-  ['type_1',['type',['../structILLIXR_1_1data__format_1_1cam__base__type.html#a9a4e8ef77b7c12c3c595f7afd1ba5d1f',1,'ILLIXR::data_format::cam_base_type']]]
-];
