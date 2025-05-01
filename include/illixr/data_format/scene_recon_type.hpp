2d7f293295e14053f2da5e97eafd8ec86e3fa2b3
astro-friedel <friedel@illinois.edu>
2025-05-01 12:22:18 -0500

    added data type

---
+++ include/illixr/data_format/scene_recon_type.hpp
@@ -0,0 +1,38 @@
+#pragme once
+
+#include "illixr/data_format/pose.hpp"
+#include "illixr/phonebook.hpp"
+#include "illixr/switchboard.hpp"
+
+#include <opencv4/opencv2/core/mat.hpp>
+
+namespace ILLIXR::data_format {
+struct scene_recon_type : public switchboard::event {
+    [[maybe_unused]] time_point time;
+    pose_type                   pose;
+    cv::Mat                     depth;
+    cv::Mat                     rgb;
+    bool                        last_frame;
+    unsigned                    id;
+
+    scene_recon_type(time_point camera_time, pose_type pose_, cv::Mat depth_, cv::Mat rgb_, bool is_last_frame, unsigned id_)
+        : time{camera_time}
+        , pose{pose_}
+        , depth{depth_}
+        , rgb{rgb_}
+        , last_frame{is_last_frame}
+        , id{id_} { }
+
+    // scene_recon_type(time_point camera_time, pose_type pose_, cv::Mat depth_, cv::Mat rgb_, bool is_last_frame, unsigned id_)
+    //   : time{camera_time}
+    // , pose{pose_}
+    // , depth{depth_}
+    // , rgb{rgb_}
+    // , last_frame{is_last_frame}
+    // , id{id_}{}
+    // //~scene_recon_type() {
+    // //   std::cout << "scene_recon_type id: "<<id<<" is being destroyed." << std::endl;
+    // //   std::cout.flush();
+    // //}
+};
+} // namespace ILLIXR::data_format
