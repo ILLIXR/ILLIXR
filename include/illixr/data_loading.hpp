c33b62ab3bb490c09f69bac2bcfa48cbe8d36eed
astro-friedel <friedel@illinois.edu>
2025-08-21 15:45:47 -0500

    added a file_name component to make it more flexible

--- include/illixr/data_loading.hpp
+++ include/illixr/data_loading.hpp
@@ -17,18 +17,19 @@ using namespace ILLIXR;
 template<typename T>
 static std::map<ullong, T> load_data(const std::string& spath, const std::string& plugin_name,
                                      std::map<ullong, T> (*func)(std::ifstream&, const std::string&),
-                                     const std::shared_ptr<switchboard>& sb) {
+                                     const std::shared_ptr<switchboard>& sb,
+                                     const std::string& file_name = "data.csv") {
     const char* illixr_data_c_str = sb->get_env_char("ILLIXR_DATA");
     if (!illixr_data_c_str) {
         ILLIXR::abort("Please define ILLIXR_DATA");
     }
-    const std::string subpath     = "/" + spath + "/data.csv";
+    const std::string subpath     = "/" + spath + "/" + file_name;
     std::string       illixr_data = std::string{illixr_data_c_str};
 
     std::ifstream gt_file{illixr_data + subpath};
 
     if (!gt_file.good()) {
-        spdlog::get("illixr")->error("[{0}] ${ILLIXR_DATA}{1} ({2}{1}) is not a good path", plugin_name, subpath, illixr_data);
+        spdlog::get("illixr")->error("[{0}] ${ILLIXR_DATA}{1} ({2}{1}{3}) is not a good path", plugin_name, subpath, illixr_data, file_name);
         ILLIXR::abort();
     }
 
