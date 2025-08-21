71b50d4b7fe476e0794c57a4ff28d3c42f584843
astro-friedel <friedel@illinois.edu>
2025-08-21 10:42:55 -0500

    updated draco repo

--- cmake/GetDraco.cmake
+++ cmake/GetDraco.cmake
@@ -3,9 +3,9 @@ pkg_check_modules(draco QUIET draco)
 if (NOT draco_found)
     externalproject_add(Draco_ext
                         GIT_REPOSITORY https://github.com/ILLIXR/draco.git
-                        GIT_TAG 97656cd3d94875184894fb2ca3eb1f79ecde0211
+                        GIT_TAG bba2a71ae3d46631a3b6d969e60730d570e904aa
                         PREFIX ${CMAKE_BINARY_DIR}/_deps/draco
-                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DDRACO_TRANSCODER_SUPPORTED=ON -DCMAKE_CXX_COMPILER=${CLANGPP_EXEC} -DCMAKE_C_COMPILER=${CLANG_EXEC}
+                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DDRACO_TRANSCODER_SUPPORTED=ON -DCMAKE_BUILD_TYPE=Release ${DEPENDENCY_COMPILE_ARGS}
     )
     set(Draco_DEP_STR Draco_ext)
     set(Draco_EXTERNAL Yes)
