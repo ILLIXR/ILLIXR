1748fa36a9ed14f29df963a2f6f395332850494f
astro-friedel <friedel@illinois.edu>
2025-08-22 16:44:12 -0500

    initial server_rx plugin code
    converted to ILLIXR style
    converted to use tcp_network_backend
    converted video_decode to be a subclass of illixr/video_decode

---
+++ plugins/ada/server_rx/video_decoder.cpp
@@ -0,0 +1,21 @@
+#include "video_decoder.hpp"
+
+#include <cstring>
+#include <gst/app/gstappsrc.h>
+
+using namespace ILLIXR;
+
+void ada_video_decoder::enqueue(std::string& img0, std::string& img1) {
+    size_t img0_size = img0.size();
+    size_t img1_size = img1.size();
+    GstBuffer* buffer_img0 = gst_buffer_new_allocate(nullptr, img0_size, nullptr);
+    GstBuffer* buffer_img1 = gst_buffer_new_allocate(nullptr, img1_size, nullptr);
+    gst_buffer_fill(buffer_img0, 0, img0.data(), img0_size);
+    gst_buffer_fill(buffer_img1, 0, img1.data(), img1_size);
+
+    auto ret_img0 = gst_app_src_push_buffer(GST_APP_SRC(appsrc_img0_), buffer_img0);
+    auto ret_img1 = gst_app_src_push_buffer(GST_APP_SRC(appsrc_img1_), buffer_img1);
+    if (ret_img0 != GST_FLOW_OK || ret_img1 != GST_FLOW_OK) {
+        abort("Failed to push buffer");
+    }
+}
