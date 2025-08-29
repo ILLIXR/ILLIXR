bde9afcd92fbbbf350aefe34ce2189febde6fde6
astro-friedel <friedel@illinois.edu>
2025-08-29 14:15:32 -0500

    fixed up mesh to carry thread_id for compression/decompression

--- include/illixr/data_format/mesh.hpp
+++ include/illixr/data_format/mesh.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "draco_illixr/io/ply_reader.h"
+#include <draco_illixr/io/ply_reader.h>
 #include "illixr/switchboard.hpp"
 
 #include <vector>
 
 namespace ILLIXR::data_format {
+
 struct mesh_type : public switchboard::event {
+    unsigned type;
     const std::vector<char> mesh{};
     [[maybe_unused]] bool compressed = false;
     [[maybe_unused]] bool active     = false;
@@ -23,18 +25,22 @@ struct mesh_type : public switchboard::event {
     // 12/29 draco integration
     [[maybe_unused]] std::unique_ptr<draco_illixr::PlyReader> reader;
 
-    [[maybe_unused]]mesh_type(std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_)
-            : id{id_}
+    [[maybe_unused]]mesh_type(const unsigned type_, std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_)
+            : type{type_}
+            , id{id_}
             , reader{std::move(input_reader)} { }
 
-    [[maybe_unused]]mesh_type(std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_, bool is_active)
-            : active{is_active}
+    [[maybe_unused]]mesh_type(const unsigned type_,std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_, bool is_active)
+            : type{type_}
+            , active{is_active}
             , id{id_}
             , reader{std::move(input_reader)}{ }
 
-    [[maybe_unused]]mesh_type(std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_, unsigned chunk_id_,
-                              unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_, bool is_active)
-            : active{is_active}
+    [[maybe_unused]]mesh_type(const unsigned type_, std::unique_ptr<draco_illixr::PlyReader> input_reader, unsigned id_,
+                              unsigned chunk_id_, unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_,
+                              bool is_active)
+            : type{type_}
+            , active{is_active}
             , id{id_}
             , chunk_id{chunk_id_}
             , max_chunk{max_chunk_}
@@ -42,21 +48,25 @@ struct mesh_type : public switchboard::event {
             , num_vertices{num_vertices_}
             , reader(std::move(input_reader)){ }
 
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, unsigned id_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, unsigned id_)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , id{id_} { }
 
     // for baseline parallel compression 2
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, unsigned id_, unsigned chunk_id_, unsigned max_chunk_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, unsigned id_, unsigned chunk_id_,
+                              unsigned max_chunk_)
+            : type{type_}
+            , mesh(input_mesh)
             , id{id_}
             , chunk_id{chunk_id_}
             , max_chunk{max_chunk_} { }
 
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, bool is_active, int cell_x_, int cell_y_,
-                              int cell_z_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, bool is_active,
+                              int cell_x_, int cell_y_, int cell_z_)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , active{is_active}
             , cell_x{cell_x_}
@@ -64,9 +74,10 @@ struct mesh_type : public switchboard::event {
             , cell_z{cell_z_} { }
 
     // used by server mesh manager in draco integration
-    [[maybe_unused]]mesh_type(std::unique_ptr<draco_illixr::PlyReader> input_reader, bool is_compressed, bool is_active,
-                              unsigned id_)
-            : compressed{is_compressed}
+    [[maybe_unused]]mesh_type(const unsigned type_, std::unique_ptr<draco_illixr::PlyReader> input_reader, bool is_compressed,
+                              bool is_active, unsigned id_)
+            : type{type_}
+            , compressed{is_compressed}
             , active{is_active}
             , id{id_}
             , chunk_id{1}
@@ -74,17 +85,20 @@ struct mesh_type : public switchboard::event {
             , reader{std::move(input_reader)} { }
 
     // used by server mesh manager in non-draco integration
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, bool is_active, unsigned id_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, bool is_active,
+                              unsigned id_)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , active{is_active}
             , id{id_}
             , chunk_id{1}
             , max_chunk{1} { }
 
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, bool is_active, unsigned id_,
-                              unsigned num_faces_, unsigned num_vertices_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, bool is_active,
+                              unsigned id_, unsigned num_faces_, unsigned num_vertices_)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , active{is_active}
             , id{id_}
@@ -92,9 +106,10 @@ struct mesh_type : public switchboard::event {
             , num_vertices{num_vertices_} { }
 
     // type to pass to mesh manager
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, bool is_active, unsigned id_,
-                              unsigned chunk_id_, unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, bool is_active,
+                              unsigned id_, unsigned chunk_id_, unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , active{is_active}
             , id{id_}
@@ -103,10 +118,11 @@ struct mesh_type : public switchboard::event {
             , num_faces{num_faces_}
             , num_vertices{num_vertices_} { }
 
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_compressed, bool is_active, unsigned id_,
-                              unsigned chunk_id_, unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_,
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_compressed, bool is_active,
+                              unsigned id_, unsigned chunk_id_, unsigned max_chunk_, unsigned num_faces_, unsigned num_vertices_,
                               std::unique_ptr<draco_illixr::PlyReader> input_reader)
-            : mesh(input_mesh)
+            : type{type_}
+            , mesh(input_mesh)
             , compressed{is_compressed}
             , active{is_active}
             , id{id_}
@@ -116,9 +132,10 @@ struct mesh_type : public switchboard::event {
             , num_vertices{num_vertices_}
             , reader{std::move(input_reader)} { }
 
-    [[maybe_unused]]mesh_type(const std::vector<char>& input_mesh, bool is_active, unsigned id_, unsigned chunk_id_,
-                              unsigned max_chunk_)
-            : mesh(input_mesh)
+    [[maybe_unused]]mesh_type(const unsigned type_, const std::vector<char>& input_mesh, bool is_active, unsigned id_,
+                              unsigned chunk_id_, unsigned max_chunk_)
+            : type{type_}
+            , mesh(input_mesh)
             , active{is_active}
             , id{id_}
             , chunk_id{chunk_id_}
