945464534cebcbea968f662ba0f171605040f922
astro-friedel <friedel@illinois.edu>
2025-08-29 14:08:24 -0500

    fixed draco dependency resolution

--- cmake/GetDraco.cmake
+++ cmake/GetDraco.cmake
@@ -1,12 +1,23 @@
-pkg_check_modules(draco QUIET draco)
-
-if (NOT draco_found)
+find_package(draco_illixr QUIET CONFIG)
+if (NOT draco_illixr_FOUND)
     externalproject_add(Draco_ext
                         GIT_REPOSITORY https://github.com/ILLIXR/draco.git
                         GIT_TAG bba2a71ae3d46631a3b6d969e60730d570e904aa
                         PREFIX ${CMAKE_BINARY_DIR}/_deps/draco
-                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DDRACO_TRANSCODER_SUPPORTED=ON -DCMAKE_BUILD_TYPE=Release ${DEPENDENCY_COMPILE_ARGS}
+                        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DDRACO_TRANSCODER_SUPPORTED=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=${CLANG_CXX_EXE} -DCMAKE_C_COMPILER=${CLANG_EXE}
     )
     set(Draco_DEP_STR Draco_ext)
     set(Draco_EXTERNAL Yes)
+    add_library(draco_illixr::draco STATIC IMPORTED)
+    set_target_properties(draco_illixr::draco PROPERTIES
+                          INTERFACE_COMPILE_FEATURES "cxx_std_11"
+                          INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+    )
+    set_target_properties(draco_illixr::draco PROPERTIES
+                          IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+                          IMPORTED_LOCATION "/home/friedel/ilxr-c/lib/libdraco_illixr.a"
+    )
+
+else()
+    message(STATUS "  Found draco_illixr, ${draco_illixr_VERSION}")
 endif()
