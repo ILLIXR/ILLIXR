78d79c3ef7eaef6bc555d562ade9128589329046
Doug Friedel <friedel@illinois.edu>
2025-09-15 10:16:46 -0500

    updated package detection for windows

--- cmake/GetYamlcpp.cmake
+++ cmake/GetYamlcpp.cmake
@@ -1,7 +1,9 @@
 # CMake module to look for yaml-cpp
 # if it is not found then it is downloaded and marked for compilation and install
-
-pkg_check_modules(yaml-cpp QUIET yaml-cpp)
+find_package(yaml-cpp QUIET CONFIG)
+if(NOT yaml-cpp_FOUND)
+    pkg_check_modules(yaml-cpp QUIET yaml-cpp)
+endif()
 
 if(yaml-cpp_FOUND)
     set(Yamlcpp_VERSION "${yaml-cpp_VERSION}")   # set current version
@@ -11,12 +13,12 @@ else()
             GIT_TAG 0.8.0                                           # sha5 hash for specific commit to pull (if there is no specific tag to use)
             PREFIX ${CMAKE_BINARY_DIR}/_deps/yaml-cpp               # the build directory
             # arguments to pass to CMake
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF -DYAML_BUILD_SHARED_LIBS=ON
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${DEPENDENCY_BUILD_TYPE} -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF -DYAML_BUILD_SHARED_LIBS=ON
             )
     # set variables for use by modules that depend on this one
     set(Yamlcpp_EXTERNAL Yes)      # Mark that this module is being built
     set(yaml-cpp_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
     set(yaml-cpp_LIBDIR ${CMAKE_INSTALL_PREFIX}/lib)
-    set(yaml-cpp_LIBRARIES yaml-cpp)
+    set(yaml-cpp_LIBRARIES "yaml-cpp${DEPENDENCY_BUILD_SUFFIX}")
     add_dependencies(plugin.main${ILLIXR_BUILD_SUFFIX} cpp-yaml)
 endif()
