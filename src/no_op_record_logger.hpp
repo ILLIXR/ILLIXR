0cf7fbbd490027d70c9d309ba312cf2bdbf8ac58
astro-friedel <friedel@illinois.edu>
2025-09-23 10:20:36 -0500

    added no-op record logger

---
+++ src/no_op_record_logger.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "illixr/record_logger.hpp"
+
+namespace ILLIXR {
+class no_op_record_logger : public record_logger {
+    void log(const record& r) override {
+        r.mark_used();
+    }
+};
+}
