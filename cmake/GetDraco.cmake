f1099ec47b260d516adb5bc7060c1bc9123f89da
astro-friedel <friedel@illinois.edu>
2025-09-30 13:30:48 -0500

    fix for building infinitam

--- cmake/GetDraco.cmake
+++ cmake/GetDraco.cmake
@@ -1,8 +1,10 @@
 find_package(draco_illixr QUIET CONFIG)
 if (NOT draco_illixr_FOUND)
+    message(STATUS "Draco not found, downloading.")
     FetchContent_Declare(Draco_ILLIXR
                          GIT_REPOSITORY https://github.com/ILLIXR/draco.git
                          GIT_TAG bba2a71ae3d46631a3b6d969e60730d570e904aa
+                         OVERRIDE_FIND_PACKAGE
     )
     set(TEMP_BUILD_TYPE ${CMAKE_BUILD_TYPE})
     set(DRACO_TRANSCODER_SUPPORTED ON)
@@ -10,21 +12,7 @@ if (NOT draco_illixr_FOUND)
     FetchContent_MakeAvailable(Draco_ILLIXR)
     set(CMAKE_BUILD_TYPE ${TEMP_BUILD_TYPE})
     unset(DRACO_TRANSCODER_SUPPORT)
-     	       
-
-#[[
-    set(Draco_DEP_STR Draco_ext)
-    set(Draco_EXTERNAL Yes)
-    add_library(draco_illixr::draco STATIC IMPORTED)
-    set_target_properties(draco_illixr::draco PROPERTIES
-                          INTERFACE_COMPILE_FEATURES "cxx_std_11"
-                          INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include"
-    )
-    set_target_properties(draco_illixr::draco PROPERTIES
-                          IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-                          IMPORTED_LOCATION "${CMAKE_INSTALL_PREFIX}/lib/libdraco_illixr.a"
-    )
-]]
+    set(Draco_EXTERNAL Yes)      # Mark that this module is being built
 else()
     message(STATUS "  Found draco_illixr, ${draco_illixr_VERSION}")
 endif()
--- cmake/GetInfiniTAM.cmake
+++ cmake/GetInfiniTAM.cmake
@@ -1,4 +1,6 @@
+get_external_for_plugin(Draco)
 if (NOT Infinitam_FOUND)
+    message(STATUS "Downloading InfiniTAM")
     FetchContent_Declare(InfiniTAM_ext
                          GIT_REPOSITORY https://github.com/ILLIXR/InfiniTAM.git
                          GIT_TAG 40ec1705f169b2eddb97e4c2d983d099518ef8bc
@@ -6,4 +8,8 @@ if (NOT Infinitam_FOUND)
     set(ILLIXR_ROOT ${CMAKE_SOURCE_DIR}/include)
 
     FetchContent_MakeAvailable(InfiniTAM_ext)
+    if(TARGET draco_static)
+        add_dependencies(plugin.ada.infinitam${ILLIXR_BUILD_SUFFIX} draco_static)
+        target_include_directories(plugin.ada.infinitam${ILLIXR_BUILD_SUFFIX} PUBLIC ${draco_illixr_SOURCE_DIR}/src ${CMAKE_BINARY_DIR})
+    endif()
 endif()
