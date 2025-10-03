74ee4931597337cfddfbc9bd55b32ac0be2bf704
astro-friedel <friedel@illinois.edu>
2025-10-03 14:46:22 -0500

    reverted to https for git pulls

--- cmake/GetInfiniTAM.cmake
+++ cmake/GetInfiniTAM.cmake
@@ -2,7 +2,7 @@ get_external_for_plugin(Draco)
 if (NOT Infinitam_FOUND)
     message(STATUS "Downloading InfiniTAM")
     FetchContent_Declare(InfiniTAM_ext
-                         GIT_REPOSITORY git@github.com:ILLIXR/InfiniTAM.git
+                         GIT_REPOSITORY https://github.com/ILLIXR/InfiniTAM.git
                          GIT_TAG dc3c2841a6137c05cf0aef52f1d051c86f3f7b8b
     )
     set(ILLIXR_ROOT ${CMAKE_SOURCE_DIR}/include)
