56d195d1e1451f66ba2f7b17e856f9ebf620c6b1
astro-friedel <friedel@illinois.edu>
2025-10-07 12:51:56 -0600

    Deployed ddb700cb0 to v4.1 with MkDocs 1.6.1 and mike 2.1.3

---
+++ v4.1/ILLIXR_API/html/search/classes_a.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['phonebook_0',['phonebook',['../classILLIXR_1_1phonebook.html',1,'ILLIXR']]],
+  ['plugin_1',['plugin',['../classILLIXR_1_1plugin.html',1,'ILLIXR']]],
+  ['pose_5fdata_2',['pose_data',['../structILLIXR_1_1data__format_1_1pose__data.html',1,'ILLIXR::data_format']]],
+  ['pose_5ftype_3',['pose_type',['../structILLIXR_1_1data__format_1_1pose__type.html',1,'ILLIXR::data_format']]],
+  ['position_4',['position',['../structILLIXR_1_1data__format_1_1ht_1_1position.html',1,'ILLIXR::data_format::ht']]],
+  ['print_5ftimer_5',['print_timer',['../classprint__timer.html',1,'']]],
+  ['proper_5fquaternion_6',['proper_quaternion',['../classILLIXR_1_1data__format_1_1proper__quaternion.html',1,'ILLIXR::data_format']]]
+];
---
+++ v4.1/ILLIXR_API/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fps_0',['fps',['../structILLIXR_1_1data__format_1_1camera__data.html#a16aaa71772045a48a001e35c5e736361',1,'ILLIXR::data_format::camera_data']]]
+];
