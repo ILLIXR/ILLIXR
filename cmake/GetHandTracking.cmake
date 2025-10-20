0d281896e34e7315c4a17cd9c1f6b2553c9fd923
astro-friedel <friedel@illinois.edu>
2025-10-20 09:57:31 -0500

    revert to external_project

--- cmake/GetHandTracking.cmake
+++ cmake/GetHandTracking.cmake
@@ -4,38 +4,33 @@ if (HT_ENABLE_GPU)
 else()
     set(POSTFIX "")
 endif()
-if(UNIX) # on windows this is done via vcpkg
-    fetch_git(NAME hand_tracking_dependencies${POSTFIX}
-              REPO https://github.com/ILLIXR/hand_tracking_dependencies.git
-              TAG 8dc45876a9eebc3ad6900fafa5b6ca144e290a9f
-    )
 
-    set(ENABLE_GPU ${HT_ENABLE_GPU})
-    set(LIBRARY_POSTFIX ht)
-    configure_target(hand_tracking_dependencies${POSTFIX})
-    unset(ENABLE_GPU)
-endif()
+externalproject_add(
+        hand_tracking_dependencies${POSTFIX}
+        GIT_REPOSITORY https://github.com/ILLIXR/hand_tracking_dependencies.git
+        GIT_TAG a422f847e9e85d8b77fabb07f6cbbc3af8c5ec32
+        PREFIX ${CMAKE_BINARY_DIR}/_deps/hand_tracking_deps
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release -DENABLE_GPU=${HT_ENABLE_GPU} -DLIBRARY_POSTFIX=ht -DCMAKE_CXX_COMPILER=${CLANG_CXX_EXE} -DCMAKE_C_COMPILER=${CLANG_EXE}
+        INSTALL_COMMAND ""
+)
+
+set(Hand_Tracking_Deps${POSTFIX}_EXTERNAL Yes PARENT_SCOPE)
+list(APPEND EXTERNAL_LIBRARIES "Hand_Tracking_Deps${POSTFIX}")
+set(EXTERNAL_LIBRARIES ${EXTERNAL_LIBRARIES} PARENT_SCOPE)
 
 # hand tracking plugin
+set(PRFX ${CMAKE_BINARY_DIR}/_deps/hand_tracking)
 set(HT_TARGET_NAME "HAND_TRACKING")
 if(HT_ENABLE_GPU)
+    set(PRFX "${PRFX}_gpu")
     set(HT_TARGET_NAME "${HT_TARGET_NAME}_GPU")
 endif()
 
-fetch_git(NAME ${HT_TARGET_NAME}
-          REPOS https://github.com/ILLIXR/hand_tracking.git
-          TAG 0cc6e2cb04514001f6ef8c3f3bea348cc1d965ea
+externalproject_add(
+        ${HT_TARGET_NAME}
+        GIT_REPOSITORY https://github.com/ILLIXR/hand_tracking.git
+        GIT_TAG 0cc6e2cb04514001f6ef8c3f3bea348cc1d965ea
+        PREFIX ${PRFX}
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DHT_ENABLE_GPU=${HT_ENABLE_GPU} -DTFLIBRARY_POSTFIX=ht -DILLIXR_ROOT=${CMAKE_SOURCE_DIR} -DILLIXR_BUILD_SUFFIX=${ILLIXR_BUILD_SUFFIX} -DBUILD_OXR_INTERFACE=${BUILD_OXR_INTERFACE} -DBUILD_OXR_TEST=${BUILD_OXR_TEST} -DCMAKE_CXX_COMPILER=${CLANG_CXX_EXE} -DCMAKE_C_COMPILER=${CLANG_EXE}
+        DEPENDS hand_tracking_dependencies${POSTFIX}
 )
-
-set(TFLIBRARY_POSTFIX ${LIBRARY_POSTFIX})
-set(BUILD_OXR_INTERFACE ${BUILD_OXR_INTERFACE})
-set(BUILD_OXR_TEST ${BUILD_OXR_TEST})
-configure_target(NAME ${HT_TARGET_NAME}
-                 MATCH_BUILD_TYPE
-                 NO_FIND
-)
-unset(TFLIBRARY_POSTFIX)
-unset(BUILD_OXR_INTERFACE)
-unset(BUILD_OXR_TEST)
-
-unset(LIBRARY_POSTFIX)
