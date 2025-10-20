751bf6212fef6609d367806d88c8bf1db7ac3ad2
astro-friedel <friedel@illinois.edu>
2025-10-20 09:56:20 -0500

    revert to external project

--- cmake/GetDepthAI.cmake
+++ cmake/GetDepthAI.cmake
@@ -4,25 +4,24 @@
 find_package(depthai QUIET)
 list(APPEND EXTERNAL_PROJECTS depthai)
 if(depthai_FOUND)
-    set(depthai_VERSION "${depthai_VERSION}" PARENT_SCOPE)   # set current version
+    set(DepthAI_VERSION "${depthai_VERSION}" PARENT_SCOPE)   # set current version
 else()
-    if(WIN32 OR MSVC)
-        message(FATAL_ERROR "depthai should be installed with vcpkg")
-    endif()
-    fetch_git(NAME depthai
-              REPO https://github.com/luxonis/depthai-core.git
-              TAG v2.29.0
-    )
-    set(BUILD_SHARED_LIBS ON)
-    configure_target(NAME depthai)
-
-        #set(DepthAI_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include ${CMAKE_INSTALL_PREFIX}/include/depthai-shared ${CMAKE_INSTALL_PREFIX}/include/depthai-shared/3rdparty ${CMAKE_INSTALL_PREFIX}/include/depthai ${CMAKE_INSTALL_PREFIX}/lib/cmake/depthai/dependencies/include)
-    #set(DepthAI_LIBRARIES depthai-core;depthai-opencv)
+    EXTERNALPROJECT_ADD(DepthAI_ext
+            GIT_REPOSITORY https://github.com/luxonis/depthai-core.git   # Git repo for source code
+            GIT_TAG v2.29.0                                              # sha5 hash for specific commit to pull (if there is no specific tag to use)
+            PREFIX ${CMAKE_BINARY_DIR}/_deps/depthai                     # the build directory
+            #arguments to pass to CMake
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release ${DEPTHAI_CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_CXX_COMPILER=${CLANG_CXX_EXE} -DCMAKE_C_COMPILER=${CLANG_EXE} -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+            )
+    # set variables for use by modules that depend on this one
+    set(DepthAI_DEP_STR DepthAI_ext)   # Dependency string for other modules that depend on this one
+    set(DepthAI_EXTERNAL Yes)      # Mark that this module is being built
+    set(DepthAI_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include ${CMAKE_INSTALL_PREFIX}/include/depthai-shared ${CMAKE_INSTALL_PREFIX}/include/depthai-shared/3rdparty ${CMAKE_INSTALL_PREFIX}/include/depthai ${CMAKE_INSTALL_PREFIX}/lib/cmake/depthai/dependencies/include)
+    set(DepthAI_LIBRARIES depthai-core;depthai-opencv)
 
     add_custom_target(cleanup_depthai_spdlog
                       COMMAND rm -rf ${CMAKE_INSTALL_PREFIX}/lib/cmake/depthai/dependencies/include/spdlog
     )
-    add_dependencies(cleanup_depthai_spdlog depthai)
-endif()
 
-set(EXTERNAL_PROJECTS ${EXTERNAL_PROJECTS} PARENT_SCOPE)
+    add_dependencies(cleanup_depthai_spdlog DepthAI_ext)
+endif()
