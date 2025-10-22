b55cf7c19df843d2ac40daaa62fef703f97b9253
Doug Friedel <friedel@illinois.edu>
2025-10-22 10:25:20 -0500

    updated openvins

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -1,9 +1,8 @@
-set(OPENVINS_CMAKE_ARGS "")
-
-EXTERNALPROJECT_ADD(OpenVINS
-        GIT_REPOSITORY https://github.com/ILLIXR/open_vins.git   # Git repo for source code
-        GIT_TAG 06146ec3be7167a2f4b519e104912ea814738e9a         # sha5 hash for specific commit to pull (if there is no specific tag to use)
-        PREFIX ${CMAKE_BINARY_DIR}/_deps/OpenVINS                # the build directory
-        #arguments to pass to CMake
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_CXX_FLAGS=-L${CMAKE_INSTALL_PREFIX}/lib -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DILLIXR_ROOT=${PROJECT_SOURCE_DIR}/include -DILLIXR_BUILD_SUFFIX=${ILLIXR_BUILD_SUFFIX} -DILLIXR_INTEGRATION=ON ${OPENVINS_CMAKE_ARGS}
-        )
+FetchContent_Declare(OpenVINS
+                     GIT_REPOSITORY https://github.com/ILLIXR/open_vins.git   # Git repo for source code
+                     GIT_TAG 23894d601bfc1e1fa064239916cf2276e00b9ca2         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+)
+set(ILLIXR_ROOT ${CMAKE_SOURCE_DIR}/include)
+set(ILLIXR_INTEGRATION ON)
+FetchContent_MakeAvailable(OpenVINS)
+unset(ILLIXR_INTEGRATION)
