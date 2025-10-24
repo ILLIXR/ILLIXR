c31bfc418061daa22cf41f06ee2fe322890c16dc
Doug Friedel <friedel@illinois.edu>
2025-10-24 14:20:39 -0500

    force opencv version

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(OpenVINS
                      GIT_REPOSITORY https://github.com/ILLIXR/open_vins.git   # Git repo for source code
-                     GIT_TAG 23894d601bfc1e1fa064239916cf2276e00b9ca2         # sha5 hash for specific commit to pull (if there is no specific tag to use)
+                     GIT_TAG 4976aa6426e01dad6115d09ad575d84a9ae575da         # sha5 hash for specific commit to pull (if there is no specific tag to use)
 )
 set(ILLIXR_ROOT ${CMAKE_SOURCE_DIR}/include)
 set(ILLIXR_INTEGRATION ON)
