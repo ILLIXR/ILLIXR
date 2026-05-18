ebf98566a9a8607c9b102c49f80f6e16532a7ed7
astro-friedel <friedel@illinois.edu>
2026-05-18 14:58:57 -0500

    updated to hand tracking plugin

--- plugins/hand_tracking/viewer/plugin.hpp
+++ plugins/hand_tracking/viewer/plugin.hpp
@@ -5,8 +5,15 @@
 #include <GLFW/glfw3.h> // Also loading first, just to be safe
 // clang-format on
 
+#ifdef ILLIXR_HT_DIR
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#include TOSTRING(ILLIXR_HT_DIR/hand_tracking_data.hpp)
+#else
+#error "Can not locate hand_tracking_data.hpp, please specify its path with -DILLIXR_HT_DIR"
+#endif
+
 #include "illixr/data_format/camera_data.hpp"
-#include "illixr/data_format/hand_tracking_data.hpp"
 #include "illixr/imgui/imgui.h"
 #include "illixr/plugin.hpp"
 #include "illixr/switchboard.hpp"
@@ -19,16 +26,16 @@ public:
     viewer(const std::string& name_, phonebook* pb_);
     ~viewer() override;
     void start() override;
-    void make_gui(const switchboard::ptr<const data_format::ht::ht_frame>& frame);
+    void make_gui(const switchboard::ptr<const ht::ht_frame>& frame);
 
 private:
-    void make_detection_table(data_format::units::eyes eye, int idx, const std::string& label) const;
+    void make_detection_table(data_format::pose::side eye, int idx, const std::string& label) const;
     void make_position_table() const;
 
-    std::shared_ptr<relative_clock>            clock_;
-    const std::shared_ptr<switchboard>         switchboard_;
-    std::shared_ptr<data_format::ht::ht_frame> ht_frame_;
-    const data_format::pose_data               pose_;
+    std::shared_ptr<relative_clock>    clock_;
+    const std::shared_ptr<switchboard> switchboard_;
+    std::shared_ptr<ht::ht_frame>      ht_frame_;
+    const data_format::pose::pose_base pose_;
 #ifdef VIEW_DUMP
     std::map<data_format::ht::hand, data_format::ht::hand_points> _true_hand_positions;
 #endif
@@ -40,18 +47,16 @@ private:
     Eigen::Vector2i processed_size = Eigen::Vector2i::Zero();
     Eigen::Vector2i combined_size  = Eigen::Vector2i::Zero();
 
-    Eigen::Matrix4f                  basicProjection_;
-    const data_format::ht::ht_frame* current_frame_;
-    ImVec4                           clear_color_ = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    Eigen::Matrix4f     basicProjection_;
+    const ht::ht_frame* current_frame_;
+    ImVec4              clear_color_ = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     bool                                                enabled_right_ = false;
     std::string                                         tab_label_;
-    data_format::units::eyes                            single_eye_ = data_format::units::LEFT_EYE;
+    data_format::pose::side                             single_eye_ = data_format::pose::LEFT;
     std::vector<ILLIXR::data_format::image::image_type> detections_;
 
-    std::map<uint64_t, data_format::ht::ht_frame> ht_frames_;
-    static int                                    requested_unit_;
-    static data_format::units::measurement_unit   base_unit_;
+    std::map<uint64_t, ht::ht_frame> ht_frames_;
 };
 
 } // namespace ILLIXR
