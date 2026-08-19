5d18035537bf4f92fd2fed5149641854ba31823e
astro-friedel <friedel@illinois.edu>
2026-08-19 11:27:17 -0500

    fixed missing parameters in constructor

--- plugins/offload_rendering_server/offload_rendering_server.cpp
+++ plugins/offload_rendering_server/offload_rendering_server.cpp
@@ -268,10 +268,10 @@ void offload_rendering_server::enqueue_for_network_send(pose::fast_head_pose_typ
     if (use_pass_depth_) {
         frames_topic_.put(std::make_shared<compressed_frame>(encode_out_color_packets_[0], encode_out_color_packets_[1],
                                                              encode_out_depth_packets_[0], encode_out_depth_packets_[1], pose,
-                                                             timestamp, 0.f, 0.f, nalu_only_));
+                                                             timestamp, 0, 0.f, 0.f, nalu_only_));
     } else {
         frames_topic_.put(std::make_shared<compressed_frame>(encode_out_color_packets_[0], encode_out_color_packets_[1], pose,
-                                                             timestamp, nalu_only_));
+                                                             timestamp, 0,nalu_only_));
     }
 }
 
