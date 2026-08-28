6817c03f89fec498af7cbecd67e6d7b425505bb7
astro-friedel <friedel@illinois.edu>
2026-08-28 14:43:58 -0500

    new monado tag

--- cmake/GetMonado_vk.cmake
+++ cmake/GetMonado_vk.cmake
@@ -23,7 +23,7 @@ if (MONADO_VK_FOUND AND MONADO_VK_OXR_LIB_FOUND AND MONADO_LIBRARY_FOUND)
 else ()
     fetch_git(NAME Monado_VK
               REPO https://github.com/ILLIXR/monado_update.git
-              TAG  866d03408130c84f4d46f3da985a27ec59d62c55
+              TAG  356e53a81229af502c4a3801f35f622945ba7b75
     )
     if (UNIX)
         set(XRT_HAVE_LIBUDEV ON)
