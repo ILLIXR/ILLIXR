acfceb4fe6b7040e0b53526b2e1c4268393db308
astro-friedel <friedel@illinois.edu>
2026-08-28 14:11:04 -0500

    new openvins tag

--- cmake/GetOpenVINS.cmake
+++ cmake/GetOpenVINS.cmake
@@ -1,6 +1,6 @@
 fetch_git(NAME OpenVINS
           REPO https://github.com/ILLIXR/open_vins.git
-          TAG df5c70fdbd24ec8b5c6711ef1067eef614551779
+          TAG c7fe7c09c3a16e4d7e1062fdce949aa145fb010a
 )
 
 set(TEMP_FLAGS ${CMAKE_CXX_FLAGS})
