f78ab22fe17bef55fcba52b99a511a192dea0e85
astro-friedel <friedel@illinois.edu>
2026-09-02 13:49:21 -0500

    added logging

--- plugins/android_sensors/plugin.cpp
+++ plugins/android_sensors/plugin.cpp
@@ -461,6 +461,13 @@ bool xr_sensor_capture::get_pose_at_timestamp(XrTime timestamp, float out_matrix
         return false;
     }
 
+    spdlog::get("illixr")->debug("[xrLocateSpace] pose: {}, {}, {}, {}; {}, {}, {}", location.pose.orientation.x,
+    location.pose.orientation.y,
+    location.pose.orientation.z,
+    location.pose.orientation.w,
+    location.pose.position.x,
+    location.pose.position.y,
+    location.pose.position.z);
     const float x  = location.pose.orientation.x;
     const float y  = location.pose.orientation.y;
     const float z  = location.pose.orientation.z;
