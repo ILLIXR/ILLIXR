fbecee53760e8287dca6a81c65c5a98d64789ff3
astro-friedel <friedel@illinois.edu>
2026-09-03 12:34:53 -0500

    remove changes from other PR's

--- cmake/GetMonado_vk.cmake
+++ cmake/GetMonado_vk.cmake
@@ -41,10 +41,10 @@ else ()
         XRT_BUILD_DRIVER_ROKID XRT_FEATURE_SLAM XRT_FEATURE_COLOR_LOG XRT_MODULE_MONADO_GUI)
 
     foreach(ITEM IN LISTS ON_VARS)
-        set(${ITEM} ON)
+        set(${ITEM} ON CACHE BOOL "" FORCE)
     endforeach()
     foreach(ITEM IN LISTS OFF_VARS)
-        set(${ITEM} OFF)
+        set(${ITEM} OFF CACHE BOOL "" FORCE)
     endforeach()
 
     if(MSVC OR WIN32)
