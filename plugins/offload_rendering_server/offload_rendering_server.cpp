5d88dd10c8567ed78b4abc5b6eb8f226722465a8
Douglas N Friedel <friedel@illinois.edu>
2026-09-14 10:32:31 -0500

    fixed bug

--- plugins/offload_rendering_server/offload_rendering_server.cpp
+++ plugins/offload_rendering_server/offload_rendering_server.cpp
@@ -91,8 +91,9 @@ void offload_rendering_server::stop() {
 
 void offload_rendering_server::_p_thread_setup() {
 #ifdef OPENXR_CLIENT
-    hmd_setup_.recommended_image_width  = (uint32_t) (width_pixels * overscan_ / 2.);
-    hmd_setup_.recommended_image_height = (uint32_t) (height_pixels * overscan_);
+    display_params disp_p;
+    hmd_setup_.recommended_image_width  = (uint32_t) (disp_p.width_pixels * overscan_ / 2.);
+    hmd_setup_.recommended_image_height = (uint32_t) (disp_p.height_pixels * overscan_);
     for (int eye = 0; eye < 2; eye++) {
         hmd_setup_.fov_angle_left[eye]  = overscan_ * ILLIXR::server_params::fov_left[eye];
         hmd_setup_.fov_angle_right[eye] = overscan_ * ILLIXR::server_params::fov_right[eye];
